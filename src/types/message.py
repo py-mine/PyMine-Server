8d431c63223382301dcea605b6ebf22810dcffd0
Iapetus-11 <iapetus011@gmail.com>
2020-12-19 18:20:53 -0500

    start rewriting Message

--- src/types/message.py
+++ src/types/message.py
@@ -1,129 +1,34 @@
 from __future__ import annotations
-import re
-import functools
-from typing import Any
-from src.types.packet import Packet
 
-__all__ = (
-    'Message'
-)
+from src.types.buffer import Buffer
 
 
-def _load_styles():
-    data = {
-        '0': 'black',
-        '1': 'dark_blue',
-        '2': 'dark_green',
-        '3': 'dark_aqua',
-        '4': 'dark_red',
-        '5': 'dark_purple',
-        '6': 'gold',
-        '7': 'gray',
-        '8': 'dark_gray',
-        '9': 'blue',
-        'a': 'green',
-        'b': 'aqua',
-        'c': 'red',
-        'd': 'light_purple',
-        'e': 'yellow',
-        'f': 'white',
-        'k': 'obfuscated',
-        'l': 'bold',
-        'm': 'strikethrough',
-        'n': 'underline',
-        'o': 'italic',
-        'r': 'reset',
-    }
-
-    _code_by_name = {}
-    _code_by_prop = {}
-    for code, name in data.items():
-        _code_by_name[name] = code
-        if code in 'klmnor':
-            if name == 'underline':
-                prop = 'underlined'
-            else:
-                prop = name
-            _code_by_prop[prop] = code
-
-    return _code_by_name, _code_by_prop
-
-
-code_by_name, code_by_prop = _load_styles()
-
-
-@functools.total_ordering
 class Message:
     """
     A Minecraft chat message
     """
 
-    def __init__(self, message):
-        self.message = message
+    def __init__(self, msg):
+        self.msg = msg
 
     @classmethod
-    def from_packet(cls, packet: Packet) -> object:
-        """Initialize a :class:`Message` from a :class:`Packet`"""
+    def from_buf(cls, buf: Buffer) -> Message:
+        """Creates a Minecraft chat message from a buffer"""
 
-        return cls(packet.unpack_json())
+        return cls(buf.unpack_json())
 
     @classmethod
-    def from_string(cls, text) -> Message:
-        """Initialize a :class:`Message` from a string"""
+    def from_string(cls, text: str) -> Message:
+        """Creates a Minecraft chat message from json"""
 
         return cls({'text': text})
 
-    @classmethod
-    def strip_styles(cls, text: str) -> str:
-        """Remove color styles from the given text"""
+    def to_bytes(self) -> bytes:
+        """Converts a Minecraft chat message to bytes"""
 
-        return re.sub(u'\u00A7.', '', text)
+        return Buffer.pack_json(self.msg)
 
-    @staticmethod
-    def parse(obj: Any):
-        if isinstance(obj, str):
-            return obj
-        if isinstance(obj, list):
-            return ''.join((Message.parse(e) for e in obj))
-        if isinstance(obj, dict):
-            text = ''
-            for prop, code in code_by_prop.items():
-                if obj.get(prop):
-                    text += u'\u00a7' + code
-            if 'color' in obj:
-                text += u'\u00a7' + code_by_name[obj['color']]
-            if 'translate' in obj:
-                text += obj['translate']
-                if 'with' in obj:
-                    args = u', '.join((Message.parse(e) for e in obj['with']))
-                    text += u'{0}'.format(args)
-            if 'text' in obj:
-                text += obj['text']
-            if 'extra' in obj:
-                text += Message.parse(obj['extra'])
-            return text
+    def to_string(self):
+        """Converts a Minecraft chat message to text"""
 
-    def to_bytes(self):
-        """Return the :class:`Message` in the form of bytes."""
-
-        return Packet.pack_json(self.message)
-
-    def to_string(self, strip_styles=True) -> str:
-        """
-        Return the actual content of the message, optionally keep the styles.
-        """
-
-        text = Message.parse(self.strip_styles(self.message) if strip_styles else self.message)
-        return text
-
-    def __eq__(self, other: Any) -> bool:
-        return isinstance(other, Message) and self.message == other.message
-
-    def __lt__(self, other: Any) -> bool:
-        return isinstance(other, Message) and self.message < other.message
-
-    def __str__(self):
-        self.to_string()
-
-    def __repr__(self) -> str:
-        return f'<Message {str(self)}>'
+        pass

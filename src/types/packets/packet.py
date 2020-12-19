5c8c16cab51ae4645d24ec20141e3bf50658a975
Iapetus-11 <iapetus011@gmail.com>
2020-12-19 11:41:05 -0500

    fix docstring formatting

--- src/types/packets/packet.py
+++ src/types/packets/packet.py
@@ -23,6 +23,7 @@ class Packet:
         Read data from the buffer, if the length is None
         then all remaining data from the buffer is sent.
         """
+
         try:
             if length is None:
                 length = len(self.buf)
@@ -34,30 +35,36 @@ class Packet:
 
     def reset(self) -> None:
         """Reset the position in the buffer."""
+
         self.pos = 0
 
     @classmethod  # Packet.pack_array() not an instance of a packet.pack_array() (Packet().pack_array())
     def pack_array(cls, f, array: list) -> bytes:
         """Pack an array/list to bytes."""
+
         return struct.pack(f'>{f*len(array)}', *array)
 
     def unpack_array(self, f, length: int) -> list:
         """Unpack an array/list from the buffer."""
+
         data = self.read(struct.calcsize(f'>{f}') * length)
         return list(struct.unpack(f'>{f*length}', data))
 
     @classmethod
     def pack_bool(cls, boolean) -> bytes:
         """Pack a boolean into bytes."""
+
         return struct.pack(f'>?', boolean)
 
     def unpack_bool(self) -> bool:
         """Unpack a boolean from the buffer."""
+
         return struct.unpack(f'>?', self.read(1))
 
     @classmethod
     def pack_varint(cls, num: int, max_bits: int = 32) -> bytes:
         """Pack a varint (Varying Integer) into bytes."""
+
         num_min, num_max = (-1 << (max_bits - 1)), (+1 << (max_bits - 1))
 
         if not (num_min <= num < num_max):
@@ -82,6 +89,7 @@ class Packet:
 
     def unpack_varint(self, max_bits: int = 32) -> int:
         """Unpack a varint from the buffer."""
+
         num = 0
 
         for i in range(10):
@@ -103,8 +111,11 @@ class Packet:
         return num
 
     def pack(self, comp_thresh: int = -1) -> bytes:
-        """Packs the final packet to bytes, readies the data to be sent,
-         handles compression and length prefixing."""
+        """
+        Packs the final packet to bytes, readies the data to be sent,
+        handles compression and length prefixing.
+        """
+
         if comp_thresh >= 0:
             if len(self.buf) >= comp_thresh:
                 data = self.pack_varint(len(self.buf)) + \
@@ -118,6 +129,7 @@ class Packet:
 
     def unpack(self, comp_thresh: int = -1):
         """Unpack packet from the buffer, handles compression, length prefixing."""
+
         p = Packet(self.read(self.unpack_varint()))
 
         if comp_thresh >= 0:
@@ -131,19 +143,23 @@ class Packet:
     @classmethod
     def pack_string(cls, text: str) -> bytes:
         """Packs a string into bytes."""
+
         text = text.encode('utf-8')
         return cls.pack_varint(len(text), max_bits=16) + text
 
     def unpack_string(self) -> str:
         """Unpacks a string from the buffer."""
+
         length = self.unpack_varint(max_bits=16)
         return self.read(length).decode('utf-8')
 
     @classmethod
     def pack_json(cls, obj: object) -> bytes:
         """Packs json serializable data into bytes."""
+
         return cls.pack_string(json.dumps(obj))
 
     def unpack_json(self) -> object:
         """Unpacks serialized json data from the buffer."""
+        
         return json.loads(self.unpack_string())

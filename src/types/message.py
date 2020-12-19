c4e5da572ecc5d2bd64b93aa42fd744081e8af08
Iapetus-11 <iapetus011@gmail.com>
2020-12-19 12:27:54 -0500

    work on message.py

---
+++ src/types/message.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from src.types.packets.packet import Packet
+
+class Message:
+    """
+    A Minecraft chat message
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+    @classmethod
+    def from_packet(cls, packet: Packet) -> object:
+        return cls(packet.unpack_json())
+
+    def to_bytes(self):
+        return Packet.pack_json(self.value)
+
+    @classmethod
+    def from_string(cls, text):
+        pass

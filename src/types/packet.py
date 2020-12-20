bca2f00828a2c17a5d91907ee5b0cb6830bdec48
Iapetus-11 <iapetus011@gmail.com>
2020-12-20 15:14:00 -0500

    add code for unpack_pos() in packet.py

--- src/types/packet.py
+++ src/types/packet.py
@@ -28,9 +28,9 @@ class Packet(Buffer):
         """Packs a Minecraft position (x, y, z) into bytes."""
 
         def to_twos_complement(num, bits):
-            return number + (1 << bits) if number < 0 else number
+            return num + (1 << bits) if num < 0 else num
 
-        return struct.pack('Q', sum((
+        return struct.pack('>Q', sum((
             to_twos_complement(x, 26) << 38,
             to_twos_complement(y, 12) << 26,
             to_twos_complement(z, 26)
@@ -39,4 +39,16 @@ class Packet(Buffer):
     def unpack_pos(self) -> tuple:
         """Unpacks a Minecraft position (x, y, z) from the buffer."""
 
-        pass
+        def from_twos_complement(num, bits):
+            if num & (1 << (bits - 1)) != 0:
+                num -= (1 << bits)
+
+            return num
+
+        data = self.unpack('>Q')
+
+        x = from_twos_complement(data >> 38, 26)
+        y = from_twos_complement(data >> 26 & 0xFFF, 12)
+        z = from_twos_complement(data & 0x3FFFFFF, 26)
+
+        return x, y, z

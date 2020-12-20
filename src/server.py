f8bae057b46e673a0b09ffea1c527db6b7e14412
Iapetus-11 <iapetus011@gmail.com>
2020-12-20 12:50:07 -0500

    stuff

--- src/server.py
+++ src/server.py
@@ -1,10 +1,11 @@
 import asyncio
+import quarry
 
-async def handle_con(*args):
-    print(args)
+async def handle_con(reader, writer):
+    pass
 
 async def start():
-    server = await asyncio.start_server(port=25565)
+    server = await asyncio.start_server(handle_con, port=69)
 
     async with server:
         await server.serve_forever()

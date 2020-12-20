9c5efbf34717b63c275aa2811fda0b83335e53c4
Iapetus-11 <iapetus011@gmail.com>
2020-12-20 15:56:40 -0500

    fix position of __all__ in blocks.py

--- src/data/blocks.py
+++ src/data/blocks.py
@@ -1,7 +1,5 @@
 import json
 
-__all__ = [BLOCKS, BLOCKS_BY_ID]
-
 # Blocks.json generated via data generator java -cp server.jar net.minecraft.data.Main
 
 with open('blocks.json', 'r') as blocks_file:
@@ -12,3 +10,5 @@ BLOCKS_BY_ID = {}
 for k, v in BLOCKS.items():
     for state in v['states']:
         BLOCKS_BY_ID[state['id']] = k
+
+__all__ = [BLOCKS, BLOCKS_BY_ID]

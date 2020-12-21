d7228cbf2b95f45d3ea5a05a098ec251b56d8c71
Iapetus-11 <iapetus011@gmail.com>
2020-12-21 17:21:54 -0500

    add registry.py

---
+++ src/data/registry.py
@@ -0,0 +1,11 @@
+import json
+
+__all__ = (
+    'REGISTRY',
+    'ITEMS'
+)
+
+with open('registries.json') as registry:  # generated from server jar
+    REGISTRY = json.load(registry)
+
+ITEMS = REGISTRY['minecraft:item']

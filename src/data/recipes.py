39c0a315cd68280b413ba071bb1c55e08dbebe88
Iapetus-11 <iapetus011@gmail.com>
2020-12-21 15:31:54 -0500

    finish recipes.py with debug

---
+++ src/data/recipes.py
@@ -0,0 +1,11 @@
+import json
+import os
+
+__all__ = ['RECIPES']
+
+RECIPES = {}
+
+for recipe in os.listdir('recipes'):
+    print('getting recipe')
+    with open(recipe, 'r') as recipe_file:
+        RECIPES[recipe[:5]] = json.load(recipe_file)

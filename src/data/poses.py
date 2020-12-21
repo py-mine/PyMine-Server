dd9499b3beeea5ef7fa618785d74d795d2bdaa74
Iapetus-11 <iapetus011@gmail.com>
2020-12-21 11:04:01 -0500

    formatting changes to poses.py

--- src/data/poses.py
+++ src/data/poses.py
@@ -1,4 +1,12 @@
 
 __all__ = ('POSES',)
 
-POSES = ('standing', 'fall_flying', 'sleeping', 'swimming', 'spin_attack', 'sneaking', 'dying',)
+POSES = (
+    'standing',
+    'fall_flying',
+    'sleeping',
+    'swimming',
+    'spin_attack',
+    'sneaking',
+    'dying',
+)

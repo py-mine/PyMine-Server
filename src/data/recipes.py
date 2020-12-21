bb588690de438e58252a946c1bff83893e380721
Iapetus-11 <iapetus011@gmail.com>
2020-12-21 15:32:55 -0500

    fix path in os.listdir()

--- src/data/recipes.py
+++ src/data/recipes.py
@@ -5,7 +5,7 @@ __all__ = ['RECIPES']
 
 RECIPES = {}
 
-for recipe in os.listdir('recipes'):
+for recipe in os.listdir('src/data/recipes'):
     print('getting recipe')
     with open(recipe, 'r') as recipe_file:
         RECIPES[recipe[:5]] = json.load(recipe_file)

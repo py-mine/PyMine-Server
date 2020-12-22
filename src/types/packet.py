b41d2834ad752d79fa90c7c93b6f5813a5f59846
Sh-wayz <shwayz03@gmail.com>
2020-12-21 21:20:32 -0500

    removed typehints

--- src/types/packet.py
+++ src/types/packet.py
@@ -2,6 +2,6 @@ from buffer import Buffer
 class Packet(Buffer):
     """Base packet class used for networking stuff i don't get enough to explain here."""
     def __init__(self):
-        self.length: int = 0
-        self.id: int = 0
-        self.data: list = []
+        self.length = 0
+        self.id = 0
+        self.data = []

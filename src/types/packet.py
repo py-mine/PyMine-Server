0a898ac2fb8c2603622adec8fffa3d29ef82d0cb
Iapetus-11 <iapetus011@gmail.com>
2020-12-21 22:50:05 -0500

    finish __init__ and start on from_bytes()

--- src/types/packet.py
+++ src/types/packet.py
@@ -3,7 +3,15 @@ from buffer import Buffer
 class Packet(Buffer):
     """Base packet class."""
 
-    def __init__(self):
-        self.length = 0
-        self.id = 0
-        self.data = []
+    def __init__(self, compression_threshold: int = -1):
+        Buffer.__init__(self)
+
+        self.id = -0x1
+        self.compression_threshold = compression_threshold
+
+    @classmethod
+    def from_bytes(cls, data: bytes, comp_thresh: int = -1):
+        out = Buffer.from_bytes(data, comp_thresh)
+
+        self.id = self.unpack_varint()
+        self.compression_threshold = comp_thresh

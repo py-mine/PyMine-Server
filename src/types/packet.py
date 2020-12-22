4507dddaee59f6c303cc82c340cbc1ee29deafbe
Sh-wayz <shwayz03@gmail.com>
2020-12-22 08:20:54 -0500

    remove ✨*obsolete*✨ comments

--- src/types/packet.py
+++ src/types/packet.py
@@ -11,8 +11,6 @@ class Packet:
 
     """
 
-    # id is the packet id (Like 0x00), buf is the bytes/data of the packet,
-    # comp_thresh is compression threshold (-1 for no compression)
     def __init__(self, id: int = -0x1, comp_thresh: int = -1) -> None:
         self.id = id
         self.comp_thresh = comp_thresh

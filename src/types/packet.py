de6aa128641b8cc5d588e7193809ccab59212435
Iapetus-11 <iapetus011@gmail.com>
2020-12-22 11:14:43 -0500

    fix docstring formatting in packet.py

--- src/types/packet.py
+++ src/types/packet.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 
 class Packet:
-    """Base Packet class.
+    """
+    Base Packet class.
 
     :param int id: Packet identifaction number. Defaults to -0x1.
     :param int comp_thresh: Compression threshold. Defaults to -1(no compression).
     :attr id:
     :attr comp_thresh:
-
     """
 
     def __init__(self, id: int = -0x1, comp_thresh: int = -1) -> None:

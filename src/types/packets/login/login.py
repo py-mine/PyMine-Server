703c01d295b8729da6971bc7c4ac9c1168edb0f2
Iapetus-11 <iapetus011@gmail.com>
2020-12-22 12:41:10 -0500

    add LoginEncryptionRequest

--- src/types/packets/login/login.py
+++ src/types/packets/login/login.py
@@ -14,3 +14,14 @@ class LoginStart(Packet):  # Client -> Server
     @classmethod
     def decode(cls, buf: Buffer) -> LoginStart:
         return LoginStart(buf.read(buf.unpack_varint()).decode('UTF-8'))
+
+
+class LoginEncryptionRequest(Packet):  # Server -> Client
+    def __init__(self, public_key: bytes) -> None:  # https://wiki.vg/Protocol#Encryption_Request
+        self.public_key = public_key
+        self.verify_token = secrets.token_bytes(16)
+
+    def encode(self) -> bytes:
+        return Buffer.pack_varint(20) + (' '*20).encode('UTF-8') + \
+         Buffer.pack_varint(len(self.public_key)) + self.public_key + \
+         Buffer.pack_varint(len(self.verify_token)) + self.verify_token

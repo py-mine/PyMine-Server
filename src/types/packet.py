26a6881fb32991f26a3efbb69978c4a4bcec7fa0
Iapetus-11 <iapetus011@gmail.com>
2020-12-22 00:38:03 -0500

    rewrite Packet

--- src/types/packet.py
+++ src/types/packet.py
@@ -1,25 +1,18 @@
 from __future__ import annotations
 
-from src.types.buffer import Buffer
 
-
-class Packet(Buffer):
+class Packet:
     """Base packet class."""
 
     # id is the packet id (Like 0x00), buf is the bytes/data of the packet,
     # comp_thresh is compression threshold (-1 for no compression)
-    def __init__(self, id: int = -0x1, buf: bytes = None, comp_thresh: int = -1) -> None:
-        super().__init__(buf)
-
+    def __init__(self, id: int = -0x1, comp_thresh: int = -1) -> None:
         self.id = id
         self.comp_thresh = comp_thresh
 
     @classmethod
-    def from_bytes(cls, data: bytes, comp_thresh: int = -1) -> Packet:
-        buf = super().from_bytes(data, comp_thresh)
+    def decode(cls, data: bytes, comp_thresh: int = -1) -> Packet:
+        raise NotImplementedError
 
-        return cls(buf.unpack_varint(), buf.read(), comp_thresh)
-
-    def to_bytes(self) -> bytes:
-        self.buf = self.pack_varint(self.id) + self.buf
-        return super().to_bytes(self.comp_thresh)
+    def encode(self) -> bytes:
+        raise NotImplementedError

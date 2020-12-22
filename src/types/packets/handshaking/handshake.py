1e84ab20d6e55f12e5067e5da4669e744539a7dd
Iapetus-11 <iapetus011@gmail.com>
2020-12-21 23:31:15 -0500

    finish HandshakeHandshake

--- src/types/packets/handshaking/handshake.py
+++ src/types/packets/handshaking/handshake.py
@@ -1,1 +1,12 @@
 from src.types.packet import Packet
+
+__all__ = ('HandshakeHandshake',)
+
+class HandshakeHandshake(Packet):  # Serverbound (client -> server) only
+    def __init__(self, buf: bytes, comp_thresh = -1):
+        super().__init__(0x00, buf, comp_thresh)
+
+        self.protocol = super().unpack_varint()
+        self.address = super().unpack_string()
+        self.port = super().unpack('H')
+        self.next_state = super().unpack_varint()

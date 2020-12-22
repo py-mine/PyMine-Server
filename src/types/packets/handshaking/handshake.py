4cb3b592fd54161c7fa90c5e21b92430f77ad3e1
Iapetus-11 <iapetus011@gmail.com>
2020-12-22 00:44:00 -0500

    rewrite HandshakeHandshake packet

--- src/types/packets/handshaking/handshake.py
+++ src/types/packets/handshaking/handshake.py
@@ -1,12 +1,24 @@
+from __future__ import annotations
+
+from src.types.buffer import Buffer
 from src.types.packet import Packet
 
-__all__ = ('HandshakeHandshakeToServer',)
+__all__ = ('HandshakeHandshake',)
 
-class HandshakeHandshakeToServer(Packet):
-    def __init__(self, buf: bytes, comp_thresh = -1) -> None:
-        super().__init__(0x00, buf, comp_thresh)
+class HandshakeHandshake(Packet):  # Serverbound only (client -> server)
+    def __init__(self, protocol: int, address: str, port: int, next_state: int, comp_thresh: int = -1) -> None:
+        super().__init__(0x00, comp_thresh)
 
-        self.protocol = super().unpack_varint()
-        self.address = super().unpack_string()
-        self.port = super().unpack('H')
-        self.next_state = super().unpack_varint()
+        self.protocol = protocol
+        self.address = address
+        self.port = port
+        self.next_state = next_state
+
+    @classmethod
+    def decode(self, buf: Buffer) -> HandshakeHandshake:
+        return HandshakeHandshake(
+            protocol=buf.unpack_varint(),
+            address=buf.unpack_string(),
+            port=buf.unpack('H'),
+            next=buf.unpack_varint()
+        )

2e8a6d86a36bf46ddb34d8f83ed771079f2d1672
Iapetus-11 <iapetus011@gmail.com>
2020-12-21 23:36:55 -0500

    update args

--- src/types/packets/handshaking/handshake.py
+++ src/types/packets/handshaking/handshake.py
@@ -2,9 +2,9 @@ from src.types.packet import Packet
 
 __all__ = ('HandshakeHandshake',)
 
-class HandshakeHandshake(Packet):  # Serverbound (client -> server) only
+class HandshakeHandshake(Packet):  #  client TO server (Serverbound) ONLY
     def __init__(self, buf: bytes, comp_thresh = -1) -> None:
-        super().__init__(0x00, buf, comp_thresh)
+        super().__init__(0x00, 'server', buf, comp_thresh)
 
         self.protocol = super().unpack_varint()
         self.address = super().unpack_string()

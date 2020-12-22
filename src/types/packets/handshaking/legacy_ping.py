d91320f8a7bd4ff16709323f972d476a07048831
Iapetus-11 <iapetus011@gmail.com>
2020-12-22 01:04:04 -0500

    add legacy server list ping packets

--- src/types/packets/handshaking/legacy_ping.py
+++ src/types/packets/handshaking/legacy_ping.py
@@ -1,18 +1,33 @@
+from __future__ import annotations
+
+from src.types.buffer import Buffer
 from src.types.packet import Packet
 
 __all__ = ('HandshakeLegacyPing',)
 
-class HandshakeLegacyPingToServer(Packet):
-    def __init__(self, buf: bytes):
-        super.__init__(-0x1, buf)
+class HandshakeLegacyPing_1(Packet):  # Client -> Server
+    def __init__(self, protocol: int, hostname: str, port: int):
+        super.__init__(-0x1)
 
-        assert self.buf.read(1) == b'\xFE'  # required for legacy slp to be valid
+        self.protocol = protocol
+        self.hostname = hostname
+        self.port = port
 
-        self.buf.read(15)  # Read constant (unecessary) data
+    @classmethod
+    def decode(cls, buf: Buffer) -> HandshakeHandshake:
+        buf.read(15)
+        return cls(buf.read(1), buf.read(buf.unpack('h')).decode('UTF-16BE'), buf.unpack('i'))
 
-        self.protocol = self.buf.read(1)
-        self.hostname = self.read(self.unpack('h'))  # Encoded in UTF-16BE
-        self.port = self.unpack('i')
+class HandshakeLegacyPing_2(Packet):  # Server -> CLient
+    def __init__(self, protocol: int = 127, version: str, motd: str, player_count: int, max_players: int):
+        super.__init__(-0x1)
 
-class HandshakeLegacyPingToClient(Packet):
-    def __init__(self, )
+        self.protocol = protocol
+        self.version = version
+        self.motd = motd
+        self.player_count = player_count
+        self.max_players = max_players
+
+    def encode(self):
+        out_string = f'§1\x00{self.protocol}\x00{self.motd}\x00{self.player_count}\x00{self.max_players}'
+        return b'\xff' + Buffer.pack('h', len(out_string)) + out_string.encode('UTF-16BE')

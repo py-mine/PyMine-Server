f90205f4fb1c9fa0997df5fed2fec18a431c06d1
Iapetus-11 <iapetus011@gmail.com>
2020-12-21 22:52:18 -0500

    restructure

--- src/types/packet.py
+++ src/types/packet.py
@@ -6,14 +6,14 @@ from src.types.buffer import Buffer
 class Packet(Buffer):
     """Base packet class."""
 
-    def __init__(self, compression_threshold: int = -1):
-        Buffer.__init__(self)
+    def __init__(self, id: int, buf: bytes = None, comp_thresh: int = -1):
+        Buffer.__init__(self, buf)
 
         self.id = -0x1
-        self.compression_threshold = compression_threshold
+        self.comp_thresh = comp_thresh
 
     @classmethod
-    def from_bytes(cls, data: bytes, comp_thresh: int = -1):
+    def from_bytes(cls, data: bytes, comp_thresh: int = -1) -> Packet:
         out = Buffer.from_bytes(data, comp_thresh)
 
         self.id = self.unpack_varint()

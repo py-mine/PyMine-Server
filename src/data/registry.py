9181fb892c792c97242ce6d213d2abc52b260610
Iapetus-11 <iapetus011@gmail.com>
2020-12-24 17:14:22 -0500

    attempt to fix unhashable type errors in registry.py

--- src/data/registry.py
+++ src/data/registry.py
@@ -9,5 +9,5 @@ __all__ = (
 with open('src/data/registries.json') as registry:  # generated from server jar
     REGISTRY = json.load(registry)
 
-ITEMS_BY_NAME = REGISTRY['minecraft:item']['entries']
+ITEMS_BY_NAME = {k: tuple(v) for k, v in REGISTRY['minecraft:item']['entries'].items()}
 ITEMS_BY_ID = {v: k for k, v in ITEMS_BY_NAME.items()}

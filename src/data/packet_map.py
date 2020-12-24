b3c022671ff59889e1f3767cb0ed803d808d452f
Iapetus-11 <iapetus011@gmail.com>
2020-12-24 17:00:50 -0500

    actually call load_packets() in packet_map.py

--- src/data/packet_map.py
+++ src/data/packet_map.py
@@ -16,3 +16,5 @@ def load_packets():
                 PACKET_MAP[state].append(module.__dict__.get(name))
 
         PACKET_MAP[state] = tuple(sorted(PACKET_MAP[state], key=(lambda p: p.id)))
+
+load_packets()

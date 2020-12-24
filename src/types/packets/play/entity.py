bb796e2314b3202f45b8ec9d6079c087e604ae45
Sh-wayz <shwayz03@gmail.com>
2020-12-24 10:37:01 -0500

    add entity.py, PlayBlockEntityData

---
+++ src/types/packets/play/entity.py
@@ -0,0 +1,15 @@
+"""Contains packets related to entitys."""
+
+from __future__ import annotations
+
+from src.types.base import PacketClientboundJSON
+
+
+__all__ = ('PlayBlockEntityData',)
+
+
+class PlayBlockEntityData(PacketClientboundJSON):
+    """Sets the block entity associated with the block at the given location. Client bound(Client -> Server)."""
+
+    def __init__(self, response_data: dict) -> None:
+        super.__init__(0x09)

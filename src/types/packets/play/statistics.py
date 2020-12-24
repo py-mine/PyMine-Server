56b99991699460b2d8838655830dfd6928849002
Sh-wayz <shwayz03@gmail.com>
2020-12-24 09:26:09 -0500

    update PlayStatistics

--- src/types/packets/play/statistics.py
+++ src/types/packets/play/statistics.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from src.types.base import PacketClientboundJSON
 from src.types.buffer import Buffer
 from src.types.packet import Packet
 
@@ -9,11 +10,8 @@ from src.types.packet import Packet
 __all__ = ('PlayStatistics',)
 
 
-class PlayStatistics(Packet):
+class PlayStatistics(PacketClientboundJSON):
     """Sent as a response to Client Status 0x04 (id 1). Will only send the changed values if previously requested. Client bound(Client -> Server)."""
 
     def __init__(self, response_data: dict) -> None:
         super.__init__(0x06)
-
-    def encode(self):
-        return Buffer.pack_json(self.response_data)

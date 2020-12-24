cc98bf4ea3f87fdca7ed817db786c05fc5e9569e
Iapetus-11 <iapetus011@gmail.com>
2020-12-24 17:15:06 -0500

    remove .py from end of file to import

--- src/data/packet_map.py
+++ src/data/packet_map.py
@@ -10,7 +10,7 @@ def load_packets():
         PACKET_MAP[state] = []
 
         for file in os.listdir(f'src/types/packets/{state}'):
-            module = importlib.import_module(f'src.types.packets.{state}.{file}')
+            module = importlib.import_module(f'src.types.packets.{state}.{file.rstrip(".py")}')
 
             for name in module.__all__:
                 PACKET_MAP[state].append(module.__dict__.get(name))

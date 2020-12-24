1b6ae23d2cf58ba1f75063a5e7ae28988c39aeb4
Sh-wayz <shwayz03@gmail.com>
2020-12-24 14:05:30 -0500

    create boss.py, PlayBossBar

---
+++ src/types/packets/play/boss.py
@@ -0,0 +1,15 @@
+"""Contains packets related to bosses."""
+
+from __future__ import annotations
+
+from src.types.base import PacketClientboundJSON
+
+
+__all__ = ('PlayBossBar',)
+
+
+class PlayBossBar(PacketClientboundJSON):
+    """Has boss bar stuff in it. Client bound(Client -> Server)."""
+
+    def __init__(self, response_data: dict) -> None:
+        super.__init__(0x0C)

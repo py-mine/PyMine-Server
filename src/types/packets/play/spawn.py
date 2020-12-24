d493e8ae88d35d3bc41fd2f5df4a2e88fb03e958
Iapetus-11 <iapetus011@gmail.com>
2020-12-24 17:45:17 -0500

    update packets in spawn.py

--- src/types/packets/play/spawn.py
+++ src/types/packets/play/spawn.py
@@ -11,26 +11,34 @@ __all__ = ('PlayEntitySpawn', 'PlayLivingEntitySpawn',
 class PlayEntitySpawn(PacketClientboundJSON):
     """Sent by the server when a vehicle or other non-living entity is created. Client bound(Client -> Server)."""
 
+    id_ = 0x00
+
     def __init__(self, response_data: dict) -> None:
-        super().__init__(0x00)
+        super().__init__()
 
 
 class PlayLivingEntitySpawn(PacketClientboundJSON):
     """Sent by the server when a living entity is spawned.  Client bound(Client -> Server)."""
 
+    id_ = 0x02
+
     def __init__(self, response_data: dict) -> None:
-        super().__init__(0x02)
+        super().__init__()
 
 
 class PlayPaintingSpawn(PacketClientboundJSON):
     """This packet shows location, name, and type of painting.  Client bound(Client -> Server)."""
 
+    id_ = 0x03
+
     def __init__(self, response_data: dict) -> None:
-        super().__init__(0x03)
+        super().__init__()
 
 
 class PlaySpawnExperienceOrb(PacketClientboundJSON):
     """Spawns one or more experience orbs. Client bound(Client -> Server)."""
 
+    id_ = 0x01
+
     def __init__(self, response_data: dict) -> None:
-        super().__init__(0x01)
+        super().__init__()

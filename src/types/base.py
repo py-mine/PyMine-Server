7a31ec83b0e38ada9c933f1fa66b7614cb9c60c1
Sh-wayz <shwayz03@gmail.com>
2020-12-24 09:37:16 -0500

    add __init__ to PacketClientboundJSON

--- src/types/base.py
+++ src/types/base.py
@@ -10,5 +10,8 @@ __all__ = ('PacketClientboundJSON',)
 class PacketClientboundJSON(Packet):
     """Base class used in most client-bound play packets."""
 
+    def __init__(self, id: int):
+        super.__init__(id)
+
     def encode(self):
         return Buffer.pack_json(self.response_data)

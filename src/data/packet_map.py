78e0327c33867f80778ac5f6cbd4ecee46a5083a
Iapetus-11 <iapetus011@gmail.com>
2020-12-24 17:09:11 -0500

    attempt to fix import logic

--- src/data/packet_map.py
+++ src/data/packet_map.py
@@ -5,16 +5,13 @@ __all__ = ('PACKET_MAP',)
 
 PACKET_MAP = {}  # {state: (packet, packet,..),..}
 
-def load_packets():
-    for state in os.listdir('src/types/packets'):
-        PACKET_MAP[state] = []
+for state in os.listdir('src/types/packets'):
+    PACKET_MAP[state] = []
 
-        for file in os.listdir(f'src/types/packets/{state}'):
-            module = importlib.import_module(f'src.types.packets.{state}.{file}')
+    for file in os.listdir(f'src/types/packets/{state}'):
+        module_all = __import__(f'src.types.packets.{state}.{file}', fromlist=('__all__',))
 
-            for name in module.__all__:
-                PACKET_MAP[state].append(module.__dict__.get(name))
+        for name in module_all:
+            PACKET_MAP[state] += __import__(f'src.types.packets.{state}.{file}', fromlist=module_all)
 
-        PACKET_MAP[state] = tuple(sorted(PACKET_MAP[state], key=(lambda p: p.id)))
-
-load_packets()
+    PACKET_MAP[state] = tuple(sorted(PACKET_MAP[state], key=(lambda p: p.id)))

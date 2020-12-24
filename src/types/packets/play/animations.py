53fc11f846c7f41778c25c0c918b58349c0b8081
Sh-wayz <shwayz03@gmail.com>
2020-12-24 09:13:09 -0500

    type fixes

--- src/types/packets/play/animations.py
+++ src/types/packets/play/animations.py
@@ -1,14 +1,16 @@
+"""Contains animation packets"""
+
 from __future__ import annotations
 
 from src.types.buffer import Buffer
 from src.types.packet import Packet
-"""Contains animation packets"""
+
 
 __all__ = ('PlayEntityAnimation',)
 
 
 class PlayEntityAnimation(Packet):
-    """Sent whenever an entity should change animation. sClient bound(Client -> Server)."""
+    """Sent whenever an entity should change animation. Client bound(Client -> Server)."""
 
     def __init__(self, response_data: dict) -> None:
         super.__init__(0x05)

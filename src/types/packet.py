6795d8d63f6b5d252852291cb2d9875054eb6b07
Iapetus-11 <iapetus011@gmail.com>
2020-12-24 17:21:07 -0500

    move PacketClientboundJSON to packet.py

--- src/types/packet.py
+++ src/types/packet.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from src.types.buffer import Buffer
+
+__all__ = ('Packet', '')
 
 class Packet:
     """Base Packet class.
@@ -8,5 +11,19 @@ class Packet:
     :attr id:
     """
 
+    id_ = None
+
     def __init__(self, id_: int = -0x1) -> None:
         self.id_ = id_
+
+
+class PacketClientboundJSON(Packet):
+    """Base class used in most client-bound play packets."""
+
+    id_ = None
+
+    def __init__(self, id_: int):
+        super().__init__(id_)
+
+    def encode(self):
+        return Buffer.pack_json(self.response_data)  # assumes response_data is present

a0633646ed6f2cdc4cf9fe417800c9ef00ee6280
Iapetus-11 <iapetus011@gmail.com>
2020-12-25 16:11:35 -0500

    add STATES_BY_ID and STATES_BY_NAME in states.py

---
+++ src/data/states.py
@@ -0,0 +1,12 @@
+from immutables import Map
+
+__all__ = ('STATES_BY_ID', 'STATES_BY_NAME',)
+
+STATES_BY_ID = (
+    'handshake',
+    'status',
+    'login',
+    'play',
+)
+
+STATES_BY_NAME = Map({v: i for i, v in enumerate(STATES_BY_ID)})

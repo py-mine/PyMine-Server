48302e35fc477d3d8a5e7d3f4f177ed6fdf99ceb
Iapetus-11 <iapetus011@gmail.com>
2020-12-24 19:02:40 -0500

    fancy shmancy docstrings for packets in status.py

--- src/types/packets/status/status.py
+++ src/types/packets/status/status.py
@@ -7,6 +7,11 @@ __all__ = ('StatusStatusRequest', 'StatusStatusResponse', 'StatusStatusPingPong'
 
 
 class StatusStatusRequest(Packet):
+    """Request from the client to get information on the server. (Client -> Server)
+
+    :attr type id_: Unique packet ID.
+    """
+
     id_ = 0x00
 
     def __init__(self) -> None:
@@ -18,6 +23,13 @@ class StatusStatusRequest(Packet):
 
 
 class StatusStatusResponse(Packet):
+    """Returns server status data back to the requesting client. (Server -> Client)
+
+    :param dict response_data: JSON response data sent back to the client.
+    :attr type id_: Unique packet ID.
+    :attr response_data:
+    """
+
     id_ = 0x00
 
     def __init__(self, response_data: dict) -> None:

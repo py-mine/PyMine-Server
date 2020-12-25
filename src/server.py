6da76bdf049742d1c496722adaa397ff0f3d9f59
Iapetus-11 <iapetus011@gmail.com>
2020-12-25 16:17:58 -0500

    assume client will send EOF

--- src/server.py
+++ src/server.py
@@ -9,9 +9,11 @@ states = {}  # {remote_address: state_id}
 
 
 async def handle_con(r, w):
-    buf = Buffer(await r.read(5))  # Varint is no longer than 5 bytes, so 1st 5 are always required
-    buf.add(await r.read(buf.unpack_varint()))  # Read the rest of the packet
-    buf.reset()  # Reset position in buf back to 0
+    # buf = Buffer(await r.read(5))  # Varint is no longer than 5 bytes, so 1st 5 are always required
+    # buf.add(await r.read(buf.unpack_varint()))  # Read the rest of the packet
+    # buf.reset()  # Reset position in buf back to 0
+
+    buf = Buffer(await r.read())
 
     remote_address = w.get_extra_info('peername')
     packet = buf.unpack_packet(STATES_BY_ID[states.get(remote_address, 0)])

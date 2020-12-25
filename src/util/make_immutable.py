21089e749aebd9d4e64eddcb73a01994535861e3
Iapetus-11 <iapetus011@gmail.com>
2020-12-25 13:02:42 -0500

    add make_immutable(obj) to recursively make data structures immutable

---
+++ src/util/make_immutable.py
@@ -0,0 +1,22 @@
+from immutables import Map
+
+def make_immutable(obj: object) -> object:
+    """Recursively converts lists to tuples and dictionaries to maps.
+
+    :param object obj: A list or dict object.
+    :return: Should be an immutable list or dict, or whatever else put in.
+    :rtype: object
+    """
+
+    if isinstance(obj, dict):
+        for k, v in obj.items():
+            obj[k] = make_immutable(v)
+
+        return Map(obj)
+    elif isinstance(obj, list):
+        for i, v in enumerate(obj):
+            obj[i] = make_immutable(v)
+
+        return tuple(obj)
+
+    return obj

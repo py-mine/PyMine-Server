1de91b1d8d45a7a0b66f092ad67d391461ec188c
Iapetus-11 <iapetus011@gmail.com>
2020-12-24 22:34:23 -0500

    add encode to PlayBlockAction

--- src/types/packets/play/block.py
+++ src/types/packets/play/block.py
@@ -8,14 +8,29 @@ __all__ = ('PlayBlockAction', 'PlayBlockChange',)
 
 
 class PlayBlockAction(Packet):
-    """This packet is used for a number of actions and animations performed by blocks, usually non-persistent.
-    Client bound(Client -> Server).
+    """This packet is used for a number of actions and animations performed by blocks. (Server -> Client)
+
+    :param dict response_data: Description of parameter `response_data`.
+    :attr type id_: Description of parameter `id_`.
     """
 
     id_ = 0x0A
 
-    def __init__(self, response_data: dict) -> None:
+    def __init__(self, x: int, y: int, z: int, action_id: int, action_param: int, block_type: int) -> None:
         super().__init__()
+
+        self.x = x
+        self.y = y
+        self.z = z
+        self.action_id = action_id
+        self.action_param = action_param
+        self.block_type = block_type
+
+    def encode(self) -> bytes:
+        return Buffer.pack_pos(self.x, self.y, self.z) + \
+         Buffer.pack('B', self.action_id) + \
+         Buffer.pack('B', self.action_param) + \
+         Buffer.pack_varint(self.block_type)
 
 
 class PlayBlockChange(Packet):

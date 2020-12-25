080751ebee2d1ce40def9a026ab4cc63272ae3ef
Iapetus-11 <iapetus011@gmail.com>
2020-12-25 16:16:04 -0500

    add basic packet getting logic

--- src/server.py
+++ src/server.py
@@ -3,9 +3,9 @@ import uvloop
 
 from src.data.packet_map import PACKET_MAP
 from src.types.buffer import Buffer
+from src.data.states import *
 
-
-async def status(r, w):
+states = {}  # {remote_address: state_id}
 
 
 async def handle_con(r, w):
@@ -13,8 +13,11 @@ async def handle_con(r, w):
     buf.add(await r.read(buf.unpack_varint()))  # Read the rest of the packet
     buf.reset()  # Reset position in buf back to 0
 
-    packet = buf.unpack_packet()
+    remote_address = w.get_extra_info('peername')
+    packet = buf.unpack_packet(STATES_BY_ID[states.get(remote_address, 0)])
 
+    print(type(packet))
+    print(packet.__dict__)
 
 async def start():
     server = await asyncio.start_server(handle_con, port=69)

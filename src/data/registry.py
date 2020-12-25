06943b8802aa47715c9e0b4eb1ac200cbf51e562
Iapetus-11 <iapetus011@gmail.com>
2020-12-25 12:20:15 -0500

    Fix unhashable type errors and structure in registry.py

--- src/data/registry.py
+++ src/data/registry.py
@@ -9,8 +9,8 @@ __all__ = (
 with open('src/data/registries.json') as registry:  # generated from server jar
     REGISTRY = json.load(registry)
 
-ITEMS_BY_NAME = {k: tuple(v) for k, v in REGISTRY['minecraft:item']['entries'].items()}
-ITEMS_BY_ID = {v: k for k, v in ITEMS_BY_NAME.items()}
+ITEMS_BY_NAME = REGISTRY['minecraft:item']['entries']
+ITEMS_BY_ID = {v['protocol_id']: k for k, v in ITEMS_BY_NAME.items()}
 
 PARTICLES_BY_NAME = REGISTRY['minecraft:particle_type']['entries']
-PARTICLES_BY_ID = {v: k for k, v in PARTICLES_BY_NAME.items()}
+PARTICLES_BY_ID = {v['protocol_id']: k for k, v in PARTICLES_BY_NAME.items()}

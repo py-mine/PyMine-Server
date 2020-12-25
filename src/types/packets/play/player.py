e31a288ea352cb5ce7acbbaf826d86c102e19d25
Iapetus-11 <iapetus011@gmail.com>
2020-12-25 13:54:52 -0500

    finish rewrite of PlayAcknowledgePlayerDigging

--- src/types/packets/play/player.py
+++ src/types/packets/play/player.py
@@ -3,14 +3,43 @@
 from __future__ import annotations
 
 from src.types.packet import Packet
+from src.types.buffer import Buffer
 
 __all__ = ('PlayAcknowledgePlayerDigging',)
 
 
 class PlayAcknowledgePlayerDigging(Packet):
-    """Acknowledges player digging. Client bound(Client -> Server)."""
+    """Sent by server to acknowledge player digging. (Server -> Client)
+
+    :param int x: The x coordinate of where the player is digging.
+    :param int y: The y coordinate of where the player is digging.
+    :param int z: The z coordinate of where the player is digging.
+    :param int block: The block state id of the block that is being broken/dug.
+    :param int status: Value 0-2 to denote whether player should start, cancel, or finish.
+    :param bool successful: True if the block was dug successfully.
+    :attr type id_: Unique packet ID.
+    :attr x:
+    :attr y:
+    :attr z:
+    :attr block:
+    :attr status:
+    :attr successful:
+    """
 
     id_ = 0x07
 
-    def __init__(self, response_data: dict) -> None:
+    def __init__(self, x: int, y: int, z: int, block: int, status: int, successful: bool) -> None:
         super().__init__()
+
+        self.x = x
+        self.y = y
+        self.z = z
+        self.block = block
+        self.status = status
+        self.successful = successful
+
+    def encode(self):
+        return Buffer.pack_pos(self.x, self.y, self.z) + \
+            Buffer.pack_varint(self.block) + \
+            Buffer.pack_varint(self.status) + \
+            Buffer.pack_bool(self.successful)

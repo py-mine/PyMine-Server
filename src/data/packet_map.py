553e95fc75e524c69246105fd4746d91c02fc182
Iapetus-11 <iapetus011@gmail.com>
2020-12-25 13:14:26 -0500

    change how packets are stored within their states

--- src/data/packet_map.py
+++ src/data/packet_map.py
@@ -10,14 +10,15 @@ def load_packets():
     packet_map = {}
 
     for state in os.listdir('src/types/packets'):
-        packet_map[state] = []
+        packet_map[state] = {}
 
         for file in os.listdir(f'src/types/packets/{state}'):
             if file.endswith('.py'):
                 module = importlib.import_module(f'src.types.packets.{state}.{file[:-3]}')
 
                 for name in module.__all__:
-                    packet_map[state].append(module.__dict__.get(name))
+                    packet = module.__dict__.get(name)
+                    packet_map[state][packet.id_] = packet
 
         packet_map[state] = sorted(packet_map[state], key=(lambda p: p.id_))
 

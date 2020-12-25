924e90152c561c788da9ca269434e0191ebf3412
Iapetus-11 <iapetus011@gmail.com>
2020-12-25 12:57:13 -0500

    convert dicts in registry.py to immutable maps

--- src/data/registry.py
+++ src/data/registry.py
@@ -1,3 +1,4 @@
+from immutables import Map
 import json
 
 __all__ = (
@@ -9,8 +10,8 @@ __all__ = (
 with open('src/data/registries.json') as registry:  # generated from server jar
     REGISTRY = json.load(registry)
 
-ITEMS_BY_NAME = {k: v['protocol_id'] for k, v in REGISTRY['minecraft:item']['entries'].items()}
-ITEMS_BY_ID = {v: k for k, v in ITEMS_BY_NAME.items()}
+ITEMS_BY_NAME = Map({k: v['protocol_id'] for k, v in REGISTRY['minecraft:item']['entries'].items()})
+ITEMS_BY_ID = Map({v: k for k, v in ITEMS_BY_NAME.items()})
 
-PARTICLES_BY_NAME = {k: v['protocol_id'] for k, v in REGISTRY['minecraft:particle_type']['entries'].items()}
-PARTICLES_BY_ID = {v: k for k, v in PARTICLES_BY_NAME.items()}
+PARTICLES_BY_NAME = Map({k: v['protocol_id'] for k, v in REGISTRY['minecraft:particle_type']['entries'].items()})
+PARTICLES_BY_ID = Map({v: k for k, v in PARTICLES_BY_NAME.items()})

e78eb09b37792402be28228a60d658f10b3479bc
Iapetus-11 <iapetus011@gmail.com>
2020-12-24 22:48:36 -0500

    finish PlayBlockChange

--- src/types/packets/play/block.py
+++ src/types/packets/play/block.py
@@ -13,7 +13,7 @@ class PlayBlockAction(Packet):
 
     :param int x: The x coordinate of the location where this occurs.
     :param int y: The y coordinate of the location where this occurs.
-    :param int z: The z coordinate of the location where this occurs..
+    :param int z: The z coordinate of the location where this occurs.
     :param int action_id: Block action ID, see here: https://wiki.vg/Block_Actions.
     :param int action_param: Action param of the action, see here: https://wiki.vg/Block_Actions.
     :param int block_type: The type of block which the action is for.
@@ -46,9 +46,28 @@ class PlayBlockAction(Packet):
 
 
 class PlayBlockChange(Packet):
-    """Fired when a block is changed within the render distance. Client bound(Client -> Server)."""
+    """Fired when a block is changed within the render distance. (Server -> Client)
+
+    :param int x: The x coordinate of the location where this occurs.
+    :param int y: The y coordinate of the location where this occurs.
+    :param int z: The z coordinate of the location where this occurs.
+    :param int block_id: Block ID of what to change the block to.
+    :attr type id_: Unique packet ID.
+    :attr x:
+    :attr y:
+    :attr z:
+    :attr block_id:
+    """
 
     id_ = 0x0B
 
-    def __init__(self, response_data: dict) -> None:
+    def __init__(self, x: int, y: int, z: int, block_id: int) -> None:
         super().__init__()
+
+        self.x = x
+        self.y = y
+        self.z = z
+        self.block_id = block_id
+
+    def encode(self) -> bytes:
+        return Buffer.pack_pos(self.x, self.y, self.z) + Buffer.pack_varint(self.block_id)

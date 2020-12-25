bc401491256bede82229afdd838f215ccb76cc15
Iapetus-11 <iapetus011@gmail.com>
2020-12-25 17:04:26 -0500

    add logging + fix some things

--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+import logging
 import asyncio
 import sys
 import os
@@ -12,15 +13,20 @@ from src.data.states import *
 
 states = {}  # {remote_address: state_id}
 
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 async def handle_con(r, w):
-    # buf = Buffer(await r.read(5))  # Varint is no longer than 5 bytes, so 1st 5 are always required
-    # buf.add(await r.read(buf.unpack_varint()))  # Read the rest of the packet
-    # buf.reset()  # Reset position in buf back to 0
+    remote_address = w.get_extra_info('peername')
+    logger.info(f'Connection received from {remote_address}')
+
+    buf = Buffer(await r.read(5))  # Varint is no longer than 5 bytes, so 1st 5 are always required
+    buf.write(await r.read(buf.unpack_varint()))  # Read the rest of the packet
+    buf.reset()  # Reset position in buf back to 0
+
 
     buf = Buffer(await r.read())
-
-    remote_address = w.get_extra_info('peername')
     packet = buf.unpack_packet(STATES_BY_ID[states.get(remote_address, 0)])
 
     print(type(packet))

cca844aa5e2df39342effa7593c734b34fc18dcb
Iapetus-11 <iapetus011@gmail.com>
2020-12-26 12:26:24 -0500

    add favicon to config.py

--- src/data/config.py
+++ src/data/config.py
@@ -1,7 +1,8 @@
 from immutables import Map
+import base64
 import yaml
 
-__all__ = ('SERVER_PROPERTIES_DEFAULT', 'SERVER_PROPERTIES',)
+__all__ = ('SERVER_PROPERTIES_DEFAULT', 'SERVER_PROPERTIES', 'FAVICON',)
 
 SERVER_PROPERTIES_DEFAULT = Map({
     'debug': True,
@@ -33,3 +34,12 @@ def load_properties():
         return SERVER_PROPERTIES_DEFAULT
 
 SERVER_PROPERTIES = load_properties()
+
+def load_favicon():
+    try:
+        with open('server-icon.png', 'rb') as favicon:
+            return 'data:image/png;base64,' + base64.b64encode(favicon.read()).decode('utf-8')
+    except FileNotFoundError:
+        return None
+
+FAVICON = load_favicon()

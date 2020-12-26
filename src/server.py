70aeed52b057c45aa992ee06a9e49532c7bad68e
Iapetus-11 <iapetus011@gmail.com>
2020-12-26 12:51:58 -0500

    add missing comma

--- src/server.py
+++ src/server.py
@@ -26,7 +26,7 @@ share = {
     'protocol': 754,
     'timeout': .15,
     'rsa': {  # https://stackoverflow.com/questions/54495255/python-cryptography-export-key-to-der
-        'private': rsa.generate_private_key()
+        'private': rsa.generate_private_key(),
         'public': None
     },
     'properties': SERVER_PROPERTIES,

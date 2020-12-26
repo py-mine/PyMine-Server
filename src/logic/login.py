eb10d6dad00b23f388a9e77ff0ab83406fbb77b5
Iapetus-11 <iapetus011@gmail.com>
2020-12-26 13:29:54 -0500

    finish request_encryption() in login logic

--- src/logic/login.py
+++ src/logic/login.py
@@ -1,9 +1,19 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
 import aiohttp
 
 from src.types.packets.login.login import *
 from src.types.buffer import Buffer
 
 
-async def request_encryption(r: 'StreamReader', w: 'StreamWriter', packet: 'LoginStart'):
-    w.write(Buffer.pack_packet(LoginEncryptionRequest()))
+ses = aiohttp.ClientSession()
+
+async def request_encryption(r: 'StreamReader', w: 'StreamWriter', packet: 'LoginStart', share: dict):
+    w.write(Buffer.pack_packet(LoginEncryptionRequest(
+        share['public_key'].public_bytes(
+            encoding=serialization.Encoding.DER,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+    )))
+
+    await w.drain()

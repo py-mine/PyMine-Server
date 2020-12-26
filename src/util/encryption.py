4da16a00e35209688df2a5ade9b9330fcee29da0
Iapetus-11 <Iapetus-11@users.noreply.github.com>
2020-12-26 21:50:26 +0000

    autopep8 action fixes

--- src/util/encryption.py
+++ src/util/encryption.py
@@ -6,7 +6,7 @@ __all__ = ('generate_verify_hash',)
 def generate_verify_hash(shared_secret: bytes, public_key: bytes):
     verify_hash = hashlib.sha1()
 
-    verify_hash.update((' '*20).encode('utf-8'))
+    verify_hash.update((' ' * 20).encode('utf-8'))
     verify_hash.update(shared_secret)
     verify_hash.update(public_key)
 

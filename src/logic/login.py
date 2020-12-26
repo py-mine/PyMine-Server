d688d2f5e951652e9edcf4e0c9406e557bb15ab1
Iapetus-11 <iapetus011@gmail.com>
2020-12-26 14:05:21 -0500

    more login functions

--- src/logic/login.py
+++ src/logic/login.py
@@ -5,6 +5,10 @@ import aiohttp
 from src.types.packets.login.login import *
 from src.types.buffer import Buffer
 
+from src.util.encryption import *
+
+# https://github.com/ammaraskar/pyCraft/blob/master/minecraft/networking/encryption.py
+
 ses = aiohttp.ClientSession()
 
 
@@ -16,4 +20,27 @@ async def request_encryption(r: 'StreamReader', w: 'StreamWriter', packet: 'Logi
         )
     )))
 
+    await w.drain()
+
+
+async def server_auth(packet: 'LoginEncryptionResponse', remote: tuple, username: str, share: dict):
+    resp = await ses.get(
+        'https://sessionserver.mojang.com/session/minecraft/hasJoined?username=username&serverId=hash',
+        params={
+            'username': username,
+            'serverId': generate_verify_hash(
+                packet.shared_key,
+                share['public_key'].public_bytes(
+                    encoding=serialization.Encoding.DER,
+                    format=serialization.PublicFormat.SubjectPublicKeyInfo
+                )
+            )
+        }
+    )
+
+    return uuid.
+
+
+async def login_success(r: 'StreamReader', w: 'StreamWriter', uuid: 'uuid.UUID', username: str):
+    w.write(Buffer.pack_packet(LoginSuccess(uuid, username)))
     await w.drain()

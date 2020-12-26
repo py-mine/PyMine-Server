d950d5ec1f613842ae6b6548a4b30180834352f8
Sh-wayz <shwayz03@gmail.com>
2020-12-26 13:45:12 -0500

    create PLaySetCooldown

---
+++ src/types/packets/play/cooldown.py
@@ -0,0 +1,18 @@
+"""Contains the PlaySetCooldown packet"""
+from __future__ import annotations
+
+from src.types.packet import Packet
+from src.types.buffer import Buffer
+
+
+class PlaySetCooldown(Packet):
+    """Applies a cooldown period to all items with the given type."""
+    id = 0x16
+    to = 1
+
+    def __init__(self, item_id: int, cooldown_ticks: int):
+        self.item_id = item_id
+        self.cooldown_ticks = cooldown_ticks
+
+    def encode(self):
+        return Buffer.pack_varint(self.item_id) + Buffer.pack_varint(self.cooldown_ticks)

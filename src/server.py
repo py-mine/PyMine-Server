a02de07c2f1d4a790081ce845edf35c1502b942f
Iapetus-11 <iapetus011@gmail.com>
2020-12-26 16:05:07 -0500

    idk

--- src/server.py
+++ src/server.py
@@ -53,18 +53,33 @@ logger = logging.getLogger(__name__)
 share['logger'] = logger
 
 
+def print_buf(buf):
+    print(buf.buf)
+    print(' '*buf.pos + '^')
+
+
 async def handle_packet(r: asyncio.StreamReader, w: asyncio.StreamWriter, remote: tuple):
     buf = Buffer(await r.read(1))
 
     if buf.buf == b'\xFE':
-        #raise NotImplementedError('legacy ping is not currently supported.')
-        logger.warning('legacy ping is currently not supported.')
+        logger.warn('legacy ping is not supported yet.')
         return
 
-    buf.write(await r.read(await Buffer.unpack_varint_stream(r)))
+    try:
+        for _ in range(2):
+            buf.write(await asyncio.wait_for(r.read(1), share['timeout']))
+    except asyncio.TimeoutError:
+        pass
+
+    buf.write(await r.read(buf.unpack_varint()))
+
+    print_buf(buf)
 
     state = STATES_BY_ID[states.get(remote, 0)]
     packet = buf.unpack_packet(state, 0, PACKET_MAP)
+
+    print_buf(buf)
+    print(packet.__dict__)
 
     logger.debug(f'state:{state:<12} | id:{hex(packet.id_):<4} | packet:{type(packet).__name__}')
 

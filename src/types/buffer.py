e06f48daca8ce6fda5dbaee3ec238b484159c604
Iapetus-11 <iapetus011@gmail.com>
2020-12-26 15:41:09 -0500

    consistency changes

--- src/types/buffer.py
+++ src/types/buffer.py
@@ -63,13 +63,7 @@ class Buffer:
 
         return cls.pack_varint(len(data)) + data
 
-    def unpack_packet(
-            self,
-            state: str,
-            to: int,
-            PACKET_MAP: object,
-            comp_thresh: int = -
-            1) -> Packet:
+    def unpack_packet(self, state: str, to: int, PACKET_MAP: object, comp_thresh: int = -1) -> Packet:  # nopepe8
         data = self.buf
 
         if comp_thresh >= 0:

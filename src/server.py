5ac78269707d0e8c0db27dc684607a2801f88b6a
Iapetus-11 <olimdahacker@gmail.com>
2020-12-26 19:59:04 -0500

    specify RSABackend in call to generate_private_key()

--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+from cryptography.hazmat.backends.interfaces import RSABackend
 from cryptography.hazmat.primitives.asymmetric import rsa
 import immutables
 import logging
@@ -31,7 +32,7 @@ share = {
     'protocol': 754,
     'timeout': .15,
     'rsa': {  # https://stackoverflow.com/questions/54495255/python-cryptography-export-key-to-der
-        'private': rsa.generate_private_key(65537, 1024),
+        'private': rsa.generate_private_key(65537, 1024, backend=RSABackend),
         'public': None
     },
     'properties': SERVER_PROPERTIES,

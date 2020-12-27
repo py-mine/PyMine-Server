9d8918b6a6d8b2c2c50e643b10cb0fe68613dfff
Iapetus-11 <iapetus011@gmail.com>
2020-12-26 23:27:24 -0500

    update aioinput.py

--- src/util/aioinput.py
+++ src/util/aioinput.py
@@ -3,4 +3,4 @@ from concurrent.futures import ThreadPoolExecutor
 
 async def aioinput(prompt: str = '') -> str:
     with ThreadPoolExecutor() as pool:
-        return await asyncio.get_event_loop().run_in_executor(pool, (lambda: input(prompt)))
+        return await asyncio.run_in_executor(pool, (lambda: input(prompt)))

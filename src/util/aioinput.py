f4c51edafed3c7c31163da0765b2ddabce870839
Iapetus-11 <iapetus011@gmail.com>
2020-12-26 23:28:33 -0500

    use run_in_executor() properly

--- src/util/aioinput.py
+++ src/util/aioinput.py
@@ -4,4 +4,4 @@ import asyncio
 
 async def aioinput(prompt: str = '') -> str:
     with ThreadPoolExecutor() as pool:
-        return await asyncio.run_in_executor(pool, (lambda: input(prompt)))
+        return await asyncio.get_event_loop().run_in_executor(pool, (lambda: input(prompt)))

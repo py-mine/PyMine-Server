a64a83db7c02773ab3efbc291c2287f5cb325b42
Iapetus-11 <iapetus011@gmail.com>
2020-12-27 00:35:33 -0500

    remove prompt for commands for now

--- src/logic/commands.py
+++ src/logic/commands.py
@@ -4,6 +4,6 @@ from src.util.aioinput import aioinput
 async def handle_commands():
     try:
         while True:
-            print(await aioinput('>'))
+            print(await aioinput(''))
     except KeyboardInterrupt:
         pass

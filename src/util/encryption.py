d3131aaf435fea5afae60fb05bbe5e74ec1d1536
Iapetus-11 <iapetus011@gmail.com>
2020-12-27 15:40:08 -0500

    consistency changes

--- src/util/encryption.py
+++ src/util/encryption.py
@@ -4,18 +4,18 @@ import hashlib
 __all__ = ('gen_verify_hash', 'gen_aes_cipher',)
 
 
-def gen_verify_hash(shared_secret: bytes, public_key: bytes):
+def gen_verify_hash(shared_key: bytes, public_key: bytes):
     verify_hash = hashlib.sha1()
 
     verify_hash.update((' ' * 20).encode('utf-8'))
-    verify_hash.update(shared_secret)
+    verify_hash.update(shared_key)
     verify_hash.update(public_key)
 
     return format(int.from_bytes(verify_hash.digest(), byteorder='big', signed=True), 'x')
 
 
-def gen_aes_cipher(shared_secret: bytes):
+def gen_aes_cipher(shared_key: bytes):
     return Cipher(
-        algorithms.AES(shared_secret),
-        modes.CFB8(shared_secret)
+        algorithms.AES(shared_key),
+        modes.CFB8(shared_key)
     )

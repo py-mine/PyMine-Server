e1edebd2ef1ddc1727eb635d6bd5b3d7f08586bf
Iapetus-11 <olimdahacker@gmail.com>
2020-12-26 20:41:33 -0500

    add basic logger class with basic logging methods

---
+++ src/util/logging.py
@@ -0,0 +1,42 @@
+import time
+import os
+
+if os.name == 'nt':
+    import colorama
+    colorama.init()
+
+nice_time = (lambda: time.strftime('%x %H:%M:%S'))
+
+RESET = '\x1b[0m'
+WHITE = '\x1b[97m'
+GREY = '\x1b[37m'
+BLUE = '\x1b[34m'
+YELLOW = '\x1b[33m'
+RED = '\x1b[91m'
+BG_RED = '\x1b[41;1m'
+
+
+class Logger:
+    def __init__(self, debug: bool = True) -> None:
+        self.debug = debug
+
+    def debug(*message):
+        if self.debug:
+            message = ' '.join(str(m) for m in message)
+            print(f'{WHITE}[{nice_time()} {GREY}DEBUG{WHITE}]: {GREY}{message}{RESET}')
+
+    def info(*message):
+        message = ' '.join(str(m) for m in message)
+        print(f'{WHITE}[{nice_time()} {BLUE}INFO{WHITE}]: {message}{RESET}')
+
+    def warn(*message):
+        message = ' '.join(str(m) for m in message)
+        print(f'{WHITE}[{nice_time()} {YELLOW}WARNING{WHITE}]: {YELLOW}{message}{RESET}')
+
+    def error(*message):
+        message = ' '.join(str(m) for m in message)
+        print(f'{WHITE}[{nice_time()} {RED}ERROR{WHITE}]: {RED}{message}{RESET}')
+
+    def critical(*message):
+        message = ' '.join(str(m) for m in message)
+        print(f'{WHITE}{BG_RED}[{nice_time()} CRITICAL ERROR]: {message}{RESET}')

25aaebd4c991b0913d0053a5f128fcda443005e3
Iapetus-11 <iapetus011@gmail.com>
2020-12-27 16:39:52 -0500

    update failed auth login kick reason

--- src/logic/login.py
+++ src/logic/login.py
@@ -65,6 +65,6 @@ async def login_success(r: 'StreamReader', w: 'StreamWriter', username: str, uui
 
 async def login_kick(w: 'StreamWriter'):
     w.write(Buffer.pack_packet(
-        LoginKick('You must be on a legit client and logged in to join this server.')
+        LoginKick('Failed to authenticate your connection.')
     ))
     await w.drain()

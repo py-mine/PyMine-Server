9b648ca0ba860bf62dd3fab7d5e06db9e17bd012
Iapetus-11 <iapetus011@gmail.com>
2020-12-26 23:25:15 -0500

    add aioinput.py for async input() calls

---
+++ src/util/aioinput.py
@@ -0,0 +1,6 @@
+from concurrent.futures import ThreadPoolExecutor
+
+
+async def aioinput(prompt: str = '') -> str:
+    with ThreadPoolExecutor() as pool:
+        return await asyncio.get_event_loop().run_in_executor(pool, (lambda: input(prompt)))

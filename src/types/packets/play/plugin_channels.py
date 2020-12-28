53be721986468abdef7015f48627a9305925a358
Iapetus-11 <iapetus011@gmail.com>
2020-12-28 16:01:30 -0500

    add plugin channel packets

---
+++ src/types/packets/play/plugin_channels.py
@@ -0,0 +1,37 @@
+
+from src.types.packet import Packet
+from src.types.buffer import Buffer
+
+class PlayPluginMessageClientBound(Packet):
+    """Used to send a "plugin message". See here https://wiki.vg/Protocol#Plugin_Message_.28serverbound.29 (Server -> Client)
+
+    :param bytes data: Data to be sent to the client.
+    :attr int id_: Unique packet ID.
+    :attr int to: Packet direction.
+    :attr data:
+    """
+
+    id_ = 0x17
+    to = 1
+
+    def __init__(self, data: bytes) -> None:
+        super().__init__()
+
+        self.data = data
+
+    def encode(self) -> bytes:
+        return data
+
+
+# class PlayPluginMessageServerBound(Packet):
+#
+#     id_ = 0x0B
+#     to = 0
+#
+#     def __init__(self, data: bytes) -> None:
+#         super().__init__()
+#
+#         self.data = data
+#
+#     def decode(self, buf: Buffer) -> PlayPluginMessageServerBound:
+#         raise NotImplementedError

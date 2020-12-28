3a32b150f0d20bd7eb0492069db7b5a1a79d1e83
Iapetus-11 <iapetus011@gmail.com>
2020-12-28 00:18:11 -0500

    attempt to fix lan support (use UDP not TCP for connection)

--- src/logic/lan_support.py
+++ src/logic/lan_support.py
@@ -1,3 +1,4 @@
+import asyncio_dgram
 import asyncio
 
 from src.util.share import share
@@ -6,16 +7,14 @@ from src.util.share import share
 async def ping_lan():
     while True:
         try:
-            r, w, = await asyncio.wait_for(
-                asyncio.open_connection(
-                    host='224.0.2.60',
-                    port=4445
-                ),
-                .5
+            stream = await asyncio.wait_for(asyncio_dgram.connect(('224.0.2.60', 4445)), .5)
+
+            await stream.send(
+                f'[MOTD]{share["conf"]["motd"]}[/MOTD]' \
+                f'[AD]{share["conf"]["server_port"]}[/AD]'.encode('utf-8')
             )
 
-            w.write(f'[MOTD]{share["conf"]["motd"]}[/MOTD][AD]{share["conf"]["server_port"]}[/AD]'.encode('utf-8'))
-            await w.drain()
+            stream.close()
         except asyncio.TimeoutError:
             pass
         except Exception as e:

8712b2948b9bc8ae6c10e15aa80dad6a1abe4457
Iapetus-11 <iapetus011@gmail.com>
2020-12-28 17:23:35 -0500

    fix missing

--- src/server.py
+++ src/server.py
@@ -34,11 +34,25 @@ if '--dump-packets' in sys.argv:
     play_only = '--play-only' in sys.argv
 
     for state, tup in PACKET_MAP.items():
+        done = []
         if (play_only and state == 'play') or not play_only:
             print('\n' + state)
 
             for id_, to in sorted(tup, key=(lambda t: t[0])):
                 print(f'0x{id_:02X} ({dirs[to] if to is not None else "missing dir (to)"})')
+
+                if (id_ < 0xFE and state == 'handshaking') or state != 'handshaking':  # Exclude legacy packets
+                    done.append(id_)
+
+            print('MISSING: ', end='')
+
+            for i in range(max(done)):
+                try:
+                    done.index(i)
+                except ValueError:
+                    print(f'0x{i:02x}, ', end='')
+
+            print()
 
     exit(0)
 

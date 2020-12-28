f45c3f21d0f883eabdef3184a3c5a0d301b7408f
Iapetus-11 <iapetus011@gmail.com>
2020-12-27 19:37:25 -0500

    add info logging to close_server

--- src/util/close.py
+++ src/util/close.py
@@ -1,6 +1,7 @@
-from src.util.share import share
+from src.util.share import share, logger
 
 
 async def close_server():
+    logger.info('closing server...')
     share['server'].close()
     await share['server'].wait_closed()

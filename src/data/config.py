9cadd2d55d66bac12d428c68e0e3fb73df7ff657
Iapetus-11 <iapetus011@gmail.com>
2020-12-28 15:34:14 -0500

    remove debug

--- src/data/config.py
+++ src/data/config.py
@@ -38,15 +38,12 @@ def load_properties():
             f.write(yaml.dump(dict(SERVER_PROPERTIES_DEFAULT)))
 
     if any([(key not in properties) for key in SERVER_PROPERTIES_DEFAULT.keys()]):
-        print('need update')
         p_temp = properties
         properties = dict(SERVER_PROPERTIES_DEFAULT)
         properties.update(p_temp)
 
         with open('server.yml', 'w') as f:
             f.write(yaml.dump(properties))
-
-    print('after')
 
     return Map(properties)
 

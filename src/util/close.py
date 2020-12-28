f88b9b5c6f823c465f32cb0cbdd4270f52664c46
Iapetus-11 <iapetus011@gmail.com>
2020-12-27 19:42:48 -0500

    add server closed message

--- src/util/close.py
+++ src/util/close.py
@@ -3,5 +3,9 @@ from src.util.share import share, logger
 
 async def close_server():
     logger.info('closing server...')
+
+    # Close actual asyncio server
     share['server'].close()
     await share['server'].wait_closed()
+
+    logger.info('server closed.')

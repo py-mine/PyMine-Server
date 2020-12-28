cebe9640c9d80192a529d4731e98202c7574759c
Iapetus-11 <iapetus011@gmail.com>
2020-12-28 18:05:33 -0500

    update commands system

--- src/logic/commands.py
+++ src/logic/commands.py
@@ -3,20 +3,34 @@ import asyncio
 
 from src.util.share import share
 
+registered_commands = {}
 
-async def handle_command(cmd: str):
-    if cmd == 'stop':
-        share['server'].close()
-        return False
+# Example command
+# @command(name='somecmd')
+# async def some_command(uuid: str, text: str) -> none:
+#     pass
 
-    return True
+
+def command(name: str):
+    if name in registered_commands:
+        raise Exception('Command name is already in use.')
+
+    if ' ' in name:
+        raise Exception('Command name may not contain spaces.')
+
+    async def command_deco(func: 'function'):
+        if not asyncio.iscoroutine(func):
+            raise Exception('Commands must be coroutines.')
+
+        registered_commands[name] = func
+        return func
+
+    return command_deco
 
 
 async def handle_commands():
-    run = True
-
     try:
-        while True:
-            await handle_command(await aioconsole.ainput(''))
+        cmd = await aioconsole.ainput('>')
+        registered_commands.get(cmd.split(' ')[0], (lambda *args: pass))('system', cmd)
     except (KeyboardInterrupt, asyncio.CancelledError):
         pass

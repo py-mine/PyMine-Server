104affabd8089d5de162d89b873df61e029715aa
Sh-wayz <shwayz03@gmail.com>
2020-12-27 20:19:09 -0500

    fix variable name

--- src/types/packets/play/sounds.py
+++ src/types/packets/play/sounds.py
@@ -10,7 +10,7 @@ __all__ = ('PlayNamedSoundEffect',)
 
 class PlayNamedSoundEffect(Packet):
     """Used to play a sound effect on the client. Custom sounds may be added by resource packs. Clientbound."""
-    id = 0x18
+   id_ = 0x18
     to = 1
 
     def __init__(self, name: str, category: int, effect_pos_x: int, effect_pos_y: int, effect_pos_z: int, volume: int, pitch: int):  # nopep8

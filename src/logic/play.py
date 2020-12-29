d21c7f8ca17ecc409db117e8bccc137662017f87
Iapetus-11 <iapetus011@gmail.com>
2020-12-29 15:31:04 -0500

    start on finish_login

--- src/logic/play.py
+++ src/logic/play.py
@@ -1,3 +1,4 @@
+from src.types.packets.play import player as packets_player
 from src.types.buffer import Buffer
 
 from src.util.share import share, logger
@@ -8,4 +9,8 @@ async def play(r: 'StreamReader', w: 'StreamWriter', packet: 'Packet', remote: t
 
 
 async def finish_login(r: 'StreamReader', w: 'StreamWriter', remote: tuple) -> None:
-    pass
+    w.write(Buffer.pack_packet(packets_player.PlayJoinGame(
+
+    )))
+
+    await w.drain()

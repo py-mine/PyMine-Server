d63db6a1052b2e6d07de652a68f2f566ddbe4c6c
Iapetus-11 <iapetus011@gmail.com>
2020-12-28 22:10:19 -0500

    cut login logic

--- src/logic/login.py
+++ src/logic/login.py
@@ -15,7 +15,34 @@ login_cache = {}  # {remote: {username: username, verify_token: verify_token]}
 
 
 async def login(r: 'StreamReader', w: 'StreamWriter', packet: 'Packet', remote: remote):
-    pass
+    if packet.id_ == 0x00:  # LoginStart
+        if share['conf']['online_mode']:
+            login_cache[remote] = {'username': packet.username, 'verify': None}
+            await logic_request_encryption(r, w, packet, login_cache[remote])
+        else:  # If no auth is used, go straight to login success
+            await logic_login_success(r, w, packet.username)
+    elif packet.id_ == 0x01:  # LoginEncryptionResponse
+        shared_key, auth = await logic_server_auth(packet, remote, login_cache[remote])
+
+        del login_cache[remote]
+
+        if not auth:
+            await logic_login_kick(w)
+            return await close_con(w, remote)
+
+        # Generate a cipher for that client using the shared key from the client
+        cipher = encryption.gen_aes_cipher(shared_key)
+
+        # Replace streams with ones which auto decrypt + encrypt data when reading/writing
+        r = encryption.EncryptedStreamReader(r, cipher.decryptor())
+        w = encryption.EncryptedStreamWriter(w, cipher.encryptor())
+
+        if share['comp_thresh'] > 0:  # Send set compression packet if needed
+            await logic_login_set_compression(w)
+
+        await logic_login_success(r, w, *auth)
+
+        states[remote] = 3  # PLAY
 
 
 # Send an encryption request packet to the client

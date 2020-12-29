c86949356482cb9b64d74ff283de44798db7d5b0
Iapetus-11 <iapetus011@gmail.com>
2020-12-29 14:41:02 -0500

    finish method headers

--- src/logic/play.py
+++ src/logic/play.py
@@ -3,5 +3,9 @@ from src.types.buffer import Buffer
 from src.util.share import share, logger
 
 
-async def play(r: 'StreamReader', w: 'StreamWriter', packet: 'Packet', remote: tuple):
-    
+async def play(r: 'StreamReader', w: 'StreamWriter', packet: 'Packet', remote: tuple) -> tuple:
+    pass
+
+
+async def finish_login(r: 'StreamReader', w: 'StreamWriter', remote: tuple) -> None:
+    pass

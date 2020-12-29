b83473368162a704ecfcb7d4f06ca0c0dbb7609a
Iapetus-11 <iapetus011@gmail.com>
2020-12-28 20:29:32 -0500

    add exec command

---
+++ src/logic/cmds/exec_file.py
@@ -0,0 +1,16 @@
+from src.logic.commands import command, handle_command
+from src.util.share import logger
+
+@command(name='exec')
+async def exec_command(uuid: str, args: list):
+    file_name = ''.join(args)
+
+    try:
+        with open(file_name, 'r') as f:
+            cmds_lines = [l.rstrip('\n') for l in  f.readlines()]
+
+            for cmd_line in cmds_line:
+                await handle_command(cmd_line)
+
+    except FileNotFoundError:
+        logger.warn('Can\'t find that file...')

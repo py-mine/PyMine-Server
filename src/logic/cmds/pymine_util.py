58b3279e214372cd2f8289bbcedc4569eb9de18e
Iapetus-11 <iapetus011@gmail.com>
2020-12-28 21:21:06 -0500

    fix call to handle_server_command

--- src/logic/cmds/pymine_util.py
+++ src/logic/cmds/pymine_util.py
@@ -3,7 +3,7 @@ from src.util.share import logger
 
 
 @command(name='exec', node='pymine.cmds.exec')
-async def exec_command(uuid: str, args: list):
+async def exec_(uuid: str, args: list):
     file_name = ''.join(args)
 
     try:
@@ -11,7 +11,7 @@ async def exec_command(uuid: str, args: list):
             cmds_lines = [l.rstrip('\n') for l in f.readlines()]
 
             for cmd_line in cmds_lines:
-                await handle_command(cmd_line)
+                await handle_server_command(cmd_line)
 
     except FileNotFoundError:
         logger.warn('Can\'t find that file...')

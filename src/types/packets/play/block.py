5d44e6cadbc59755ba85012737165163fab17047
Iapetus-11 <iapetus011@gmail.com>
2020-12-30 16:21:01 -0500

    consistency changes

--- src/types/packets/play/block.py
+++ src/types/packets/play/block.py
@@ -44,10 +44,8 @@ class PlayBlockAction(Packet):
         self.block_type = block_type
 
     def encode(self) -> bytes:
-        return Buffer.pack_pos(self.x, self.y, self.z) + \
-            Buffer.pack('B', self.action_id) + \
-            Buffer.pack('B', self.action_param) + \
-            Buffer.pack_varint(self.block_type)
+        return Buffer.pack_pos(self.x, self.y, self.z) + Buffer.pack('B', self.action_id) + \
+            Buffer.pack('B', self.action_param) + Buffer.pack_varint(self.block_type)
 
 
 class PlayBlockChange(Packet):

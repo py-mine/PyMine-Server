7e85d79d7e4dcbd02a00e96a7059b8c15eadf181
Iapetus-11 <iapetus011@gmail.com>
2020-12-30 14:04:36 -0500

    update the __all__ in animations.py

--- src/types/packets/play/animations.py
+++ src/types/packets/play/animations.py
@@ -5,7 +5,10 @@ from __future__ import annotations
 from src.types.packet import Packet
 from src.types.buffer import Buffer
 
-__all__ = ('PlayEntityAnimation',)
+__all__ = (
+    'PlayEntityAnimation',
+    'PlayBlockBreakAnimation',
+)
 
 
 class PlayEntityAnimation(Packet):

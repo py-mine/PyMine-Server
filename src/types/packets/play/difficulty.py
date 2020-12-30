cd5e3ee849dec8f2abbf7832cfa391941c04f1fd
Iapetus-11 <iapetus011@gmail.com>
2020-12-30 14:15:03 -0500

    add docstring

--- src/types/packets/play/difficulty.py
+++ src/types/packets/play/difficulty.py
@@ -9,6 +9,13 @@ __all__ = ('PlayServerDifficulty',)
 
 
 class PlayServerDifficulty(Packet):
+    """Used by the server to update the difficulty in the client's menu. (Server -> Client)
+
+    :param int difficulty: The difficulty level, see here: https://wiki.vg/Protocol#Server_Difficulty.
+    :attr int id_: Unique packet ID.
+    :attr int to: Packet direction.
+    :attr difficulty:
+    """
 
     id_ = 0x0D
     to = 1

6c4132ea43d3af3ef8102eee3e9acb8699f250a8
Iapetus-11 <iapetus011@gmail.com>
2020-12-29 19:56:01 -0500

    update import in player.py

--- src/types/packets/play/player.py
+++ src/types/packets/play/player.py
@@ -1,8 +1,8 @@
 """Contains packets related to players."""
 
 from __future__ import annotations
+from nbt import nbt
 import asyncio
-import nbt
 
 from src.types.packet import Packet
 from src.types.buffer import Buffer

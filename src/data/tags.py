d9408c46412bc725e0ba54173f8af3d8e44e9ea0
Iapetus-11 <iapetus011@gmail.com>
2020-12-30 20:54:00 -0500

    update tags.py to handle tag shortcuts

--- src/data/tags.py
+++ src/data/tags.py
@@ -27,4 +27,16 @@ for tag_type in os.listdir('src/data/tags'):
         with open(f'src/data/tags/{tag_type}/{tag_file}') as f:
             TAGS[tag_type][tag_file[:-5]] = json.load(f)['values']
 
+for tag_type in TAGS:
+    for identifier, values in TAGS[tag_type].items():
+        new_values = []
+
+        for value in values:
+            if value.startswith('#'):
+                new_values += TAGS[tag_type][value.split(':')[1]]
+
+        if len(new_values) > 0:
+            TAGS[tag_type][identifier] = new_values
+
+print(TAGS['items']['flowers'])
 TAGS = make_immutable(TAGS)

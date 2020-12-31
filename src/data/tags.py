17beed33e4a7a95c1db9dd6b748afbfa51d39c42
Iapetus-11 <iapetus011@gmail.com>
2020-12-30 19:50:06 -0500

    add src/data/tags.py for loading tags

---
+++ src/data/tags.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+from src.util.immutable import make_immutable
+
+__all__ = ('TAGS',)
+
+TAGS = {}
+
+for tag_type in os.listdir('src/data/tags'):
+    for tag_file in os.listdir(f'src/data/tags/{tag_type}'):
+        with open(f'src/data/tags/{tag_type}/{tag_file}') as f:
+            TAGS[tag_type][tag_file[:-5]] = json.load(f)['values']
+
+TAGS = make_immutable(TAGS)

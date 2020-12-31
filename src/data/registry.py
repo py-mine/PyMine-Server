f39643e4a239fa7d9801ff322344be7f56d8c058
Iapetus-11 <iapetus011@gmail.com>
2020-12-30 20:25:28 -0500

    add missing self

--- src/data/registry.py
+++ src/data/registry.py
@@ -11,7 +11,7 @@ __all__ = (
 )
 
 class Registry:
-    def __init__(data: dict):
+    def __init__(self, data: dict):
         self.data = make_immutable({k: v['protocol_id'] for k, v in data.items()})
         self.data_reversed = make_immutable({v: k for k, v in data.items()})
 

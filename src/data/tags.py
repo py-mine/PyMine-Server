53fbd1e0c41208f48969924daea11d2911e4c083
Iapetus-11 <iapetus011@gmail.com>
2020-12-30 19:52:17 -0500

    fix tag loading

--- src/data/tags.py
+++ src/data/tags.py
@@ -8,6 +8,8 @@ __all__ = ('TAGS',)
 TAGS = {}
 
 for tag_type in os.listdir('src/data/tags'):
+    TAGS[tag_type] = {}
+    
     for tag_file in os.listdir(f'src/data/tags/{tag_type}'):
         with open(f'src/data/tags/{tag_type}/{tag_file}') as f:
             TAGS[tag_type][tag_file[:-5]] = json.load(f)['values']

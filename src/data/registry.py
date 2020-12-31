7f0abff319a9f855fd514b3b2d76a9c3b90bca7b
Iapetus-11 <iapetus011@gmail.com>
2020-12-30 20:07:29 -0500

    add registry class

--- src/data/registry.py
+++ src/data/registry.py
@@ -9,6 +9,17 @@ __all__ = (
     'PARTICLES_BY_ID',
 )
 
+class Registry:
+    def __init__(data: dict):
+        self.data = Map(data)
+        self.data_reversed = Map({v: k for k, v in data.items()})
+
+    def encode(self, key: object) -> object:
+        return self.data[key]
+
+    def decode(self, value: object) -> object:
+        return self.data_reversed[value]
+
 with open('src/data/registries.json') as registry:  # generated from server jar
     REGISTRY = json.load(registry)
 
@@ -18,3 +29,6 @@ ITEMS_BY_ID = Map({v: k for k, v in ITEMS_BY_NAME.items()})
 PARTICLES_BY_NAME = Map({k: v['protocol_id']
                          for k, v in REGISTRY['minecraft:particle_type']['entries'].items()})
 PARTICLES_BY_ID = Map({v: k for k, v in PARTICLES_BY_NAME.items()})
+
+FLUIDS_BY_NAME = Map({k: v['protocol_id'] for k, v in REGISTRY['minecraft:fluid']['entries'].items()})
+FLUIDS_BY_ID = Map({v: k for k, v in FLUIDS_BY_NAME.items()})

9ca00c968c859ba9c446285fb4a26c266648ae72
Iapetus-11 <iapetus011@gmail.com>
2021-01-03 12:05:56 -0500

    pep8

--- src/types/packets/play/entity.py
+++ src/types/packets/play/entity.py
@@ -38,5 +38,5 @@ class PlayBlockEntityData(Packet):
         self.nbt_data = nbt_data
 
     def encode(self) -> bytes:
-        return Buffer.pack_pos(self.x, self.y, self.z) + \
-            Buffer.pack('B', self.action) + Buffer.pack_nbt(self.nbt_data)
+        return Buffer.pack_pos(self.x, self.y, self.z) + Buffer.pack('B', self.action) + \
+            Buffer.pack_nbt(self.nbt_data)

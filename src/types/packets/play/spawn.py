decefe893d84f6d9d3e4f2e06e14a7a133e0e981
Iapetus-11 <iapetus011@gmail.com>
2021-01-03 00:42:57 -0500

    merge spawn.py

--- src/types/packets/play/spawn.py
+++ src/types/packets/play/spawn.py
@@ -22,14 +22,14 @@ class PlayEntitySpawn(Packet):
     id = 0x00
     to = 1
 
-    def __init__(self, entity_id: int, object_uuid: uuid.UUID, type: int,
+    def __init__(self, entity_id: int, object_uuid: uuid.UUID, type_: int,
                  x: int, y: int, z: int, pitch: int, yaw: int, data: int,
                  vloc_x: int, vloc_y: int, vloc_z: int) -> None:
         super().__init__()
 
         self.entity_id = entity_id
         self.object_uuid = object_uuid
-        self.type = type
+        self.type_ = type_
         self.x, self.y, self.z = x, y, z
         self.pitch = pitch
         self.yaw = yaw
@@ -40,7 +40,7 @@ class PlayEntitySpawn(Packet):
 
     def encode(self):
         return Buffer.pack_varint(self.entity_id) + Buffer.pack_uuid(self.object_uuid) + \
-            Buffer.pack_varint(self.type) + Buffer.pack('d' + self.x) +\
+            Buffer.pack_varint(self.type_) + Buffer.pack('d' + self.x) +\
             Buffer.pack('d' + self.y) + Buffer.pack('d' + self.z) + Buffer.pack('i' + self.pitch) +\
             Buffer.pack('i' + self.yaw) + Buffer.pack('h' + self.vloc_x) +\
             Buffer.pack('h' + self.vloc_x) + Buffer.pack('h' + self.vloc_z)
@@ -71,14 +71,14 @@ class PlayLivingEntitySpawn(Packet):
     id = 0x02
     to = 1
 
-    def __init__(self, entity_id: int, object_uuid: uuid.UUID, type: int,
+    def __init__(self, entity_id: int, object_uuid: uuid.UUID, type_: int,
                  x: int, y: int, z: int, pitch: int, head_pitch: int, yaw: int,
                  vloc_x: int, vloc_y: int, vloc_z: int) -> None:
         super().__init__()
 
         self.entity_id = entity_id
         self.object_uuid = object_uuid
-        self.type = type
+        self.type_ = type_
         self.x, self.y, self.z = x, y, z
         self.pitch = pitch
         self.yaw = yaw
@@ -89,7 +89,7 @@ class PlayLivingEntitySpawn(Packet):
 
     def encode(self):
         return Buffer.pack_varint(self.entity_id) + Buffer.pack_uuid(self.object_uuid) +\
-            Buffer.pack_varint(self.type) + Buffer.pack('d' + self.x) +\
+            Buffer.pack_varint(self.type_) + Buffer.pack('d' + self.x) +\
             Buffer.pack('d' + self.y) + Buffer.pack('d' + self.z) +\
             Buffer.pack('i' + self.pitch) + Buffer.pack('i' + self.yaw) +\
             Buffer.pack('i', self.head_pitch) + Buffer.pack('h' + self.vloc_x) +\

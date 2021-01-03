6fbbc29e9c95ae3661a95f4924feb9fb0312b11b
Iapetus-11 <iapetus011@gmail.com>
2021-01-03 14:47:38 -0500

    update registry to support creation from iterables

--- src/types/registry.py
+++ src/types/registry.py
@@ -1,9 +1,17 @@
 from src.util.immutable import make_immutable
 
 class Registry:
-    def __init__(self, data: dict):
-        self.data = make_immutable({k: v['protocol_id'] for k, v in data.items()})
-        self.data_reversed = make_immutable({v: k for k, v in data.items()})
+    def __init__(self, data: object):
+        if isinstance(data, dict):
+            self.data = make_immutable({k: v['protocol_id'] for k, v in data.items()})
+            self.data_reversed = make_immutable({v: k for k, v in data.items()})
+        elif isinstance(data, (list, tuple)):
+            self.data = data
+            self.data_reversed = make_immutable({v: i for i, v in enumerate(data)})
+        else:
+            raise Exception(
+                'Creating a registry from something other than a dict, tuple, or list is unsupported'
+            )
 
     def encode(self, key: object) -> object:
         return self.data[key]

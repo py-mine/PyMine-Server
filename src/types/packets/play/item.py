1cf44e70549eefdbd44dd3770e1d5f900b75e52d
Iapetus-11 <iapetus011@gmail.com>
2021-01-03 11:52:24 -0500

    add PlayEditBook packet

--- src/types/packets/play/item.py
+++ src/types/packets/play/item.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from src.types.packet import Packet
 from src.types.buffer import Buffer
 
-__all__ = ('PlayUseItem',)
+__all__ = ('PlayUseItem', 'PlayEditBook',)
 
 
 class PlayUseItem(Packet):
@@ -28,3 +28,31 @@ class PlayUseItem(Packet):
     @classmethod
     def decode(cls, buf: Buffer) -> PlayUseItem:
         return cls(buf.upack_varint())
+
+
+class PlayEditBook(Packet):
+    """Used by the client to edit a book. (Client -> Server)
+
+    :param dict new_book: The new slot/data for the book.
+    :param bool is_signing: Whether the player is signing the book or just saving a draft.
+    :param int hand: The hand used. Either main hand (0) or offhand (1).
+    :attr type id: Unique packet ID.
+    :attr type to: Packet direction.
+    :attr new_book:
+    :attr is_signing:
+    :attr hand:
+    """
+
+    id = 0x0C
+    to = 0
+
+    def __init__(self, new_book: dict, is_signing: bool, hand: int) -> None:
+        super().__init__()
+
+        self.new_book = new_book
+        self.is_signing = is_signing
+        self.hand = hand
+
+    @classmethod
+    def decode(cls, buf: Buffer) -> PlayEditBook:
+        return cls(buf.unpack_slot(), buf.unpack_bool(), buf.unpack_varint())

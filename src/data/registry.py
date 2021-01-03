0663868dbd4685e3569e950a0da46fcc69c5739c
Iapetus-11 <iapetus011@gmail.com>
2021-01-03 14:37:50 -0500

    update data/registry.py

--- src/data/registry.py
+++ src/data/registry.py
@@ -1,6 +1,6 @@
 import json
 
-from src.util.immutable import make_immutable
+from src.types.registry import registry
 
 __all__ = (
     'ITEM_REGISTRY',
@@ -9,19 +9,6 @@ __all__ = (
     'BLOCK_REGISTRY',
     'ENTITY_REGISTRY',
 )
-
-
-class Registry:
-    def __init__(self, data: dict):
-        self.data = make_immutable({k: v['protocol_id'] for k, v in data.items()})
-        self.data_reversed = make_immutable({v: k for k, v in data.items()})
-
-    def encode(self, key: object) -> object:
-        return self.data[key]
-
-    def decode(self, value: object) -> object:
-        return self.data_reversed[value]
-
 
 with open('src/data/registries.json') as registry:  # generated from server jar
     REGISTRY = make_immutable(json.load(registry))

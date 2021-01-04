d2835d0c53e572457011f786f98862a56fc5b7fe
Iapetus-11 <iapetus011@gmail.com>
2021-01-03 22:11:54 -0500

    convert state.py to use spaces not tabs

--- src/types/packets/play/state.py
+++ src/types/packets/play/state.py
@@ -11,14 +11,14 @@ __all__ = ('PlayChangeGameState',)
 class PlayChangeGameState(Packet):
     """insert fancy doscstring here (server -> client)"""
 
-	id = 0x1D
-	to = 1
+    id = 0x1D
+    to = 1
 
-	def __init__(self, reason: int, value: float) -> None:
-		super().__init__()
+    def __init__(self, reason: int, value: float) -> None:
+        super().__init__()
 
-		self.reason = reason
-		self.value = value
+        self.reason = reason
+        self.value = value
 
-	def encode(self) -> bytes:
-		return Buffer.pack('B', self.reason) + Buffer.pack('f', self.value)
+    def encode(self) -> bytes:
+        return Buffer.pack('B', self.reason) + Buffer.pack('f', self.value)

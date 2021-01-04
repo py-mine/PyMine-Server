58e061fb1937ac7ce3c3b97afa983ff7d8eeeca4
Iapetus-11 <iapetus011@gmail.com>
2021-01-04 14:42:12 -0500

    start on PlayEffect packet

---
+++ src/types/packets/play/effect.py
@@ -0,0 +1,20 @@
+"""Contains packets related to potions and effects."""
+
+from __future__ import annotations
+
+from src.types.packet import Packet
+from src.types.buffer import Buffer
+
+__all__ = ('',)
+
+
+class PlayEffect(Packet):
+
+    id = 0x21
+    to = 1
+
+    def __init__(self, effect_id: int, x: int, y: int, z: int, data: int, disable_relative_volume: bool) -> None:
+        raise NotImplementedError
+
+    def encode(self) -> bytes:
+        raise NotImplementedError

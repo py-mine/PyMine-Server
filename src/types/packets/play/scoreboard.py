79858027fd29ab7641275980ac8b0a239c460daa
emerald73 <>
2021-01-04 21:29:49 -0500

    add scoreboard.py, add PlayDisplayScoreboard

---
+++ src/types/packets/play/scoreboard.py
@@ -0,0 +1,26 @@
+"""Contains packets related to scoreboard."""
+
+from __future__ import annotations
+import nbt
+
+from src.types.packet import Packet
+from src.types.buffer import Buffer
+
+__all__ = (
+    'PlayDisplayScoreboard',
+)
+
+class PlayDisplayScoreboard(Packet):
+    """Insert fancy doscstring here (server -> client)"""
+
+    id = 0x43
+    to = 1
+
+    def __init__(self, position: int, score_name: str) -> None:
+        super().__init__()
+
+        self.pos = position
+        self.score = score_name
+
+    def encode(self) -> bytes:
+        return Buffer.pack('b', self.pos) + Buffer.pack_string(self.score)

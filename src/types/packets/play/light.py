c0f6d9f8b02687b1b544207ee3d68f28c4494af9
Sh-wayz <shwayz03@gmail.com>
2021-01-06 12:10:14 -0500

    remove extra comma

--- src/types/packets/play/light.py
+++ src/types/packets/play/light.py
@@ -14,7 +14,7 @@ class PlayUpdateLight(Packet):
 
     def __init__(self, chunk_x: int, chunk_z: int, trust_edges: bool, sky_light_mask: int, block_light_mask: int, empty_sky_light_mask: int, empty_block_light_mask: int, sky_light_arr: list, block_light_arr: list) -> None:  # nopep8
         super().__init__()
-        self.chunk_x, , self.chunk_z = chunk_x, chunk_z
+        self.chunk_x, self.chunk_z = chunk_x, chunk_z
         self.trust_edges = trust_edges
         self.sky_lm, self.block_lm, self.esky_lm, self.eblock_lm = sky_light_mask, block_light_mask, empty_sky_light_mask, empty_block_light_mask
         self.sklr, self.blkr = sky_light_array, block_light_array

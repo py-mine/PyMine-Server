18a5d20eeb8842124d4a487d84d207a2e243b1fa
Milo Weinberg <iapetus011@gmail.com>
2021-01-06 20:56:54 -0500

    fix weird short names

--- src/types/packets/play/light.py
+++ src/types/packets/play/light.py
@@ -19,9 +19,9 @@ class PlayUpdateLight(Packet):
         self.chunk_x, self.chunk_z = chunk_x, chunk_z
         self.trust_edges = trust_edges
         self.sky_lm, self.block_lm, self.esky_lm, self.eblock_lm = sky_light_mask, block_light_mask, empty_sky_light_mask, empty_block_light_mask
-        self.sklr, self.blkr = sky_light_arr, block_light_arr
+        self.sky_light_array, self.block_light_array = sky_light_arr, block_light_arr
 
     def encode(self) -> bytes:
         return Buffer.pack('i', self.chunk_x) + Buffer.pack('i', self.chunk_z) + Buffer.pack('?', self.trust_edges) +\
             Buffer.pack_varint(self.sky_lm) + Buffer.pack_varint(self.block_lm) + Buffer.pack_varint(self.esky_lm) +\
-            Buffer.pack_varint(self.eblock_lm) + Buffer.pack_array('c', self.sklr) + Buffer.pack_array('c', self.blkr)
+            Buffer.pack_varint(self.eblock_lm) + Buffer.pack_array('c', self.sky_light_array) + Buffer.pack_array('c', self.block_light_array)

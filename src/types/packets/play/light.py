be1ed7fcf85e66c78684fec94b9eae02f20fc7b8
Milo Weinberg <iapetus011@gmail.com>
2021-01-06 21:01:33 -0500

    fix names and spacing

--- src/types/packets/play/light.py
+++ src/types/packets/play/light.py
@@ -23,13 +23,16 @@ class PlayUpdateLight(Packet):
             block_light_mask: int,
             empty_sky_light_mask: int,
             empty_block_light_mask: int,
-            sky_light_arr: list,
-            block_light_arr: list) -> None:
+            sky_light_array: list,
+            block_light_array: list) -> None:
         super().__init__()
+        
         self.chunk_x, self.chunk_z = chunk_x, chunk_z
         self.trust_edges = trust_edges
-        self.sky_lm, self.block_lm, self.esky_lm, self.eblock_lm = sky_light_mask, block_light_mask, empty_sky_light_mask, empty_block_light_mask
-        self.sky_light_array, self.block_light_array = sky_light_arr, block_light_arr
+        self.sky_light_mask, self.empty_sky_light_mask = sky_light_mask, empty_sky_light_mask
+        self.block_light_mask, self.empty_block_light_mask = block_light_mask, empty_block_light_mask
+        self.sky_light_array = sky_light_array
+        self.block_light_array = block_light_array
 
     def encode(self) -> bytes:
         return Buffer.pack('i', self.chunk_x) + Buffer.pack('i', self.chunk_z) + Buffer.pack('?', self.trust_edges) + \

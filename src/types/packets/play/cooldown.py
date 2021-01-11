58367e4b9251c15a49d4bc6f169780d89aad60f9
Milo Weinberg <iapetus011@gmail.com>
2021-01-10 21:35:02 -0500

    add missing return annotations for PlaySetCooldown

--- src/types/packets/play/cooldown.py
+++ src/types/packets/play/cooldown.py
@@ -20,11 +20,11 @@ class PlaySetCooldown(Packet):
     id = 0x16
     to = 1
 
-    def __init__(self, item_id: int, cooldown_ticks: int):
+    def __init__(self, item_id: int, cooldown_ticks: int) -> None:
         super().__init__()
 
         self.item_id = item_id
         self.cooldown_ticks = cooldown_ticks
 
-    def encode(self):
+    def encode(self) -> bytes:
         return Buffer.pack_varint(self.item_id) + Buffer.pack_varint(self.cooldown_ticks)

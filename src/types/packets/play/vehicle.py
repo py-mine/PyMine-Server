548983ed24fea798eebe7e288d856834b84389d8
Milo Weinberg <iapetus011@gmail.com>
2021-01-11 17:11:51 -0500

    add PlayVehicleMoveClientBound packet

--- src/types/packets/play/vehicle.py
+++ src/types/packets/play/vehicle.py
@@ -7,6 +7,7 @@ from src.types.buffer import Buffer
 
 __all__ = (
     'PlayVehicleMoveServerBound',
+    'PlayVehicleMoveClientBound',
     'PlaySteerBoat',
     'PlaySteerVehicle',
     'PlaySetPassengers',
@@ -49,6 +50,38 @@ class PlayVehicleMoveServerBound(Packet):
             buf.unpack('f'),
             buf.unpack('f')
         )
+
+
+class PlayVehicleMoveClientBound(Packet):
+    """Sent by the server when a vehicle moves. (Server -> Client)
+
+    :param float x: The x coordinate of where the vehicle is.
+    :param float y: The y coordinate of where the vehicle is.
+    :param float z: The z coordinate of where the vehicle is.
+    :param float yaw: The yaw (absolute rotation on x axis) in degrees.
+    :param float pitch: The pitch (absolute rotation on y axis) in degrees.
+    :attr int id: Unique packet ID.
+    :attr int to: Packet direction.
+    :attr x:
+    :attr y:
+    :attr z:
+    :attr yaw:
+    :attr pitch:
+    """
+
+    id = 0x2B
+    to = 1
+
+    def __init__(self, x: float, y: float, z: float, yaw: float, pitch: float) -> None:
+        super().__init__()
+
+        self.x, self.y, self.z = x, y, z
+        self.yaw = yaw
+        self.pitch = pitch
+
+    def encode(self) -> bytes:
+        return Buffer.pack('d', self.x) + Buffer.pack('d', self.y) + Buffer.pack('d', self.z) + Buffer.pack('f', self.yaw) + \
+            Buffer.pack('f', self.pitch)
 
 
 class PlaySteerBoat(Packet):

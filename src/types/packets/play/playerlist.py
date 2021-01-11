a2dbf181c4b5534b2f59cf45816e09d388d6b5e2
Sh-wayz <shwayz03@gmail.com>
2021-01-11 18:43:19 -0500

    i messed up so much lol

---
+++ src/types/packets/play/playerlist.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from src.types.packet import Packet
+from src.types.buffer import Buffer
+from src.types.chat import Chat
+
+__all__ = ('PlayPlayerListHeaderAndFooter',)
+
+class PlayPlayerListHeaderAndFooter(Packet):
+    """yep"""
+
+    id = 0x53
+    to = 1
+
+
+    def __init__(self, header: Chat, footer: Chat) -> None:
+        super().__init__()
+
+        self.header, self.footer = header, footer
+
+    def encode(self) -> bytes:
+        return Buffer.pack_chat(self.header) + Buffer.pack_chat(self.footer)
+

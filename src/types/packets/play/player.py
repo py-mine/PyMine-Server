3c97bbb9dfe2367edd0dbd427657673a3d972d5c
Milo Weinberg <iapetus011@gmail.com>
2021-01-10 21:45:46 -0500

    move line break + continuation

--- src/types/packets/play/player.py
+++ src/types/packets/play/player.py
@@ -95,8 +95,8 @@ class PlayAcknowledgePlayerDigging(Packet):
         self.successful = successful
 
     def encode(self) -> bytes:
-        return Buffer.pack_pos(self.x, self.y, self.z) + Buffer.pack_varint(self.block) + \
-            Buffer.pack_varint(self.status) + Buffer.pack('?', self.successful)
+        return Buffer.pack_pos(self.x, self.y, self.z) + Buffer.pack_varint(self.block) + Buffer.pack_varint(self.status) + \
+            Buffer.pack('?', self.successful)
 
 
 class PlayDisconnect(Packet):

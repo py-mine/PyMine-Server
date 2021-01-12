db651bf846d28f0b702c5d900f3c275db578c971
Milo Weinberg <iapetus011@gmail.com>
2021-01-12 14:41:10 -0500

    allow for multiple ids

--- src/api.py
+++ src/api.py
@@ -9,7 +9,7 @@ events/decorators:
     command: @on_command(name='name', node='plugin_name.cmds.cmd_name')
     chat message: @on_message
     packets:
-        packet logic: @packet_logic(id=0x00)
+        packet logic: @packet_logic(*id=0x00)
         after packet logic: @after_packet_logic(id=0x00)
     tasks: @task(ticks_per=1 or seconds_per=1, minutes_per=1, hours_per=1)
     players:

75bc1d40816716c3e8d9fc658228d9100beabc43
Milo Weinberg <iapetus011@gmail.com>
2021-01-12 13:08:23 -0500

    more planning

--- src/events.py
+++ src/events.py
@@ -1,7 +1,10 @@
-from src.logic.command import command as command
+from src.logic.command import command
 
 # Events:
+#   server:
+#       startup: @on_server_ready
 #   command: @command(name='name', node='plugin_name.cmds.cmd_name')
 #   packets:
 #       packet logic: @packet_logic(id=0x00)
 #       after packet logic: @after_packet_logic(id=0x00)
+#   tasks: @task(ticks_per=1, seconds_per=1, minutes_per=1, hours_per=1)

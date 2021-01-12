5d36ee2b406c60f1fef26e689ad1d29c33fe0c41
Milo Weinberg <iapetus011@gmail.com>
2021-01-12 13:12:46 -0500

    restructuringggg

--- src/api.py
+++ src/api.py
@@ -1,11 +1,14 @@
-from src.logic.command import command
+from src.logic.command import command as on_command
 
-# events:
+# events/decorators:
 #   server:
 #       startup: @on_server_ready
 #       shutdown: @on_server_stop
-#   command: @command(name='name', node='plugin_name.cmds.cmd_name')
+#   command: @on_command(name='name', node='plugin_name.cmds.cmd_name')
 #   packets:
 #       packet logic: @packet_logic(id=0x00)
 #       after packet logic: @after_packet_logic(id=0x00)
 #   tasks: @task(ticks_per=1 or seconds_per=1, minutes_per=1, hours_per=1)
+#   players:
+#       player join: @on_player_join
+#       player leave: @on_player_leave

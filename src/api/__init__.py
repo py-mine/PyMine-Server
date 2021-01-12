7f2e097e342e2297923ac3c8afd5b8cd7b8212b0
Milo Weinberg <iapetus011@gmail.com>
2021-01-12 17:49:52 -0500

    move packet handler decorator to packets.py

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -56,19 +56,3 @@ async def init():  # called when server starts up
 async def stop():  # called when server is stopping
     for task in running_tasks:
         task.cancel()
-
-
-PACKET_HANDLERS = {'handshaking': {}, 'login': {}, 'play': {}, 'status': {}}
-
-
-# Decorator for adding a coroutine to handle a specific incoming packet
-def handle_packet(state: str, id_: int):
-    def command_deco(func):
-        try:
-            PACKET_HANDLERS[state][id_].append(func)
-        except KeyError:
-            PACKET_HANDLERS[state][id_] = [func]
-
-        return func
-
-    return command_deco

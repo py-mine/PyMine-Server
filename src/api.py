95b1e0e509ddaedae8c65d6c644be6432eedd2cf
Milo Weinberg <iapetus011@gmail.com>
2021-01-12 13:10:00 -0500

    more planning

--- src/api.py
+++ src/api.py
@@ -1,10 +1,11 @@
 from src.logic.command import command
 
-# Events:
+# events:
 #   server:
 #       startup: @on_server_ready
+#       shutdown: @on_server_stop
 #   command: @command(name='name', node='plugin_name.cmds.cmd_name')
 #   packets:
 #       packet logic: @packet_logic(id=0x00)
 #       after packet logic: @after_packet_logic(id=0x00)
-#   tasks: @task(ticks_per=1, seconds_per=1, minutes_per=1, hours_per=1)
+#   tasks: @task(ticks_per=1 or seconds_per=1 or minutes_per=1 or hours_per=1)

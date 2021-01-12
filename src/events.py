22f9c3ef49d509ae45b5cbee87281b08d2758cc9
Milo Weinberg <iapetus011@gmail.com>
2021-01-12 13:05:45 -0500

    start on structure of api

--- src/events.py
+++ src/events.py
@@ -1,3 +1,7 @@
-from src.logic.command import command
+from src.logic.command import command as command
 
-registrar = {}
+# Events:
+#   command: @command(name='name', node='plugin_name.cmds.cmd_name')
+#   packets:
+#       packet logic: @packet_logic(id=0x00)
+#       after packet logic: @after_packet_logic(id=0x00)

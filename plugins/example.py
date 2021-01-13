74007b1f0534f76cc6d733e1ebe36d98e49851aa
Milo Weinberg <iapetus011@gmail.com>
2021-01-12 20:46:06 -0500

    add on_server_ready() example

--- plugins/example.py
+++ plugins/example.py
@@ -1,3 +1,4 @@
+from src.api.server import on_server_ready
 from src.api.packet import handle_packet
 
 
@@ -6,3 +7,8 @@ async def example_handle_handshake(r, w, packet, remote):
     print('Hello this is the example packet handler speaking how may I take your order sir')
 
     return True, r, w
+
+
+@on_server_ready
+async def on_server_ready():
+    print('AYYY SERVER DO BE WORKING THO NGL')

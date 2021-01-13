0093562a256c8b6a44ca283d5b1aacf7155a4e42
Milo Weinberg <iapetus011@gmail.com>
2021-01-12 21:04:01 -0500

    add checks to ensure passed function is a coroutine

--- src/api/packet.py
+++ src/api/packet.py
@@ -1,3 +1,4 @@
+import asyncio
 
 PACKET_HANDLERS = {'handshaking': {}, 'login': {}, 'play': {}, 'status': {}}
 
@@ -5,6 +6,9 @@ PACKET_HANDLERS = {'handshaking': {}, 'login': {}, 'play': {}, 'status': {}}
 # Decorator for adding a coroutine to handle a specific incoming packet
 def handle_packet(state: str, id_: int):
     def command_deco(func):
+        if not asyncio.iscoroutinefunction(func):
+            raise ValueError('Decorated object must be a coroutine function')
+
         try:
             PACKET_HANDLERS[state][id_].append(func)
         except KeyError:

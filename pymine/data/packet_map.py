5d9248a9c97bcaf905601c6a0a648a4d7475bed7
Milo Weinberg <iapetus011@gmail.com>
2021-01-15 08:37:48 -0500

    optimize load_packets()

--- pymine/data/packet_map.py
+++ pymine/data/packet_map.py
@@ -12,16 +12,15 @@ def load_packets():
     for state in os.listdir('pymine/types/packets'):
         packet_map[state] = {}
 
-        for file in os.listdir(f'pymine/types/packets/{state}'):
-            if file.endswith('.py'):
-                module = importlib.import_module(f'pymine.types.packets.{state}.{file[:-3]}')
+        for file in filter((lambda f: f.endswith('.py')), os.listdir(f'pymine/types/packets/{state}')):
+            module = importlib.import_module(f'pymine.types.packets.{state}.{file[:-3]}')
 
-                for name in module.__all__:
-                    packet = module.__dict__.get(name)
-                    if packet.to == 2:
-                        packet_map[state][(packet.id, 0,)] = packet_map[state][(packet.id, 1,)] = packet
-                    else:
-                        packet_map[state][(packet.id, packet.to,)] = packet
+            for name in module.__all__:
+                packet = module.__dict__.get(name)
+                if packet.to == 2:
+                    packet_map[state][(packet.id, 0,)] = packet_map[state][(packet.id, 1,)] = packet
+                else:
+                    packet_map[state][(packet.id, packet.to,)] = packet
 
     return make_immutable(packet_map)
 

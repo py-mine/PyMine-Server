f7b8cfe6c381953b4c4e2524ade1849e67878cb5
Milo Weinberg <iapetus011@gmail.com>
2021-01-18 19:22:35 -0500

    update fap

--- plugins/FAP/__init__.py
+++ plugins/FAP/__init__.py
@@ -56,6 +56,8 @@ def load_plugin_list():
 
 
 async def update_self(logger, root_folder):
+    logger.info('Updated FAP!')
+
     self = importlib.import_module(root_folder.replace(os.sep, '.'))
     importlib.reload(self)
 
@@ -130,12 +132,10 @@ async def setup(logger):
             logger.error(f'Failed to update plugin "{plugin_name}" due to: {logger.f_traceback(e)}')
             continue
 
-        print(did_update)
-
         if did_update is None:
-            logger.info(f'Updated {plugin_name}!')
             return
-        elif did_update:
+
+        if did_update:
             logger.info(f'Updated {plugin_name}!')
         else:
             logger.info(f'No updates found for {plugin_name}.')

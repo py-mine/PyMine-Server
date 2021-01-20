2db1ed65d2b4dec3b18438fd16670e32364e8522
Milo Weinberg <iapetus011@gmail.com>
2021-01-19 23:47:26 -0500

    fix typo (missing stream.)

--- pymine/logic/handle/handshaking.py
+++ pymine/logic/handle/handshaking.py
@@ -7,5 +7,5 @@ from pymine.util.share import share
 
 @handle_packet('handshaking', 0x00)
 async def handshake(stream: Stream, packet: Packet) -> tuple:
-    share['states'][remote] = packet.next_state
+    share['states'][stream.remote] = packet.next_state
     return True, stream

4107e8c67161a06641266d7b8d2172c42cac6eff
Milo Weinberg <iapetus011@gmail.com>
2021-01-19 23:56:54 -0500

    make Stream a subclass of StreamWriter

--- pymine/types/stream.py
+++ pymine/types/stream.py
@@ -3,14 +3,25 @@ from __future__ import annotations
 from asyncio import StreamReader, StreamWriter
 
 
-class Stream(StreamReader, StreamWriter):
+class Stream(StreamWriter):
     """Used for reading and writing from/to a connected client, merges a StreamReader and StreamWriter."""
 
     def __init__(self, reader: StreamReader, writer: StreamWriter) -> None:
-        self.original_reader = reader
-        self.original_writer = writer
+        StreamWriter.__init__(self, writer._transport, writer._protocol, writer._reader, writer._loop)
 
-        self.__dict__.update(reader.__dict__)
-        self.__dict__.update(writer.__dict__)
+        self.reader = reader
+        self.writer = writer
 
         self.remote = self.get_extra_info('peername')
+
+    async def read(self, n: int = -1) -> bytes:
+        return self.reader.read(n)
+
+    async def readline(self) -> bytes:
+        return self.reader.readline()
+
+    async def readexactly(self, n: int) -> bytes:
+        return self.reader.readexactly(n)
+
+    async def readuntil(self, separator=b'\n') -> bytes:
+        return self.reader.readuntil(separator)

06fa4aa821cd9e1a0c572b97a4dbc793d98e02f6
Milo Weinberg <iapetus011@gmail.com>
2021-01-19 23:22:02 -0500

    update handlers in handshaking.py

--- pymine/logic/handle/handshaking.py
+++ pymine/logic/handle/handshaking.py
@@ -1,12 +1,11 @@
-from asyncio import StreamReader, StreamWriter
+from pymine.types.packet import Packet
+from pymine.types.stream import Stream
 
 from pymine.api.packet import handle_packet
 from pymine.util.share import share
 
-from pymine.types.packet import Packet
-
 
 @handle_packet('handshaking', 0x00)
-async def handshake(r: 'StreamReader', w: 'StreamWriter', packet: Packet, remote: tuple) -> tuple:
+async def handshake(stream: Stream, packet: Packet) -> tuple:
     share['states'][remote] = packet.next_state
-    return True, r, w
+    return True, stream

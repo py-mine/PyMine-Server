d9fb266a5349fad09a006a2a83ee9586b7f734ea
Milo Weinberg <iapetus011@gmail.com>
2021-01-19 23:09:14 -0500

    update docstring

--- pymine/types/stream.py
+++ pymine/types/stream.py
@@ -2,7 +2,7 @@ from asyncio import StreamReader, StreamWriter
 
 
 class Stream(StreamReader, StreamWriter):
-    """Used for reading and writing from/to a connected client"""
+    """Used for reading and writing from/to a connected client, merges a StreamReader and StreamWriter."""
 
     def __init__(self, reader: StreamReader, writer: StreamWriter) -> None:
         self.__dict__.extend(reader.__dict__)

41c682d609cb923d96acd3c7e116732853e3792d
Milo Weinberg <iapetus011@gmail.com>
2021-01-19 23:38:33 -0500

    update Stream to keep the original StreamWriter and StreamReader

--- pymine/types/stream.py
+++ pymine/types/stream.py
@@ -1,10 +1,14 @@
 from asyncio import StreamReader, StreamWriter
+from __future__ import annotations
 
 
 class Stream(StreamReader, StreamWriter):
     """Used for reading and writing from/to a connected client, merges a StreamReader and StreamWriter."""
 
     def __init__(self, reader: StreamReader, writer: StreamWriter) -> None:
+        self.original_reader = reader
+        self.original_writer = writer
+
         self.__dict__.extend(reader.__dict__)
         self.__dict__.extend(writer.__dict__)
 

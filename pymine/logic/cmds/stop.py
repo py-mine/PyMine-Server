9b9e0868aadd880d8f0f43447ecdafb7c5977f86
Milo Weinberg <iapetus011@gmail.com>
2021-01-22 13:43:42 -0500

    finish move of on_command

--- pymine/logic/cmds/stop.py
+++ pymine/logic/cmds/stop.py
@@ -1,6 +1,6 @@
 from pymine.server import server
 
 
-@server.api.events.on_command(name="stop", node="minecraft.cmd.stop")
+@server.api.commands.on_command(name="stop", node="minecraft.cmd.stop")
 async def stop_server(uuid: str, args: str):
     server.close()

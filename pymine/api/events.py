856c535a2750c9cf20baa729283e73882cdd6f34
Milo Weinberg <iapetus011@gmail.com>
2021-01-22 00:28:24 -0500

    start consolidation to events.py

---
+++ pymine/api/events.py
@@ -0,0 +1,19 @@
+import asyncio
+
+from pymine import server
+
+
+
+def on_packet(state: str, id_: int):
+    def command_deco(func):
+        if not asyncio.iscoroutinefunction(func):
+            raise ValueError("Decorated object must be a coroutine function")
+
+        try:
+            server.api.handlers.packet[state][id_].append(func)
+        except KeyError:
+            server.api.handlers.packet[state][id_] = [func]
+
+        return func
+
+    return command_deco

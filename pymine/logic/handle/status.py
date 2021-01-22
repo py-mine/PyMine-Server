0ab30c386b7b5024942f321d2fdf3d2fad7a1c87
Milo Weinberg <iapetus011@gmail.com>
2021-01-22 13:20:41 -0500

    fix status packet handler (replace references to the share)

--- pymine/logic/handle/status.py
+++ pymine/logic/handle/status.py
@@ -10,21 +10,21 @@ from pymine.server import server
 @server.api.events.on_packet("status", 0x00)
 async def send_status(stream: Stream, packet: Packet) -> tuple:
     data = {
-        "version": {"name": share["version"], "protocol": share["protocol"]},
+        "version": {"name": server.meta.version, "protocol": server.meta.protocol},
         "players": {
-            "max": share["conf"]["max_players"],
-            "online": len(share["states"]),
+            "max": server.conf['max_players'],
+            "online": len(server.cache.states),
             "sample": [
                 {"name": "Iapetus11", "id": "cbcfa252-867d-4bda-a214-776c881cf370"},
                 {"name": "Sh_wayz", "id": "cbcfa252-867d-4bda-a214-776c881cf370"},
                 {"name": "emeralddragonmc", "id": "eb86dc19-c3f5-4aef-a50e-a4bf435a7528"},
             ],
         },
-        "description": {"text": share["conf"]["motd"]},  # a Chat
+        "description": {"text": server.conf["motd"]},  # a Chat
     }
 
-    if share["favicon"]:
-        data["favicon"] = share["favicon"]
+    if server.favicon:
+        data['favicon'] = server.favicon
 
     stream.write(Buffer.pack_packet(status_packets.StatusStatusResponse(data)))
     await stream.drain()

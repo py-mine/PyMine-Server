0959d05045c2413c1cf426d7b2455a3cda7400d2
Milo Weinberg <iapetus011@gmail.com>
2021-01-22 08:28:38 -0500

    move handlers to EventHandlers class

--- pymine/api/events.py
+++ pymine/api/events.py
@@ -5,15 +5,19 @@ class EventHandlers:
     def __init__(self, server):
         self.server = server
 
+        self._packet = {"handshaking": {}, "login": {}, "play": {}, "status": {}}
+        self._server_ready = []
+        self._server_stop = []
+
     def on_packet(self, state: str, id_: int):
         def command_deco(func):
             if not asyncio.iscoroutinefunction(func):
                 raise ValueError("Decorated object must be a coroutine function")
 
             try:
-                self.server.api.handlers.packet[state][id_].append(func)
+                self._packet[state][id_].append(func)
             except KeyError:
-                self.server.api.handlers.packet[state][id_] = [func]
+                self._packet[state][id_] = [func]
 
             return func
 

15c0e847818357f7cd52a27fc6502511946bc577
Milo Weinberg <iapetus011@gmail.com>
2021-01-22 13:45:27 -0500

    fix stop command

--- pymine/logic/cmds/stop.py
+++ pymine/logic/cmds/stop.py
@@ -3,4 +3,4 @@ from pymine.server import server
 
 @server.api.commands.on_command(name="stop", node="minecraft.cmd.stop")
 async def stop_server(uuid: str, args: str):
-    server.close()
+    server.server.close()

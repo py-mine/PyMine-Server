1464c26d000eea638acc06c80482e21b5f9f8ad5
Milo Weinberg <iapetus011@gmail.com>
2021-01-21 22:12:32 -0500

    remove now unecessary code from config.py

--- pymine/data/config.py
+++ pymine/data/config.py
@@ -4,36 +4,28 @@ import yaml
 
 from pymine.util.seeds import string_hash_code, gen_seed
 
-__all__ = (
-    "SERVER_PROPERTIES_DEFAULT",
-    "SERVER_PROPERTIES",
-    "FAVICON",
-)
-
-SERVER_PROPERTIES_DEFAULT = Map(
-    {
-        "debug": True,
-        "server_ip": None,
-        "server_port": 25565,
-        "level_name": "world",
-        "seed": gen_seed(),
-        "gamemode": "survival",
-        "difficulty": "easy",
-        "hardcore": False,
-        "max_players": 20,
-        "online_mode": True,
-        "white_list": True,
-        "motd": "A Minecraft Server",
-        "view_distance": 10,
-        "spawn_protection": 16,
-        "pvp": True,
-        "comp_thresh": 256,
-        "spawn_npcs": True,
-        "spawn_animals": True,
-        "spawn_monsters": True,
-        "generate_structures": True,
-    }
-)
+SERVER_PROPERTIES_DEFAULT = {
+    "debug": True,
+    "server_ip": None,
+    "server_port": 25565,
+    "level_name": "world",
+    "seed": gen_seed(),
+    "gamemode": "survival",
+    "difficulty": "easy",
+    "hardcore": False,
+    "max_players": 20,
+    "online_mode": True,
+    "white_list": True,
+    "motd": "A Minecraft Server",
+    "view_distance": 10,
+    "spawn_protection": 16,
+    "pvp": True,
+    "comp_thresh": 256,
+    "spawn_npcs": True,
+    "spawn_animals": True,
+    "spawn_monsters": True,
+    "generate_structures": True,
+}
 
 
 def load_properties():
@@ -44,7 +36,7 @@ def load_properties():
             properties = yaml.safe_load(f.read())
     except FileNotFoundError:
         with open("server.yml", "w+") as f:
-            f.write(yaml.dump(dict(SERVER_PROPERTIES_DEFAULT)))
+            f.write(yaml.dump(SERVER_PROPERTIES_DEFAULT))
 
     # Check for missing
     if any([(key not in properties) for key in SERVER_PROPERTIES_DEFAULT.keys()]):
@@ -71,7 +63,3 @@ def load_favicon():
             return "data:image/png;base64," + base64.b64encode(favicon.read()).decode("utf-8")
     except FileNotFoundError:
         return None
-
-
-SERVER_PROPERTIES = load_properties()
-FAVICON = load_favicon()

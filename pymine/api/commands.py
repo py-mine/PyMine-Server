ce7a05c878fe86aa6e459473bc32c300ec9611c9
Milo Weinberg <iapetus011@gmail.com>
2021-01-22 09:46:31 -0500

    start oopifying commands.py

--- pymine/api/commands.py
+++ pymine/api/commands.py
@@ -3,47 +3,49 @@ import importlib
 import asyncio
 import os
 
-# loads default built in commands
-def load_commands():  # only loads commands inside cmds folder, not subfolders
-    for file in os.listdir("pymine/logic/cmds"):
-        if file.endswith(".py"):
-            importlib.import_module(f"pymine.logic.cmds.{file[:-3]}")
 
+class CommandHandler:
+    def __init__(self, server):
+        self.server = server
+        self.api = server.api
 
-async def handle_server_command(in_text: str):
-    in_split = in_text.split(" ")
-    cmd = in_split.pop(0)
+    # loads default built in commands
+    @staticmethod
+    def load_commands():  # only loads commands inside cmds folder, not subfolders
+        for file in os.listdir("pymine/logic/cmds"):
+            if file.endswith(".py"):
+                importlib.import_module(f"pymine.logic.cmds.{file[:-3]}")
 
-    args = " ".join(in_split)
+    async def server_command(self, in_text: str):
+        in_split = in_text.split(" ")
+        cmd = in_split.pop(0)
 
-    reg_cmd = registered_commands.get(cmd)
+        args = " ".join(in_split)
 
-    if reg_cmd is not None:
-        cmd_func = reg_cmd[0]
+        reg_cmd = self.api.handlers._commands.get(cmd)
 
-        try:
-            if asyncio.iscoroutinefunction(cmd_func):
+        if reg_cmd is not None:
+            cmd_func = reg_cmd[0]
+
+            try:
                 await cmd_func("server", args)
-            else:
-                cmd_func("server", args)
+            except BaseException as e:
+                logger.error(logger.f_traceback(e))
+        else:
+            logger.warn(f"Invalid/unknown command: {cmd}")
+
+    async def handle_console(self):
+        try:
+            while True:
+                in_text = await aioconsole.ainput(">")
+
+                # In the future, commands *should* be handled async,
+                # however, due to the way the console works rn we can't
+                # without messing up the output
+                # asyncio.create_task(handle_command(in_text))
+
+                await self.server_command(in_text)
+        except (KeyboardInterrupt, asyncio.CancelledError):
+            pass
         except BaseException as e:
             logger.error(logger.f_traceback(e))
-    else:
-        logger.warn(f"Invalid/unknown command: {cmd}")
-
-
-async def handle_server_commands():
-    try:
-        while True:
-            in_text = await aioconsole.ainput(">")
-
-            # In the future, commands *should* be handled async,
-            # however, due to the way the console works rn we can't
-            # without messing up the output
-            # asyncio.create_task(handle_command(in_text))
-
-            await handle_server_command(in_text)
-    except (KeyboardInterrupt, asyncio.CancelledError):
-        pass
-    except BaseException as e:
-        logger.error(logger.f_traceback(e))

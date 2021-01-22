0d956c437ba5e4ea4388dffd4f8c69dc5c2f6c22
Milo Weinberg <iapetus011@gmail.com>
2021-01-22 13:13:08 -0500

    attempt to fix built-in commands

--- pymine/logic/cmds/stop.py
+++ pymine/logic/cmds/stop.py
@@ -1,6 +1,6 @@
 from pymine.server import server
 
 
-@server.api.on_command(name="stop", node="minecraft.cmd.stop")
+@server.api.events.on_command(name="stop", node="minecraft.cmd.stop")
 def stop_server(uuid: str, args: str):
     share["server"].close()

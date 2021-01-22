c456eb725f27d21b14556a49093681a6ebe66cef
Milo Weinberg <iapetus011@gmail.com>
2021-01-22 13:17:41 -0500

    fix imports + names for packet handlers

--- pymine/logic/handle/handshaking.py
+++ pymine/logic/handle/handshaking.py
@@ -1,11 +1,10 @@
 from pymine.types.packet import Packet
 from pymine.types.stream import Stream
 
-from pymine.api.packet import handle_packet
-from pymine.util.share import share
+from pymine.server import server
 
 
-@handle_packet("handshaking", 0x00)
+@server.api.events.on_packet("handshaking", 0x00)
 async def handshake(stream: Stream, packet: Packet) -> tuple:
     share["states"][stream.remote] = packet.next_state
     return True, stream

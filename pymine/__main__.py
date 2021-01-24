aa05a71813f4d5f7c86fcff3bc41341538481a8a
Milo Weinberg <iapetus011@gmail.com>
2021-01-24 12:46:46 -0500

    simplify some path related code

--- pymine/__main__.py
+++ pymine/__main__.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # ensure the pymine modules are accessible
-sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from pymine.util.logging import Logger, task_exception_handler
 import pymine.server

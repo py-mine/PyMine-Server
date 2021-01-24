d1f4a4aa11cc00c4e2151a18a9fe5a517674ecc3
Milo Weinberg <iapetus011@gmail.com>
2021-01-24 12:32:34 -0500

    update some code in data_gen.py that ensures correct working dir

--- pymine/util/data_gen.py
+++ pymine/util/data_gen.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.getcwd())
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
 from pymine.data.packet_map import PACKET_MAP, PACKET_MAP_CLIENTBOUND
 

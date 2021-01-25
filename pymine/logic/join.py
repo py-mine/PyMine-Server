5f2bb7a46a87a315fa390e10b086e62fcf7b917e
Milo Weinberg <iapetus011@gmail.com>
2021-01-25 15:36:53 -0500

    rename logic/play.py to logic/join.py

---
+++ pymine/logic/join.py
@@ -0,0 +1,11 @@
+from nbt import nbt
+import hashlib
+import time
+
+from pymine.types.packet import Packet
+from pymine.types.stream import Stream
+
+from pymine.server import server
+
+
+async def join(stream: Stream, packet: Packet)

6608be75130cebf0a20a3431dd5c2015888560bc
Milo Weinberg <iapetus011@gmail.com>
2021-01-25 18:28:10 -0500

    add basic tag type

---
+++ pymine/types/nbt.py
@@ -0,0 +1,21 @@
+import struct
+
+
+class TAG:
+    id: int = -1
+
+    def __init__(self) -> None:
+        self.id: int = self.__class__.id
+
+    @staticmethod
+    def unpack(f: str, buf) -> object:
+        unpacked = struct.unpack(">" + f, buf.read(struct.calcsize(f)))
+
+        if len(unpacked) == 1:
+            return unpacked[0]
+
+        return unpacked
+
+    @staticmethod
+    def pack(f: str, *data: object) -> bytes:
+        return struct.pack(">" + f, *data)

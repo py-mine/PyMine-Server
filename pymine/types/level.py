f86869bf104c44c3fe001ff2db0646b86e28b9d5
Milo Weinberg <iapetus011@gmail.com>
2021-01-26 20:45:01 -0500

    fix missing self arg

--- pymine/types/level.py
+++ pymine/types/level.py
@@ -2,6 +2,6 @@ import nbt
 
 
 class LevelData:
-    def __init__(data: nbt.TAG) -> None:
+    def __init__(self, data: nbt.TAG) -> None:
         self.data = data
         self.allow_commands = data

8b5e4228298c99c1ab8d4c4023d3e4af58e9716c
Milo Weinberg <iapetus011@gmail.com>
2021-01-27 10:19:42 -0500

    add test_dumping_nantest

--- tests/test_nbt.py
+++ tests/test_nbt.py
@@ -18,3 +18,13 @@ def test_dumping_bigtest():  # tests that the data dumping/tag packing works pro
         tag_bytes = tag.pack()
 
         assert tag_bytes == buf_old.read()
+
+
+def test_dumping_nantest():  # tests that the data dumping/tag packing works properly for a file that uses NaN
+    with open(os.path.join("tests", "sample_nbt", "nantest.nbt"), 'rb') as nbt_file:
+        buf_old = Buffer(gzip.decompress(nbt_file.read()))
+
+        tag = nbt.unpack(Buffer(buf_old.buf))
+        tag_bytes = tag.pack()
+
+        assert tag_bytes == buf_old.read()

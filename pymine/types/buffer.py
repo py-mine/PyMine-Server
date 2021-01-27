78fb6a79b38ad2c28f3e70868de1ed13aa9b1fa2
Milo Weinberg <iapetus011@gmail.com>
2021-01-26 21:23:33 -0500

    remove nbt import from Buffer

--- pymine/types/buffer.py
+++ pymine/types/buffer.py
@@ -6,7 +6,6 @@ import zlib
 
 from pymine.types.packet import Packet
 from pymine.types.chat import Chat
-import pymine.types.nbt as nbt
 
 from pymine.data.registry import ITEM_REGISTRY
 import pymine.data.misc as misc_data

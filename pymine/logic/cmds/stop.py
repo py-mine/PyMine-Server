5d1f28cca7830e82a13573780795085ade7f3f90
Milo Weinberg <iapetus011@gmail.com>
2021-01-27 16:27:49 -0500

    stop cmd should wait for server to close

--- pymine/logic/cmds/stop.py
+++ pymine/logic/cmds/stop.py
@@ -4,3 +4,4 @@ from pymine.server import server
 @server.api.commands.on_command(name="stop", node="minecraft.cmd.stop")
 async def stop_server(uuid: str, args: str):
     server.server.close()
+    await server.server.wait_closed()

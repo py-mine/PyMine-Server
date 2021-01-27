4bc9bdb15012a82482da9c3a46cfd5d1700d186f
Milo Weinberg <iapetus011@gmail.com>
2021-01-27 10:27:45 -0500

    rename stuff

--- tests/test_nbt.py
+++ tests/test_nbt.py
@@ -12,19 +12,19 @@ def test_loading_bigtest():  # tests that loading bigtest.nbt works without erro
 
 def test_dumping_bigtest():  # tests that the data dumping/tag packing works properly
     with open(os.path.join("tests", "sample_nbt", "bigtest.nbt"), 'rb') as nbt_file:
-        buf_old = Buffer(gzip.decompress(nbt_file.read()))
+        buf = Buffer(gzip.decompress(nbt_file.read()))
 
-        tag = nbt.unpack(Buffer(buf_old.buf))
+        tag = nbt.unpack(Buffer(buf.buf))
         tag_bytes = tag.pack()
 
-        assert tag_bytes == buf_old.read()
+        assert tag_bytes == buf.read()
 
 
 def test_dumping_nantest():  # tests that the data dumping/tag packing works properly for a file that uses NaN
     with open(os.path.join("tests", "sample_nbt", "nantest.nbt"), 'rb') as nbt_file:
-        buf_old = Buffer(gzip.decompress(nbt_file.read()))
+        buf = Buffer(gzip.decompress(nbt_file.read()))
 
-        tag = nbt.unpack(Buffer(buf_old.buf))
+        tag = nbt.unpack(Buffer(buf.buf))
         tag_bytes = tag.pack()
 
-        assert tag_bytes == buf_old.read()
+        assert tag_bytes == buf.read()

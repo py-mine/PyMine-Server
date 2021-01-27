bf9fabcb8b4a953a9fe1bef614b1b58d4ea8ecce
Milo Weinberg <iapetus011@gmail.com>
2021-01-26 21:17:55 -0500

    add __all__

--- pymine/types/nbt.py
+++ pymine/types/nbt.py
@@ -5,6 +5,24 @@ import gzip
 
 from pymine.types.buffer import Buffer
 
+__all__ = (
+    'TAG',
+    'TAG_End',
+    'TAG_Byte',
+    'TAG_Short',
+    'TAG_Int',
+    'TAG_Long',
+    'TAG_Float',
+    'TAG_Double',
+    'TAG_Byte_Array',
+    'TAG_String',
+    'TAG_List',
+    'TAG_Compound',
+    'TAG_Int_Array',
+    'TAG_Long_Array',
+    'TYPES',
+)
+
 TYPES = []
 
 

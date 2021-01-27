d355304420cb5df4921e526904630d638d19bea2
Milo Weinberg <iapetus011@gmail.com>
2021-01-27 16:14:12 -0500

    attempt to use uvloop + log if is

--- pymine/__main__.py
+++ pymine/__main__.py
@@ -6,6 +6,14 @@ if not sys.implementation.version[:3] >= (3, 7, 9):  # Ensure user is on correct
     print("You are not on a supported version of Python. Please update to version 3.7.9 or later.")
     exit(1)
 
+try:
+    import uvloop
+    uvloop.install()
+except BaseException:
+    pass
+else:
+    uvloop = None
+
 # ensure the pymine modules are accessible
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -15,6 +23,9 @@ import pymine.server
 if __name__ == "__main__":
     logger = Logger()  # debug status will be set later after config is loaded
 
+    if uvloop:
+        logger.debug('Using uvloop as the event loop.')
+
     loop = asyncio.get_event_loop()
     loop.set_exception_handler(task_exception_handler)
 
@@ -23,7 +34,7 @@ if __name__ == "__main__":
 
     try:
         loop.run_until_complete(server.start())
-    except asyncio.CancelledError:
+    except (asyncio.CancelledError, KeyboardInterrupt):
         pass
     except BaseException as e:
         logger.critical(logger.f_traceback(e))

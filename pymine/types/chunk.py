ab26d0783aeed376a86a1eebb9388a4e34f2745f
Milo Weinberg <iapetus011@gmail.com>
2021-01-28 13:05:18 -0500

    make chunk subclass a dict

--- pymine/types/chunk.py
+++ pymine/types/chunk.py
@@ -4,12 +4,13 @@ from pymine.types.buffer import Buffer
 import pymine.types.nbt as nbt
 
 
-class Chunk:
+class Chunk(dict):
     def __init__(self, tag: nbt.TAG, timestamp: int) -> None:
-        self.data_version = tag["DataVersion"]
-        self.level = tag["Level"]
+        dict.__init__(self, tag['Level'])
 
-        self.chunk_x = self.level["xPos"].data
-        self.chunk_z = self.level["zPos"].data
+        self.data_version = tag["DataVersion"]
+
+        self.chunk_x = self["xPos"].data
+        self.chunk_z = self["zPos"].data
 
         self.timestamp = timestamp

f397aa3cf536bd922320e8e1f407769a76344a09
Milo Weinberg <iapetus011@gmail.com>
2021-01-31 16:21:03 -0500

    make load_worlds() more modular

--- pymine/types/world.py
+++ pymine/types/world.py
@@ -22,17 +22,16 @@ def region_file_name(region_x: int, region_z: int) -> str:
     return ".".join(("r", str(region_x), str(region_z), "mca"))
 
 
-def load_worlds(server, region_cache_max_per: int) -> dict:
-    level_name = server.conf["level_name"]
+def load_worlds(logger, level_name: str, region_cache_max_per: int) -> dict:
     worlds = {}
 
-    server.logger.info(f"Loading worlds for level {level_name}...")
+    logger.info(f"Loading worlds for level {level_name}...")
 
     for ext in ("", "_nether", "_the_end"):
         name = level_name + ext
         worlds[name] = World(server, name, os.path.join("worlds", name), region_cache_max_per)
 
-    server.logger.info(f'Loaded worlds: {", ".join(worlds.keys())}.')
+    logger.info(f'Loaded worlds: {", ".join(worlds.keys())}.')
 
     return worlds
 

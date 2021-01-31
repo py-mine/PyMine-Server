ae4ce226e06ea7c471d4f687df4e72a70569c80a
Milo Weinberg <iapetus011@gmail.com>
2021-01-31 16:31:56 -0500

    move player io from types/player.py to logic/playerio.py

--- pymine/types/player.py
+++ pymine/types/player.py
@@ -1,20 +1,4 @@
-import uuid
-import os
-
 import pymine.types.nbt as nbt
-
-
-def fetch_player(level_name: str, uuid_: uuid.UUID) -> None:
-    playerdata_dir = os.path.join("worlds", level_name, "playerdata")
-
-    uuid_ = str(uuid_)
-    file = os.path.join(playerdata_dir, uuid_ + ".dat")
-
-    if not os.path.isfile(file):
-        raise NotImplementedError("Player creation hasn't been implemented yet...")
-
-    with open(file, "rb") as f:
-        return Player(nbt.TAG_Compound.unpack(Buffer(f.read())))
 
 
 class Player:

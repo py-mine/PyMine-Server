6d2848fcd4b4fd5a166cd6b11a4e914f98ce7dfd
Milo Weinberg <iapetus011@gmail.com>
2021-01-31 16:48:56 -0500

    have from_nbt() return a Player instance

--- pymine/types/player.py
+++ pymine/types/player.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import pymine.types.nbt as nbt
 
 
@@ -37,48 +39,36 @@ class Player:
     ) -> None:
         pass
 
-    def from_nbt(self, tag: nbt.TAG) -> None:
-        self.data_version = tag.get("DataVersion")  # int
-
-        self.player_game_type = tag.get("playerGameType")  # int
-        self.prev_player_game_type = tag.get("previousPlayerGameType")  # int
-
-        self.score = tag.get("Score")  # int
-
-        self.dimension = tag.get("Dimension")  # str
-
-        self.selected_item_slot = tag.get("SelectedItemSlot")  # int
-        self.selected_item = tag.get("SelectedItem")  # compound
-
-        self.spawn_dimension = tag.get("SpawnDimension")  # str
-        self.spawn_x = tag.get("SpawnX")  # int
-        self.spawn_y = tag.get("SpawnY")  # int
-        self.spawn_forced = tag.get("SpawnForced")  # byte
-
-        self.sleep_timer = tag.get("SleepTimer")  # short
-
-        self.food_level = tag.get("foodLevel")  # int
-        self.food_exhaustion_level = tag.get("foodExhaustionLevel")  # float
-        self.food_saturation_level = tag.get("foodSaturationLevel")  # float
-        self.food_tick_timer = tag.get("foodTickTimer")  # int
-
-        self.xp_level = tag.get("XpLevel")  # int
-        self.xp_percent = tag.get("XpP")  # float
-        self.xp_total = tag.get("XpTotal")  # int
-        self.xp_seed = tag.get("XpSeed")  # int
-
-        self.inventory = tag.get("Inventory")  # list of compound tags
-        self.ender_chest = tag.get("EnderItems")  # list of compound tags
-
-        self.abilities = tag.get("abilities")  # compound
-
-        self.entered_nether_position = tag.get("enteredNetherPosition")  # compound
-
-        self.root_vehicle = tag.get("RootVehicle")  # compound
-
-        self.shoulder_entity_left = tag.get("ShoulderEntityLeft")  # compound
-        self.shoulder_entity_right = tag.get("ShoulderEntityRight")  # compound
-
-        self.seen_credits = tag.get("seenCredits")  # byte
-
-        self.recipe_book = tag.get("recipeBook")  # compound
+    @classmethod
+    def from_nbt(cls, tag: nbt.TAG) -> Player:
+        return cls(
+            data_version=tag.get("DataVersion"),  # int
+            player_game_type=tag.get("playerGameType"),  # int
+            prev_player_game_type=tag.get("previousPlayerGameType"),  # int
+            score=tag.get("Score"),  # int
+            dimension=tag.get("Dimension"),  # str
+            selected_item_slot=tag.get("SelectedItemSlot"),  # int
+            selected_item=tag.get("SelectedItem"),  # compound
+            spawn_dimension=tag.get("SpawnDimension"),  # str
+            spawn_x=tag.get("SpawnX"),  # int
+            spawn_y=tag.get("SpawnY"),  # int
+            spawn_forced=tag.get("SpawnForced"),  # byte
+            sleep_timer=tag.get("SleepTimer"),  # short
+            food_level=tag.get("foodLevel"),  # int
+            food_exhaustion_level=tag.get("foodExhaustionLevel"),  # float
+            food_saturation_level=tag.get("foodSaturationLevel"),  # float
+            food_tick_timer=tag.get("foodTickTimer"),  # int
+            xp_level=tag.get("XpLevel"),  # int
+            xp_percent=tag.get("XpP"),  # float
+            xp_total=tag.get("XpTotal"),  # int
+            xp_seed=tag.get("XpSeed"),  # int
+            inventory=tag.get("Inventory"),  # list of compound tags
+            ender_chest=tag.get("EnderItems"),  # list of compound tags
+            abilities=tag.get("abilities"),  # compound
+            entered_nether_position=tag.get("enteredNetherPosition"),  # compound
+            root_vehicle=tag.get("RootVehicle"),  # compound
+            shoulder_entity_left=tag.get("ShoulderEntityLeft"),  # compound
+            shoulder_entity_right=tag.get("ShoulderEntityRight"),  # compound
+            seen_credits=tag.get("seenCredits"),  # byte
+            recipe_book=tag.get("recipeBook"),  # compound
+        )

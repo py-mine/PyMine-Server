8d5f1d1b733eabb2d25ac20d1a142b5e69ba0577
Milo Weinberg <iapetus011@gmail.com>
2021-02-01 12:55:37 -0500

    update Player

--- pymine/types/player.py
+++ pymine/types/player.py
@@ -6,9 +6,19 @@ import pymine.types.nbt as nbt
 
 
 class Player:
-    def __init__(self, entity_id: int, data: nbt.TAG):
+    def __init__(self, entity_id: int, data: nbt.TAG) -> None:
         self.entity_id = entity_id
         self.data = data
 
         self.uuid = uuid.UUID(bytes=struct.pack(">iiii", *data["uuid"]))
         self.x, self.y, self.z = self.pos = data["Pos"]
+
+        self.username = None
+        self.remote = None
+
+    def set_meta(self, username: str, remote: tuple) -> None:
+        self.username = username
+        self.remote = remote
+
+    def __str__(self) -> str:
+        return self.username

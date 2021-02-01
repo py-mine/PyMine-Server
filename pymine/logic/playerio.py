8d6284cd97bbe5e9681e53c2e76e6df4b32d49e0
Milo Weinberg <iapetus011@gmail.com>
2021-02-01 10:53:39 -0500

    add PlayerDataIO class

--- pymine/logic/playerio.py
+++ pymine/logic/playerio.py
@@ -6,14 +6,24 @@ from pymine.types.player import Player
 import pymine.types.nbt as nbt
 
 
-def fetch_player(level_name: str, uuid_: uuid.UUID) -> None:
-    playerdata_dir = os.path.join("worlds", level_name, "playerdata")
+class PlayerDataIO:
+    def __init__(self, level_name: str):
+        self.level_name = level_name
+        self.data_dir = os.path.join('worlds', level_name, 'playerdata')
+        self.cache = {}
 
-    uuid_ = str(uuid_)
-    file = os.path.join(playerdata_dir, uuid_ + ".dat")
+    async def fetch_player(self, uuid_: uuid.UUID) -> Player:
+        try:
+            return self.cache[int(uuid_)]
+        except KeyError:
+            file = os.path.join(self.data_dir, f"{uuid_}.dat")
 
-    if not os.path.isfile(file):
-        raise NotImplementedError("Player creation hasn't been implemented yet...")
+            if not os.path.isfile(file):
+                raise NotImplementedError("Player creation hasn't been implemented yet...")
 
-    with open(file, "rb") as f:
-        return Player(nbt.TAG_Compound.unpack(Buffer(f.read())))
+            with open(file, "rb") as f:
+                player = Player.from_nbt(nbt.TAG_Compound.unpack(Buffer(f.read())))
+
+                self.cache[int(uuid_)] = player
+
+                return player

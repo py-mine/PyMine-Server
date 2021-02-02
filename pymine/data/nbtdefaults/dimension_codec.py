40343495ff1d1d1d5246b13c475c5d770edd8b7b
Milo Weinberg <iapetus011@gmail.com>
2021-02-02 12:33:46 -0500

    fix typo (: should've been ,)

--- pymine/data/nbtdefaults/dimension_codec.py
+++ pymine/data/nbtdefaults/dimension_codec.py
@@ -23,7 +23,7 @@ def new_dim_codec_nbt() -> nbt.TAG_Compound:
     return nbt.TAG_Compound('', [
         nbt.TAG_Compound('minecraft:dimension_type', [  # dimension type registry
             nbt.TAG_String('type', 'minecraft:dimension_type'),
-            nbt.TAG_List('value': [  # list of compounds
+            nbt.TAG_List('value', [  # list of compounds
                 nbt.TAG_Compound(None, [
                     nbt.TAG_String('name', 'minecraft:overworld'),
                     nbt.TAG_Int('id', 0),

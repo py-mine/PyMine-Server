a1998688139f61f3f36dab3328b6539e685f8d30
Milo Weinberg <iapetus011@gmail.com>
2021-02-02 11:43:19 -0500

    start reworking join()

--- pymine/logic/join.py
+++ pymine/logic/join.py
@@ -14,20 +14,19 @@ from pymine.util.misc import seed_hash
 async def join(server, stream: Stream, uuid_: uuid.UUID, username: str) -> None:
     level_name = server.conf["level_name"]
 
-    player = server.playerio.fetch_player()
+    player = server.playerio.fetch_player(uuid_)
 
     server.send_packet(
         stream,
         packets_player.PlayJoinGame(
-            entity_id,
-            server.conf["hardcore"],
-            0,  # Should be current gamemode
-            -1,  # Should be previous gamemode
-            # Should be actual world names
-            [f"minecraft:{lvl_name}", f"minecraft:{lvl_name}_nether", f"minecraft:{lvl_name}_the_end"],
+            player.entity_id,
+            server.conf["hardcore"],  # whether world is hardcore or not
+            player.data['playerGameType'],  # gamemode
+            player.data['previousPlayerGameType'],  # previous gamemode
+            [level_name, f"{level_name}_nether", f"{level_name}_the_end"],  # world names
             nbt.TAG_Int(name="bruh", value=1),
             nbt.TAG_Int(name="bruh", value=1),
-            f"minecraft:{lvl_name}",  # should be actual current world name
+            player.data['Dimension'],  # Dimension/player the player is spawning in to
             seed_hash(server.conf["seed"]),
             server.conf["max_players"],
             server.conf["view_distance"],

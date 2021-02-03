3058be1f246474b3bd76c1ccba39a4cadee350f1
Milo Weinberg <iapetus011@gmail.com>
2021-02-02 23:46:18 -0500

    add send_server_difficulty

--- pymine/logic/join.py
+++ pymine/logic/join.py
@@ -3,11 +3,14 @@ import time
 import uuid
 
 from pymine.types.packet import Packet
+from pymine.types.player import Player
 from pymine.types.stream import Stream
+from pymine.types.world import World
 import pymine.types.nbt as nbt
 
 from pymine.data.default_nbt.dimension_codec import new_dim_codec_nbt, get_dimension_data
 
+import pymine.net.packets.play.difficulty as packets_difficulty
 import pymine.net.packets.play.plugin_msg as packets_plugin
 import pymine.net.packets.play.player as packets_player
 
@@ -16,16 +19,17 @@ from pymine.util.misc import seed_hash
 
 # Used to finish the process of allowing a client to actually enter the server
 async def join(server, stream: Stream, uuid_: uuid.UUID, username: str) -> None:
-    await send_join_game_packet(server, stream, uuid_)
+    player = server.playerio.fetch_player(uuid_)
+    world = server.worlds[player.data["Dimension"]]  # the world player *should* be spawning into
+
+    await send_join_game_packet(server, stream, world, player)
     await send_server_brand(server, stream)
+    await send_server_difficulty(server, stream, world)
 
 
 # crucial info pertaining to the world and player status
-async def send_join_game_packet(server, stream: Stream, uuid_: uuid.UUID) -> None:
+async def send_join_game_packet(server, stream: Stream, world: World, player: Player) -> None:
     level_name = server.conf["level_name"]  # level name, i.e. Xenon
-
-    player = server.playerio.fetch_player(uuid_)
-    world = server.worlds[player.data["Dimension"]]  # the world player *should* be spawning into
 
     await server.send_packet(
         stream,
@@ -53,3 +57,7 @@ async def send_join_game_packet(server, stream: Stream, uuid_: uuid.UUID) -> Non
 # send server branch via plugin channels
 async def send_server_brand(server, stream: Stream) -> None:
     await server.send_packet(stream, packets_plugin.PlayPluginMessageClientBound("minecraft:brand", server.pymine))
+
+
+async def send_server_difficulty(server, stream: Stream, world) -> None:
+    await server.send_packet(stream, packets_difficulty.PlayServerDifficulty(world.data['Difficulty'], world.data['DifficultyLocked']))

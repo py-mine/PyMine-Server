3e85e1b221b367d534cd8d18eae3af31e45676e9
Milo Weinberg <iapetus011@gmail.com>
2021-02-03 08:59:18 -0500

    add SingleByteBitField

---
+++ pymine/types/bitfield.py
@@ -0,0 +1,22 @@
+
+class SingleByteBitField:
+    def __init__(self, length: int, field: int):
+        self.length = length
+        self.field = field
+
+    @classmethod
+    def from_bools(cls, *values: bool) -> SingleByteBitField:
+        field = 0
+
+        for i, v in enumerate(values):
+            i = 2**i
+
+            if v:
+                field |= i
+            else:
+                field &= ~i
+
+        return cls(len(values), field)
+
+    def unpack(self) -> tuple:
+        return tuple((self.table & i & self.length != 0) for i in range(self.length))

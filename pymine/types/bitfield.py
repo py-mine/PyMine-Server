89b773fc01fae2bb013652da011ae0cc15f21428
Milo Weinberg <iapetus011@gmail.com>
2021-02-03 09:10:16 -0500

    fix typo

--- pymine/types/bitfield.py
+++ pymine/types/bitfield.py
@@ -21,7 +21,7 @@ class BoolBitField:
         return cls(len(values), field)
 
     def unpack(self) -> tuple:
-        return tuple((self.table & i & self.length != 0) for i in range(self.length))
+        return tuple((self.field & i & self.length != 0) for i in range(self.length))
 
     def __str__(self):
         return f'BoolBitField(0x{self.field:0X}, length={self.length})'

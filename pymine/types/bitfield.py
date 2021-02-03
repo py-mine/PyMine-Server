6c9cfa2b0715d34a15fb9bca0448a2adeac799d4
Milo Weinberg <iapetus011@gmail.com>
2021-02-03 09:53:31 -0500

    move BitField above BoolBitField

--- pymine/types/bitfield.py
+++ pymine/types/bitfield.py
@@ -1,33 +1,6 @@
 from __future__ import annotations
 
 
-# class BoolBitField:
-#     def __init__(self, length: int, field: int):
-#         self.length = length
-#         self.field = field
-#
-#     @classmethod
-#     def from_values(cls, *values: bool) -> BoolBitField:
-#         field = 0
-#
-#         for i, v in enumerate(values):
-#             if v:
-#                 field |= 2 ** i
-#             else:
-#                 field &= ~(2 ** i)
-#
-#         return cls(len(values), field)
-#
-#     def get_values(self) -> tuple:
-#         return tuple(bool((self.field >> i) & 1) for i in range(self.length))
-#
-#     def __str__(self):
-#         return str(self.unpack())
-#
-#     def __repr__(self):
-#         return f"BitField(0x{self.field:0X}, length={self.length})"
-
-
 class BitField:
     def __init__(self, length: int, field: int):
         self.length = length
@@ -53,3 +26,30 @@ class BitField:
 
     def __repr__(self):
         return f"BitField(0x{self.field:0X}, length={self.length})"
+
+
+# class BoolBitField:
+#     def __init__(self, length: int, field: int):
+#         self.length = length
+#         self.field = field
+#
+#     @classmethod
+#     def from_values(cls, *values: bool) -> BoolBitField:
+#         field = 0
+#
+#         for i, v in enumerate(values):
+#             if v:
+#                 field |= 2 ** i
+#             else:
+#                 field &= ~(2 ** i)
+#
+#         return cls(len(values), field)
+#
+#     def get_values(self) -> tuple:
+#         return tuple(bool((self.field >> i) & 1) for i in range(self.length))
+#
+#     def __str__(self):
+#         return str(self.unpack())
+#
+#     def __repr__(self):
+#         return f"BitField(0x{self.field:0X}, length={self.length})"

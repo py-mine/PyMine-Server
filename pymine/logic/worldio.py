e8abff301e68de2d6a0a5073fe7630de79a4f7c1
Milo Weinberg <iapetus011@gmail.com>
2021-02-02 22:48:46 -0500

    update debug message

--- pymine/logic/worldio.py
+++ pymine/logic/worldio.py
@@ -6,7 +6,7 @@ from pymine.types.world import World
 async def load_worlds(server, level_name: str, region_cache_max_per: int) -> dict:
     worlds = {}
 
-    server.logger.info(f"Loading worlds for level {level_name}...")
+    server.logger.info(f"Loading default worlds for level {level_name}...")
 
     # Worlds, in the world dict (server.worlds) are indexed by their dimension type, this, however can change in the future
     # worlds are passed their file name / folder name (i.e. level_name + ext) via their constructors

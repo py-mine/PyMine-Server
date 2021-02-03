4fbacc28e509bc0baeccc37884107df9b6301d9c
Milo Weinberg <iapetus011@gmail.com>
2021-02-03 12:34:50 -0500

    fix usage of nbt tags in send_join_game_packet()

--- pymine/logic/handle/join.py
+++ pymine/logic/handle/join.py
@@ -49,18 +49,18 @@ async def send_join_game_packet(stream: Stream, world: World, player: Player) ->
         packets_player.PlayJoinGame(
             player.entity_id,
             server.conf["hardcore"],  # whether world is hardcore or not
-            player.data["playerGameType"],  # gamemode
-            player.data["previousPlayerGameType"],  # previous gamemode
+            player.data["playerGameType"].data,  # gamemode
+            player.data["previousPlayerGameType"].data,  # previous gamemode
             [level_name, f"{level_name}_nether", f"{level_name}_the_end"],  # world names
             new_dim_codec_nbt(),  # Shouldn't change unless CUSTOM DIMENSIONS are added fml
             # This is like the the dimension data for the dim the player is currently spawning into
-            get_dimension_data(player.data["Dimension"]),  # player.data['Dimension'] should be like minecraft:overworld
+            get_dimension_data(player.data["Dimension"].data),  # player.data['Dimension'] should be like minecraft:overworld
             server.conf["level_name"],  # level name of the world the player is spawning into
             seed_hash(server.conf["seed"]),
             server.conf["max_players"],
             server.conf["view_distance"],
             (not server.conf["debug"]),
-            (world.data["GameRules"]["doImmediateRespawn"] != "true"),  # (not doImmediateRespawn gamerule)
+            (world.data["GameRules"]["doImmediateRespawn"].data != "true"),  # (not doImmediateRespawn gamerule)
             False,  # If world is a debug world iirc
             False,  # Should be true if world is superflat
         ),

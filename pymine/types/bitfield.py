d9cc1a04b0aef54e15b60b7d796458c37d0dd8aa
Milo Weinberg <iapetus011@gmail.com>
2021-02-03 09:30:00 -0500

    actually fix it

--- pymine/types/bitfield.py
+++ pymine/types/bitfield.py
@@ -1,27 +1,25 @@
 from __future__ import annotations
 
 
-class BoolBitField:
+class BitField:
     def __init__(self, length: int, field: int):
         self.length = length
         self.field = field
 
     @classmethod
-    def from_bools(cls, *values: bool) -> BoolBitField:
+    def from_values(cls, *values: bool) -> BitField:
         field = 0
 
         for i, v in enumerate(values):
-            i = 2 ** i
-
             if v:
-                field |= i
+                field |= 2**i
             else:
-                field &= ~i
+                field &= ~(2**i)
 
         return cls(len(values), field)
 
     def unpack(self) -> tuple:
-        return tuple((self.field & i & self.length != 0) for i in range(self.length))
+        return tuple((self.field >> i) & 1 for i in range(self.length))
 
     def __str__(self):
         return f"BoolBitField(0x{self.field:0X}, length={self.length})"

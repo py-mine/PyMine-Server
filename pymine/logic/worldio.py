ff4636c11e75334b8541b6604ecbf77c0169e769
Milo Weinberg <iapetus011@gmail.com>
2021-02-04 16:30:36 -0500

    add implementation of AbstractChunkIO (ChunkIO)

--- pymine/logic/worldio.py
+++ pymine/logic/worldio.py
@@ -1,6 +1,14 @@
+import aiofile
+import struct
+import zlib
 import os
 
+from pymine.types.buffer import Buffer
+from pymine.types.chunk import Chunk
 from pymine.types.world import World
+import pymine.types.nbt as nbt
+
+from pymine.api.abc import AbstractChunkIO
 
 # Setup world dict and load basic level data for each world
 async def load_worlds(server, level_name: str, region_cache_max_per: int) -> dict:
@@ -21,3 +29,53 @@ async def load_worlds(server, level_name: str, region_cache_max_per: int) -> dic
     server.logger.info(f'Loaded default worlds: {", ".join([w.name for w in worlds.values()])}.')
 
     return worlds
+
+
+class ChunkIO(AbstractChunkIO):
+    @staticmethod
+    def calc_offset(chunk_x: int, chunk_z: int) -> int:
+        return 4 * ((chunk_x & 31) + (chunk_z & 31) * 32)
+
+    @staticmethod
+    def find_chunk(location: int) -> tuple:
+        offset = (loc >> 8) & 0xFFFFFF
+        size = loc & 0xFF
+
+        return offset * 4096, size * 4096
+
+    @classmethod
+    def fetch_chunk(cls, world_name: str, chunk_x: int, chunk_z: int) -> Chunk:
+        rx, ry = chunk_x // 32, chunk_z // 32
+        region_path = os.path.join('worlds', world_name, 'region', f'r.{rx}.{ry}.mca')
+
+        loc_table_loc = cls.calc_offset(chunk_x, chunk_z)
+
+        with open(region_path, 'rb') as region_file:
+            region_file.seek(loc_table_loc)
+
+            offset, length = cls.find_chunk(region_file.read(4))
+
+            region_file.seek(loc_table_loc + 4096)
+            timestamp = region_file.read(4)
+
+            region_file.seek(offset+5)
+
+            return Chunk(nbt.TAG_Compound.unpack(Buffer(zlib.decompress(region_file.read(length-5)))), struct.unpack('>i', timestamp))
+
+    @classmethod
+    async def fetch_chunk_async(cls, world_name: str, chunk_x: int, chunk_z: int) -> Chunk:
+        rx, ry = chunk_x // 32, chunk_z // 32
+        region_path = os.path.join('worlds', world_name, 'region', f'r.{rx}.{ry}.mca')
+
+        loc_table_loc = cls.calc_offset(chunk_x, chunk_z)
+
+        async with aiofile.async_open(region_path, 'rb') as region_file:
+            region_file.seek(loc_table_loc)
+
+            offset, length = cls.find_chunk(await region_file.read(4))
+
+            region_file.seek(loc_table_loc + 4096)
+            timestamp = await region_file.read(4)
+
+            region_file.seek(offset+5)
+            return Chunk(nbt.TAG_Compound.unpack(Buffer(zlib.decompress(await region_file.read(length-5)))), struct.unpack('>i', timestamp))

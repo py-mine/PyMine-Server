4619f081a8d8c8dab9049b692ddea4ba79574548
Milo Weinberg <iapetus011@gmail.com>
2021-02-04 12:41:26 -0500

    update dimension_codec.py

--- pymine/data/default_nbt/dimension_codec.py
+++ pymine/data/default_nbt/dimension_codec.py
@@ -5,7 +5,7 @@ from pymine.types.buffer import Buffer
 import pymine.types.nbt as nbt
 
 with open(os.path.join("pymine", "data", "default_nbt", "dimension_codec.nbt"), "rb") as dim_codec_file:
-    DEFAULT_DIM_CODEC_NBT = nbt.unpack(Buffer(dim_codec_file.read()))
+    DEFAULT_DIM_CODEC_NBT = nbt.unpack(Buffer(dim_codec_file.read()), root_is_full=False)
 
 
 def new_dim_codec_nbt() -> nbt.TAG_Compound:

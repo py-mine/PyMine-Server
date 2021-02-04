f1ec38ad922460cba033bfd5a0cdf3cbc8a82c55
Milo Weinberg <iapetus011@gmail.com>
2021-02-04 13:09:27 -0500

    add the AbstractWorldGenerator class

---
+++ pymine/api/world_gen.py
@@ -0,0 +1,16 @@
+
+class AbstractWorldGenerator:
+    """Abstract class used to create a world generator.
+
+    :param int seed: The seed used to generate the world.
+    :ivar seed:
+    """
+
+    def __init__(self, seed: int) -> None:
+        self.seed = seed
+
+    def generate_chunk(self, chunk_x: int, chunk_z: int) -> Chunk:
+        raise NotImplementedError(self.__class__.__name__)
+
+    def generate_region(self, region_x: int, region_z: int) -> Region:
+        raise NotImplementedError(self.__class__.__name__)

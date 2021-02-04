dd11b0e6da3faa9e370c5547da42d7a2d71855aa
Milo Weinberg <iapetus011@gmail.com>
2021-02-04 15:14:27 -0500

    update AbstractWorldGenerator

--- pymine/api/abc.py
+++ pymine/api/abc.py
@@ -1,19 +1,9 @@
-from pymine.types.region import Region
 from pymine.types.chunk import Chunk
 
 
 class AbstractWorldGenerator:
-    """Abstract class used to create a world generator.
+    """Abstract class used to create a world generator."""
 
-    :param int seed: The seed used to generate the world.
-    :ivar seed:
-    """
-
-    def __init__(self, seed: int) -> None:
-        self.seed = seed
-
-    def generate_chunk(self, chunk_x: int, chunk_z: int) -> Chunk:
-        raise NotImplementedError(self.__class__.__name__)
-
-    def generate_region(self, region_x: int, region_z: int) -> Region:
+    @staticmethod
+    def generate_chunk(seed: int, chunk_x: int, chunk_z: int) -> Chunk:
         raise NotImplementedError(self.__class__.__name__)

98319d02b409a7334be9dfd7bff1a20077128b8f
Milo Weinberg <iapetus011@gmail.com>
2021-02-04 16:24:59 -0500

    update AbstractChunkIO class

--- pymine/api/abc.py
+++ pymine/api/abc.py
@@ -11,7 +11,19 @@ class AbstractWorldGenerator:
 
 class AbstractChunkIO:
     """Abstract class used to create chunk io."""
-    
+
+    @staticmethod
+    def calc_offset(chunk_x: int, chunk_z: int) -> int:
+        raise NotImplementedError(self.__class__.__name__)
+
+    @staticmethod
+    def find_chunk(location: int) -> tuple:
+        raise NotImplementedError(self.__class__.__name__)
+
     @classmethod
-    async def fetch_chunk(cls, world_name: str, chunk_x: int, chunk_z: int) -> Chunk:
+    def fetch_chunk(cls, world_name: str, chunk_x: int, chunk_z: int) -> Chunk:
+        raise NotImplementedError(self.__class__.__name__)
+
+    @classmethod
+    async def fetch_chunk_async(cls, world_name: str, chunk_x: int, chunk_z: int) -> Chunk:
         raise NotImplementedError(self.__class__.__name__)

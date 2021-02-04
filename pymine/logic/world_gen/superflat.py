d784cc7e7d166ad2937364c08f1829a7a3c3ec43
Milo Weinberg <iapetus011@gmail.com>
2021-02-04 14:50:50 -0500

    add block ndarray

--- pymine/logic/world_gen/superflat.py
+++ pymine/logic/world_gen/superflat.py
@@ -1,12 +1,20 @@
-from pymine.types.Region import Region
+import ctypes
+import numpy
+
 from pymine.types.chunk import Chunk
 
+from pymine.data.registries import BLOCK_REGISTRY
 from pymine.api.world_gen import AbstractWorldGenerator
 
 
 class SuperFlatWorldGenerator(AbstractWorldGenerator):
     def generate_chunk(self, chunk_x: int, chunk_z: int) -> Chunk:
-        pass
 
-    def generate_region(self, region_x: int, region_z: int) -> Region:
-        raise NotImplementedError(self.__class__.__name__)
+        # actual blocks in the chunk
+        chunk_blocks = numpy.ndarray((256, 16, 16), numpy.uint16)
+        chunk_blocks.fill(BLOCK_REGISTRY.encode('minecraft:air'))
+        chunk_blocks[0].fill(BLOCK_REGISTRY.encode('minecraft:bedrock'))
+        chunk_blocks[1:3].fill(BLOCK_REGISTRY.encode('minecraft:dirt'))
+        chunk_blocks[4].fill(BLOCK_REGISTRY.encode('minecraft:grass_block'))
+
+        

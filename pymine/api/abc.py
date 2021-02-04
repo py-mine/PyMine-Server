c39a9529584e44757e25b9ac30e2834cfc44d610
Milo Weinberg <iapetus011@gmail.com>
2021-02-04 16:55:35 -0500

    fix stupidity

--- pymine/api/abc.py
+++ pymine/api/abc.py
@@ -6,7 +6,7 @@ class AbstractWorldGenerator:
 
     @staticmethod
     def generate_chunk(seed: int, chunk_x: int, chunk_z: int) -> Chunk:
-        raise NotImplementedError(self.__class__.__name__)
+        raise NotImplementedError
 
 
 class AbstractChunkIO:

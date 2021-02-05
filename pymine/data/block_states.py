b995627c00fc9c26782d99cd8a6ca37740f3e162
Milo Weinberg <iapetus011@gmail.com>
2021-02-05 15:50:46 -0500

    add block_states.py

---
+++ pymine/data/block_states.py
@@ -0,0 +1,20 @@
+import os
+
+from pymine.util.immutable import make_immutable
+from pymine.types.registry import Registry
+
+
+def reversed_bs_data(bs_data):
+    reverse_data = {}
+
+    for k, block in bs_data.items():
+        for sv in block["states"]:
+            reverse_data[sv["id"]] = {"name": k, "properties": sv["properties"]}
+
+    return reverse_data
+
+
+with open(os.path.join("pymine", "data", "blocks.json"), "r") as block_data:
+    bs_data = make_immutable(json.load(block_data))
+
+BLOCK_STATE = Registry(bs_data, reversed_bs_data(bs_data))

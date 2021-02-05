a647a2216867a257c7adb03440f93889cddd2011
Milo Weinberg <iapetus011@gmail.com>
2021-02-05 09:54:38 -0500

    work on SuperFlatWorldGenerator

--- pymine/logic/world_gen/superflat.py
+++ pymine/logic/world_gen/superflat.py
@@ -1,5 +1,5 @@
-import ctypes
 import numpy
+import time
 
 from pymine.types.chunk import Chunk
 
@@ -10,10 +10,11 @@ from pymine.data.registries import BLOCK_REGISTRY
 class SuperFlatWorldGenerator(AbstractWorldGenerator):
     @staticmethod
     def generate_chunk(seed: int, chunk_x: int, chunk_z: int) -> Chunk:
+        chunk = Chunk.new(chunk_x, chunk_z, int(time.time()))
 
         # actual blocks in the chunk
-        chunk_blocks = numpy.ndarray((256, 16, 16), numpy.uint16)
-        chunk_blocks.fill(BLOCK_REGISTRY.encode("minecraft:air"))
-        chunk_blocks[0].fill(BLOCK_REGISTRY.encode("minecraft:bedrock"))
-        chunk_blocks[1:3].fill(BLOCK_REGISTRY.encode("minecraft:dirt"))
-        chunk_blocks[4].fill(BLOCK_REGISTRY.encode("minecraft:grass_block"))
+        # chunk_blocks = numpy.ndarray((256, 16, 16), numpy.uint16)
+        # chunk_blocks.fill(BLOCK_REGISTRY.encode("minecraft:air"))
+        # chunk_blocks[0].fill(BLOCK_REGISTRY.encode("minecraft:bedrock"))
+        # chunk_blocks[1:3].fill(BLOCK_REGISTRY.encode("minecraft:dirt"))
+        # chunk_blocks[4].fill(BLOCK_REGISTRY.encode("minecraft:grass_block"))

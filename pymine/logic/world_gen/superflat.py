2a47b5707fc3d7e106f83a1f9082e84f61e5e0ef
Milo Weinberg <iapetus011@gmail.com>
2021-02-08 11:14:00 -0500

    add new code for superflat world gen

--- pymine/logic/world_gen/superflat.py
+++ pymine/logic/world_gen/superflat.py
@@ -13,13 +13,16 @@ class SuperFlatWorldGenerator(AbstractWorldGenerator):
     # Blocks *should* be packed something like this:
     # 256*[16*[16*[block_state_id, block_light, sky_light]]]
 
+    class Break(BaseException):
+        pass
+
     @staticmethod
-    def generate_chunk(seed: int, dimension: str, chunk_x: int, chunk_z: int) -> object:
+    def generate_chunk(seed: int, dimension: str, chunk_x: int, chunk_z: int) -> numpy.ndarray:
         chunk = Chunk.new(chunk_x, chunk_z, int(time.time()))
 
         # actual blocks in the chunk
         chunk_blocks = numpy.ndarray((256, 16, 16, 3), numpy.uint16)
-        chunk_blocks.fill(DirectPalette.encode("minecraft:air"))
+        chunk_blocks.fill(DirectPalette.encode("minecraft:air"))  # should be 0
 
         # if dimension == "minecraft:overworld":
         #     chunk_blocks[0].fill(DirectPalette.encode("minecraft:bedrock"))
@@ -31,5 +34,15 @@ class SuperFlatWorldGenerator(AbstractWorldGenerator):
         # elif dimension == "minecraft:the_end":
         #     chunk_blocks[0].fill(DirectPalette.encode("minecraft:bedrock"))
         #     chunk_blocks[1:4].fill(DirectPalette.encode("minecraft:end_stone"))
+
+        if dimension == 'minecraft:overworld':
+            chunk_blocks[0] = (DirectPalette.encode("minecraft:bedrock"), 0, 0)
+            chunk_blocks[1:3] = (DirectPalette.encode("minecraft:dirt"), 0, 0)
+            chunk_blocks[4] = (DirectPalette.encode("minecraft:grass_block", {"snowy": "false"}), 0, 15)
+        elif dimension == 'minecraft:nether':
+            chunk_blocks[0] = (DirectPalette.encode("minecraft:bedrock"), 0, 0)
+            chunk_blocks[1:4] = (DirectPalette.encode("minecraft:netherrack"), 0, 6)  # idk what nether sky light level is
+        elif dimension == "minecraft:the_end":
+            chunk_blocks[0:4] = (DirectPalette.encode("minecraft:end_stone"), 0, 6)  # idk what the end sky light level is
 
         return chunk_blocks

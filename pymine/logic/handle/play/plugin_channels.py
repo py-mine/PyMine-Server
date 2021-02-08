8f3b589e592b58e78eed325169f0109cbe73b351
Milo Weinberg <iapetus011@gmail.com>
2021-02-08 17:16:56 -0500

    fix weird indentation

--- pymine/logic/handle/play/plugin_channels.py
+++ pymine/logic/handle/play/plugin_channels.py
@@ -7,5 +7,5 @@ from pymine.server import server
 
 @server.api.events.on_packet("play", 0x0B)
 async def plugin_message_recv(stream: Stream, packet: Packet) -> None:
-if packet.channel == "minecraft:brand":
-    server.cache.uuid[stream.remote].brand = packet.data.unpack_string()
+    if packet.channel == "minecraft:brand":
+        server.cache.uuid[stream.remote].brand = packet.data.unpack_string()

a6fc289e89a3c7443b03722edf775eee12ee1e22
Milo Weinberg <iapetus011@gmail.com>
2021-02-08 12:49:54 -0500

    remove remenants of old code + fix old comments in SuperFlatWorldGen...

--- pymine/logic/world_gen/superflat.py
+++ pymine/logic/world_gen/superflat.py
@@ -18,30 +18,18 @@ class SuperFlatWorldGenerator(AbstractWorldGenerator):
 
     @staticmethod
     def generate_chunk(seed: int, dimension: str, chunk_x: int, chunk_z: int) -> numpy.ndarray:
-        chunk_blocks = numpy.ndarray((256, 16, 16, 3), numpy.uint16)  # actual blocks in the chunk
-        chunk_blocks.fill(DirectPalette.encode("minecraft:air"))  # should be 0
-
-        # if dimension == "minecraft:overworld":
-        #     chunk_blocks[0].fill(DirectPalette.encode("minecraft:bedrock"))
-        #     chunk_blocks[1:3].fill(DirectPalette.encode("minecraft:dirt"))
-        #     chunk_blocks[4].fill(DirectPalette.encode("minecraft:grass_block", {"snowy": "false"}))
-        # elif dimension == "minecraft:nether":
-        #     chunk_blocks[0].fill(DirectPalette.encode("minecraft:bedrock"))
-        #     chunk_blocks[1:4].fill(DirectPalette.encode("minecraft:netherrack"))
-        # elif dimension == "minecraft:the_end":
-        #     chunk_blocks[0].fill(DirectPalette.encode("minecraft:bedrock"))
-        #     chunk_blocks[1:4].fill(DirectPalette.encode("minecraft:end_stone"))
+        chunk_data = numpy.ndarray((256, 16, 16, 3), numpy.uint16)  # (block_state_id, block_light, sky_light)
 
         if dimension == "minecraft:overworld":
-            chunk_blocks[0] = (DirectPalette.encode("minecraft:bedrock"), 0, 0)
-            chunk_blocks[1:3] = (DirectPalette.encode("minecraft:dirt"), 0, 0)
-            chunk_blocks[4] = (DirectPalette.encode("minecraft:grass_block", {"snowy": "false"}), 0, 15)
+            chunk_data[0] = (DirectPalette.encode("minecraft:bedrock"), 0, 0)
+            chunk_data[1:3] = (DirectPalette.encode("minecraft:dirt"), 0, 0)
+            chunk_data[4] = (DirectPalette.encode("minecraft:grass_block", {"snowy": "false"}), 0, 15)
         elif dimension == "minecraft:nether":
-            chunk_blocks[0] = (DirectPalette.encode("minecraft:bedrock"), 0, 0)
-            chunk_blocks[1:4] = (DirectPalette.encode("minecraft:netherrack"), 0, 6)  # idk what nether sky light level is
+            chunk_data[0] = (DirectPalette.encode("minecraft:bedrock"), 0, 0)
+            chunk_data[1:4] = (DirectPalette.encode("minecraft:netherrack"), 0, 6)  # idk what nether sky light level is
         elif dimension == "minecraft:the_end":
-            chunk_blocks[0:4] = (DirectPalette.encode("minecraft:end_stone"), 0, 6)  # idk what the end sky light level is
+            chunk_data[0:4] = (DirectPalette.encode("minecraft:end_stone"), 0, 6)  # idk what the end sky light level is
         else:
             raise ValueError(f"Unsupported dimension: {dimension}")
 
-        return chunk_blocks
+        return chunk_data

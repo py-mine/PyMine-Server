e257d7d4897d78326defc62f21360f8587a76b0e
Milo Weinberg <iapetus011@gmail.com>
2021-02-09 09:59:44 -0500

    add parsers for brigadier:bool, brigadier:double, and brigadier:float

---
+++ pymine/api/parsing/types/brigadier.py
@@ -0,0 +1,39 @@
+
+class Bool:
+    @staticmethod
+    def parse(s: str) -> bool:
+        if s == "true":
+            return True
+
+        if s == "false":
+            return False
+
+        raise ValueError
+
+
+class Double:
+    @staticmethod
+    def parse(s: str, max_=1.7976931348623157e307, min_=-1.7976931348623157e307) -> float:
+        try:
+            value = float(s)
+
+            if max_ >= value >= min_:
+                return value
+        except BaseException:
+            pass
+
+        raise ValueError
+
+
+class Float:
+    @staticmethod
+    def parse(s: str, max_=3.4028235e38, min_=-3.4028235e38) -> float:
+        try:
+            value = float(s)
+
+            if max_ >= value >= min_:
+                return value
+        except BaseException:
+            pass
+
+        raise ValueError

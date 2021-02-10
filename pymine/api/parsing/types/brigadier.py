9828814962c9a353f73e57c1398cb030b5e87d9d
Milo Weinberg <iapetus011@gmail.com>
2021-02-09 23:07:36 -0500

    add other numeric parsers

--- pymine/api/parsing/types/brigadier.py
+++ pymine/api/parsing/types/brigadier.py
@@ -10,3 +10,42 @@ class Bool(AbstractParser):
             return pos + 5, False
 
         raise ValueError
+
+
+class Float(AbstractParser):
+    @staticmethod
+    def parse(pos: int, s: str) -> tuple:
+        section = s.split(' ')[0]
+
+        try:
+            return pos + len(section), float(section)
+        except BaseException:
+            pass
+
+        raise ValueError
+
+
+class Double(AbstractParser):
+    @staticmethod
+    def parse(pos: int, s: str) -> tuple:
+        section = s.split(' ')[0]
+
+        try:
+            return pos + len(section), float(section)
+        except BaseException:
+            pass
+
+        raise ValueError
+
+
+class Integer(AbstractParser):
+    @staticmethod
+    def parse(pos: int, s: str) -> tuple:
+        section = s.split(' ')[0]
+
+        try:
+            return pos + len(section), int(section)
+        except BaseException:
+            pass
+
+        raise ValueError

7adda53e3965bfad1632e3572bd7cdd60753f7fa
Milo Weinberg <iapetus011@gmail.com>
2021-02-09 23:00:56 -0500

    add brigadier:bool parser

--- pymine/api/parsing/types/brigadier.py
+++ pymine/api/parsing/types/brigadier.py
@@ -1,1 +1,12 @@
+from pymine.api.abc import AbstractParser
 
+class Bool(AbstractParser):
+    @staticmethod
+    def parse(pos: int, s: str) -> tuple:
+        if s[pos:4] == "true":
+            return pos + 4, True
+
+        if s[pos:5] == "false":
+            return pos + 5, False
+
+        raise ValueError

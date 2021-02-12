39a3939e02c3aaee9d00ec07195c3d1b52ecc7e5
Milo Weinberg <iapetus011@gmail.com>
2021-02-11 20:54:44 -0500

    add addr and port attrs to ServerBindingError

--- pymine/api/exceptions.py
+++ pymine/api/exceptions.py
@@ -9,3 +9,6 @@ class InvalidPacketID(BaseException):
 class ServerBindingError(BaseException):
     def __init__(self, addr: str, port: int):
         super().__init__(f"Failed to bind to {addr}:{port}")
+
+        self.addr = addr
+        self.port = port

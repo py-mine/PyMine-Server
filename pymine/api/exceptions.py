0040218bccdd9d7a7487686d539327ca045cb9f4
Milo Weinberg <iapetus011@gmail.com>
2021-02-11 20:50:29 -0500

    add ServerBindingError

--- pymine/api/exceptions.py
+++ pymine/api/exceptions.py
@@ -4,3 +4,8 @@ class StopHandling(BaseException):
 
 class InvalidPacketID(BaseException):
     pass
+
+
+class ServerBindingError(BaseException):
+    def __init__(self, addr: str, port: int):
+        super().__init__(f"Failed to bind to {addr}:{port}")

d663d8b5adee4ceaa1842a8002c6fd6b1e75558f
Milo Weinberg <iapetus011@gmail.com>
2021-02-15 10:18:32 -0500

    update parsers/__init__.py

--- pymine/api/parsers/__init__.py
+++ pymine/api/parsers/__init__.py
@@ -1,2 +1,9 @@
-from pymine.api.parsers.brigadier import *
-from pymine.api.parsers.minecraft import *
+import inspect
+
+from pymine.api.abc import AbstractParser
+
+import pymine.api.parsers.brigadier as brigadier
+import pymine.api.parsers.minecraft as minecraft
+
+parsers = (*brigadier.__dict__.values(), *minecraft.__dict__.values(),)
+parsers = tuple(p for p in parsers if (inspect.isclass(p) and issubclass(p, AbstractParser)))

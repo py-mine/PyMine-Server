c683593636ec070081fd67c94d678b3c7174b597
Milo Weinberg <iapetus011@gmail.com>
2021-02-18 22:47:04 -0500

    comment out pymine utility commands for now

--- pymine/logic/cmds/pymine_util.py
+++ pymine/logic/cmds/pymine_util.py
@@ -1,49 +1,49 @@
-from pymine.server import server
-
-
-@server.api.commands.on_command(name="exec", node="pymine.cmds.exec")
-async def exec_(uuid: str, file_name: str):
-
-    try:
-        with open(file_name, "r") as f:
-            cmds_lines = [l.rstrip("\n") for l in f.readlines()]
-
-            for cmd_line in cmds_lines:
-                await server.api.commands.server_command(cmd_line)
-
-    except FileNotFoundError:
-        server.console.warn("Can't find that file...")
-
-
-@server.api.commands.on_command(name="eval", node="pymine.cmds.eval")
-async def eval_(uuid: str, text: str):
-    try:
-        server.console.info(repr(eval(text)))
-    except BaseException as e:
-        server.console.error(server.console.f_traceback(e))
-
-
-@server.api.commands.on_command(name="awaiteval", node="pymine.cmds.eval")
-async def awaiteval(uuid: str, text: str):
-    try:
-        server.console.info(repr(await eval(text)))
-    except BaseException as e:
-        server.console.error(server.console.f_traceback(e))
-
-
-@server.api.commands.on_command(name="echo", node="pymine.cmds.echo")
-async def echo(uuid: str, text: str):
-    server.console.info(f"{uuid}: {text}")
-
-
-@server.api.commands.on_command(name="help", node="pymine.cmds.help")
-async def help(uuid: str, text: str):
-    server.console.info(
-        """PyMine::help
-            help - Lists common commands and usage.
-            eval - Evaluate the arguments as python code.
-            awaiteval - Same as eval, but asynchronous.
-            exec - Execute a specific file(Generally used for debugging).
-            echo - Echos given text back to you.
-    """
-    )
+# from pymine.server import server
+#
+#
+# @server.api.commands.on_command(name="exec", node="pymine.cmds.exec")
+# async def exec_(uuid: str, file_name: str):
+#
+#     try:
+#         with open(file_name, "r") as f:
+#             cmds_lines = [l.rstrip("\n") for l in f.readlines()]
+#
+#             for cmd_line in cmds_lines:
+#                 await server.api.commands.server_command(cmd_line)
+#
+#     except FileNotFoundError:
+#         server.console.warn("Can't find that file...")
+#
+#
+# @server.api.commands.on_command(name="eval", node="pymine.cmds.eval")
+# async def eval_(uuid: str, text: str):
+#     try:
+#         server.console.info(repr(eval(text)))
+#     except BaseException as e:
+#         server.console.error(server.console.f_traceback(e))
+#
+#
+# @server.api.commands.on_command(name="awaiteval", node="pymine.cmds.eval")
+# async def awaiteval(uuid: str, text: str):
+#     try:
+#         server.console.info(repr(await eval(text)))
+#     except BaseException as e:
+#         server.console.error(server.console.f_traceback(e))
+#
+#
+# @server.api.commands.on_command(name="echo", node="pymine.cmds.echo")
+# async def echo(uuid: str, text: str):
+#     server.console.info(f"{uuid}: {text}")
+#
+#
+# @server.api.commands.on_command(name="help", node="pymine.cmds.help")
+# async def help(uuid: str, text: str):
+#     server.console.info(
+#         """PyMine::help
+#             help - Lists common commands and usage.
+#             eval - Evaluate the arguments as python code.
+#             awaiteval - Same as eval, but asynchronous.
+#             exec - Execute a specific file(Generally used for debugging).
+#             echo - Echos given text back to you.
+#     """
+#     )

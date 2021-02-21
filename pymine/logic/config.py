42046e04d819e80d92c201b1fd8223dcae896a79
Milo Weinberg <iapetus011@gmail.com>
2021-02-21 16:47:51 -0500

    remove dev_mode option

--- pymine/logic/config.py
+++ pymine/logic/config.py
@@ -6,7 +6,6 @@ from pymine.util.misc import string_hash_code, gen_seed
 
 DEFAULT_CONFIG = {
     "debug": True,
-    "dev_mode": True,
     "server_ip": None,
     "server_port": 25565,
     "level_name": "world",

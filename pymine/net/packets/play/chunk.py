74dbd91ff4dd1abe129c9eda3527602e80e63029
Milo Weinberg <iapetus011@gmail.com>
2021-02-26 19:27:20 -0500

    delete PlayChunkData

--- pymine/net/packets/play/chunk.py
+++ pymine/net/packets/play/chunk.py
@@ -28,54 +28,7 @@ class PlayUnloadChunk(Packet):
 
 
 class PlayChunkData(Packet):
-    """Sends chunk data to the client. (Server -> Client)
-
-    :param int chunk_x: The chunk x coordinate (block x coordinate // 16).
-    :param int chunk_z: The chunk z coordinate (block z coordinate // 16).
-    :param bool full_chunk: See here: https://wiki.vg/Chunk_Format#Full_chunk.
-    :param int prim_bit_mask: Bitmask with bits set to 1 for every 16×16×16 chunk section whose data is included in Data. The least significant bit represents the chunk section at the bottom of the chunk column (from y=0 to y=15).
-    :param nbt.TAG heightmaps: Compound containing one long array named MOTION_BLOCKING, which is a heightmap for the highest solid block at each position in the chunk (as a compacted long array with 256 entries at 9 bits per entry totaling 36 longs). The Notchian server also adds a WORLD_SURFACE long array, the purpose of which is unknown, but it's not required for the chunk to be accepted.
-    :param bytes data: See chunk data format: https://wiki.vg/Chunk_Format#Full_chunk.
-    :param list block_entities: Array of nbt.TAGs.
-    :param int biomes: Unknown, see here: https://wiki.vg/Protocol#Chunk_Data.
-    :ivar int id: Unique packet ID.
-    :ivar int to: Packet direction.
-    :ivar chunk_x:
-    :ivar chunk_z:
-    :ivar full_chunk:
-    :ivar prim_bit_mask:
-    :ivar heightmaps:
-    :ivar data:
-    :ivar block_entities:
-    :ivar biomes:
-    """
-
-    id = 0x20
-    to = 1
-
-    def __init__(
-        self,
-        chunk_x: int,
-        chunk_z: int,
-        full_chunk: bool,
-        prim_bit_mask: int,
-        heightmaps: nbt.TAG,
-        data: bytes,
-        block_entities: list,
-        biomes: int = None,
-    ) -> None:
-        super().__init__()
-
-        self.chunk_x, self.chunk_z = chunk_x, chunk_z
-        self.full_chunk = full_chunk
-        self.prim_bit_mask = prim_bit_mask
-        self.heightmaps = heightmaps
-        self.data = data
-        self.block_entities = block_entities
-        self.biomes = biomes
-
-    def encode(self) -> bytes:
-        raise NotImplementedError
+    
 
 
 class PlayUpdateLight(Packet):

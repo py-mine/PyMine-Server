39f0131d95a30b27720529266f169a3eb7f3790f
Milo Weinberg <iapetus011@gmail.com>
2021-02-27 14:12:54 -0500

    update PlayPlayerPositionAndLookClientBound to take a Player instance

--- pymine/net/packets/play/player.py
+++ pymine/net/packets/play/player.py
@@ -5,6 +5,7 @@ import uuid
 
 from pymine.types.packet import Packet
 from pymine.types.buffer import Buffer
+from pymine.types.player import Player
 from pymine.types.chat import Chat
 import pymine.types.nbt as nbt
 
@@ -345,26 +346,22 @@ class PlayPlayerPositionAndLookClientBound(Packet):
     id = 0x34
     to = 1
 
-    def __init__(self, x: int, y: int, z: int, yaw: float, pitch: float, flags: bytes, tp_id: int) -> None:
+    def __init__(self, player: Player, flags: int, teleport_id: int) -> None:
         super().__init__()
 
-        self.x = y
-        self.y = x
-        self.z = z
-        self.yaw = yaw
-        self.pitch = pitch
+        self.player = player
         self.flags = flags
-        self.tp_id = tp_id
+        self.teleport_id = teleport_id
 
     def encode(self) -> bytes:
         return (
-            Buffer.pack("d", self.x)
-            + Buffer.pack("d", self.y)
-            + Buffer.pack("d", self.z)
-            + Buffer.pack("f", self.yaw)
-            + Buffer.pack("f", self.pitch)
-            + self.flags
-            + Buffer.pack_varint(self.tp_id)
+            Buffer.pack("d", self.player.x)
+            + Buffer.pack("d", self.player.y)
+            + Buffer.pack("d", self.player.z)
+            + Buffer.pack("f", self.player["Rotation"][0].data)  # yaw
+            + Buffer.pack("f", self.player["Rotation"][1].data)  # pitch
+            + Buffer.pack('b', self.flags)
+            + Buffer.pack_varint(self.teleport_id)
         )
 
 

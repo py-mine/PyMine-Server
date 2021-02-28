70fc30795979892057d5b97f004c932a463ac2d1
The456gamer <The456gamer@the456gamer.dev>
2021-02-28 15:09:00 +0000

    Add startup shell script

---
+++ startserver.sh
@@ -0,0 +1,16 @@
+for command in pypy python python3.9 python3.8 python3.7 python3  
+do
+
+#!/bin/bash
+currentver=`$command -c "import platform;print(platform.python_version())"`
+# echo $currentver
+requiredver="3.7"
+ if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
+        echo "python version ok ($currentver)"
+        $command pymine
+        exit 0
+ fi
+done
+
+echo Couldnt find Suitble python version. please use your package manager to install python 3.7 or later 
+exit 1

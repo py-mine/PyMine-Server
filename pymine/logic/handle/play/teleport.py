3d22375ff8114e00f6c81d8399eae56bcb49824a
Milo Weinberg <iapetus011@gmail.com>
2021-02-28 13:46:36 -0500

    don't disconnect on incorrect teleport confirm for now

--- pymine/logic/handle/play/teleport.py
+++ pymine/logic/handle/play/teleport.py
@@ -11,6 +11,8 @@ async def on_teleport_confirm(stream: Stream, packet: Packet) -> None:
     player = await server.playerio.fetch_player(server.cache.uuid[stream.remote])
 
     if player.teleport_id != packet.teleport_id:
-        await server.send_packet(
-            stream, PlayDisconnect(f"Invalid teleport ID ({repr(player.teleport_id)} != {repr(packet.teleport_id)}).")
-        )
+        # await server.send_packet(
+        #     stream, PlayDisconnect(f"Invalid teleport ID ({repr(player.teleport_id)} != {repr(packet.teleport_id)}).")
+        # )
+
+        server.console.debug(f"Warning, invalid teleport ID. ({repr(player.teleport_id)} != {repr(packet.teleport_id)})")

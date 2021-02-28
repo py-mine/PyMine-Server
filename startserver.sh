4680c07af24c08f6ace9bd8930908ab49e8305d0
The456gamer <The456gamer@the456gamer.dev>
2021-02-28 18:27:04 +0000

    re-order commands, fix grammar

--- startserver.sh
+++ startserver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for command in pypy python python3.9 python3.8 python3.7 python3  
+for command in pypy3 pypy python3.9 python3.8 python3.7 python3 python
 do
 
 currentver=$($command -c "import platform;print(platform.python_version())")
 requiredver="3.7.9"
-if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
+if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then
         echo "python version ok ($currentver)"
         $command pymine
         exit 0
@@ -12,5 +12,5 @@ fi
 
 done
 
-echo "Couldn't find suitable Python version. Please use your package manager to install python 3.7.9 or later"
+echo "Couldn't find suitable Python version. Please use your package manager to install Python 3.7.9 or later"
 exit 1

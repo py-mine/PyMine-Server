5cfc03c17645f791747ef7743a3fe37582217ebc
The456gamer <The456gamer@the456gamer.dev>
2021-02-28 18:23:35 +0000

    fix typos + clean up script

--- startserver.sh
+++ startserver.sh
@@ -2,16 +2,15 @@
 for command in pypy python python3.9 python3.8 python3.7 python3  
 do
 
-#!/bin/bash
 currentver=$($command -c "import platform;print(platform.python_version())")
-# echo $currentver
 requiredver="3.7.9"
- if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
+if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
         echo "python version ok ($currentver)"
         $command pymine
         exit 0
- fi
+fi
+
 done
 
-echo "Couldnt find Suitable python version. please use your package manager to install python 3.7.9 or later"
+echo "Couldn't find suitable Python version. Please use your package manager to install python 3.7.9 or later"
 exit 1

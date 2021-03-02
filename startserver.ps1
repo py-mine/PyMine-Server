f42911df0f96a5af42349b378d18e60278145914
The456gamer <mr.the456gamer@gmail.com>
2021-03-02 20:46:05 +0000

    add windows start script

---
+++ startserver.ps1
@@ -0,0 +1,20 @@
+#!/bin/env pwsh
+$pythonversions = @('pypy3','pypy','py','python3','python')
+$minimumVersion = '3.7.9'
+
+foreach ($command in $pythonversions) {
+    try {
+        if(Get-Command -ErrorAction Stop $command){
+            $currentver=Invoke-Expression -Command "$command -c 'import platform;print(platform.python_version())'"
+            if ($currentver -ge $minimumVersion) {
+                Write-Output "Using Python version $currentver ($command)"
+                Invoke-Expression $command pymine
+                exit 0
+            }
+        }
+    }
+    Catch {}
+}
+
+Write-Output "Couldn't find suitable Python version. Please use your package manager to install Python 3.7.9 or later"
+exit 1

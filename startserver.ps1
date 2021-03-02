d4e030f5a0ba3dccbf39bae35492aaf054692684
The456gamer <mr.the456gamer@gmail.com>
2021-03-02 20:56:35 +0000

    [pwsh] change method of ignoring invalid versions

--- startserver.ps1
+++ startserver.ps1
@@ -3,8 +3,7 @@ $pythonversions = @('pypy3','pypy','py','python3','python')
 $minimumVersion = '3.7.9'
 
 foreach ($command in $pythonversions) {
-    try {
-        if(Get-Command -ErrorAction Stop $command){
+        if(Get-Command -ErrorAction SilentlyContinue $command){
             $currentver=Invoke-Expression -Command "$command -c 'import platform;print(platform.python_version())'"
             if ($currentver -ge $minimumVersion) {
                 Write-Output "Using Python version $currentver ($command)"
@@ -12,8 +11,6 @@ foreach ($command in $pythonversions) {
                 exit 0
             }
         }
-    }
-    Catch {}
 }
 
 Write-Output "Couldn't find suitable Python version. Please use your package manager to install Python 3.7.9 or later"

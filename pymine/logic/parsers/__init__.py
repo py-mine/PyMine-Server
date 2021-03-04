a89568e75e1ff7b17efaa9afd1edf58395bc441d
Milo Weinberg <iapetus011@gmail.com>
2021-03-04 18:41:17 -0500

    ignore special files when loading parsers

--- pymine/logic/parsers/__init__.py
+++ pymine/logic/parsers/__init__.py
@@ -7,10 +7,10 @@ from pymine.api.abc import AbstractParser
 parsers = classyjson.ClassyDict()
 
 for root, dirs, files in os.walk(os.path.join("pymine", "logic", "parsers")):
-    for file in filter((lambda f: f.endswith(".py")), files):
+    for file in filter((lambda f: f.endswith(".py") and "__" not in f), files):
         module = importlib.import_module(os.path.join(root, file)[:-3].replace("\\", "/").replace("/", "."))
 
-        for name, obj in list(module.__dict__.items()):
+        for name, obj in module.__dict__.items():
             try:
                 if issubclass(obj, AbstractParser):
                     parsers[name] = obj

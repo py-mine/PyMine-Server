86f8bb7f54de5ffe2d25aa63c83a69bc7b090838
Milo Weinberg <iapetus011@gmail.com>
2021-03-04 18:08:32 -0500

    add Player parser

--- pymine/logic/parsers/minecraft/entity.py
+++ pymine/logic/parsers/minecraft/entity.py
@@ -8,9 +8,18 @@ from pymine.server import server
 
 
 class UUID(AbstractParser):
-    def __init__(self) -> None:
-        pass
+    def parse(self, s: str) -> tuple:
+        try:
+            section = s.split()[0]
+        except IndexError:
+            raise ParsingError
 
+        try:
+            return len(section), server.playerio.cache[int(uuid.UUID(section))]
+        except (ValueError, KeyError):  # valueerror for if section isn't valid uuid, keyerror for if player isn't in cache
+            raise ParsingError
+
+class Player(AbstractParser):
     def parse(self, s: str) -> tuple:
         try:
             section = s.split()[0]

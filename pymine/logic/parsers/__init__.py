4341a1a80b804f2b6ae24cc983c3e26fedb22b4a
Milo Weinberg <iapetus011@gmail.com>
2021-03-04 18:37:22 -0500

    handle errors from loading the parsers better

--- pymine/logic/parsers/__init__.py
+++ pymine/logic/parsers/__init__.py
@@ -2,12 +2,17 @@ import classyjson
 import importlib
 import os
 
+from pymine.api.abc import AbstractParser
+
 parsers = classyjson.ClassyDict()
 
 for root, dirs, files in os.walk(os.path.join("pymine", "logic", "parsers")):
     for file in filter((lambda f: f.endswith(".py")), files):
         module = importlib.import_module(os.path.join(root, file)[:-3].replace("\\", "/").replace("/", "."))
 
-        for name, obj in module.__dict__.items():
-            if issubclass(obj, AbstractParser):
-                parsers[name] = obj
+        for name, obj in list(module.__dict__.items()):
+            try:
+                if issubclass(obj, AbstractParser):
+                    parsers[name] = obj
+            except (TypeError, KeyError):  # can't call issubclass() on non-classes
+                pass

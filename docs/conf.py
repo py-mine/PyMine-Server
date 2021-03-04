2e0e769b2deb3dfcca0797c6eab87bf6ea4530d2
Ashwin Vinod <ashwinvinodsa@gmail.com>
2021-03-04 11:52:11 +0530

    add sphinx tabs module

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ release = "0.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx_rtd_theme"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx.ext.autodoc",
+              "sphinx.ext.viewcode", "sphinx_rtd_theme", "sphinx_tabs.tabs"]
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ["_templates"]

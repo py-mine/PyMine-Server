2d751ce4d999ce425881a713011b6e5e2216a2c8
Milo Weinberg <iapetus011@gmail.com>
2021-03-05 11:10:54 -0500

    fix another typo

--- pymine/logic/cmds/list.py
+++ pymine/logic/cmds/list.py
@@ -3,6 +3,6 @@ from pymine.server import server
 
 @server.api.commands.on_command(name="list", node="minecraft.cmd.list")
 async def list(uuid):
-    self.console.info(
+    server.console.info(
         f"There are {len(server.playerio.cache)}/{server.conf['max_players']} online: {', '.join([p.username for p in server.playerio.cache.values()])}"
     )

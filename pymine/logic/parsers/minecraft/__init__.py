7049ecf85a2e407cfba4e4f6d7fde7fda05a4a64
BuildTools <unconfigured@null.spigotmc.org>
2021-03-04 18:32:40 -0600

    a

--- pymine/logic/parsers/minecraft/__init__.py
+++ pymine/logic/parsers/minecraft/__init__.py
@@ -1,2 +1,3 @@
 from pymine.logic.parsers.minecraft.entity import Entity
 from pymine.logic.parsers.minecraft.player import UUID
+from pymine.logic.parsers.minecraft.blockpredicate import BlockPredicate

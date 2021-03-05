bd1f3fafd9c5710d5ec243c57c57379924e85df8
Milo Weinberg <iapetus011@gmail.com>
2021-03-05 11:12:47 -0500

    update list command to handle 0 peeps online properly

--- pymine/logic/cmds/list.py
+++ pymine/logic/cmds/list.py
@@ -3,6 +3,13 @@ from pymine.server import server
 
 @server.api.commands.on_command(name="list", node="minecraft.cmd.list")
 async def list(uuid):
-    server.console.info(
-        f"There are {len(server.playerio.cache)}/{server.conf['max_players']} online: {', '.join([p.username for p in server.playerio.cache.values()])}"
-    )
+    players_online = len(server.playerio.cache)
+
+    if players_only > 0:
+        server.console.info(
+            f"There are {players_online}/{server.conf['max_players']} players online: {', '.join([p.username for p in server.playerio.cache.values()])}"
+        )
+    else:
+        server.console.info(
+            f"There are {players_online}/{server.conf['max_players']} players online."
+        )

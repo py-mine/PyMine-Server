10159fed082075bbc373cf55b54b604d1cb2b1d8
Milo Weinberg <iapetus011@gmail.com>
2021-03-04 19:25:49 -0500

    fix location of DualMethod

--- pymine/logic/config.py
+++ pymine/logic/config.py
@@ -68,22 +68,3 @@ def load_favicon():
             return "data:image/png;base64," + base64.b64encode(favicon.read()).decode("utf-8")
     except FileNotFoundError:
         return None
-
-
-class DualMethod:
-    """Allows a method of a class to be a classmethod or regular method.
-    If the method is called like Class.method(), the first parameter will be the class object.
-    If the method is called like instance.method(), the first parameter will be the instance of the class object.
-
-    Usage is like:
-    class Foo:
-        @DualMethod
-        def bar(self, *args):
-            print(*args)
-    """
-
-    def __init__(self, func):
-        self._func = func
-
-    def __get__(self, instance, owner=None):
-        return types.MethodType(self._func, (owner if instance is None else instance))
--- pymine/util/misc.py
+++ pymine/util/misc.py
@@ -22,3 +22,22 @@ def seed_hash(seed: int):  # probably not correct but it seems to work so?
     m = hashlib.sha256()
     m.update(seed.to_bytes(8, "big"))
     return int(str(int.from_bytes(m.digest(), "big"))[:8])
+
+
+class DualMethod:
+    """Allows a method of a class to be a classmethod or regular method.
+    If the method is called like Class.method(), the first parameter will be the class object.
+    If the method is called like instance.method(), the first parameter will be the instance of the class object.
+
+    Usage is like:
+    class Foo:
+        @DualMethod
+        def bar(self, *args):
+            print(*args)
+    """
+
+    def __init__(self, func):
+        self._func = func
+
+    def __get__(self, instance, owner=None):
+        return types.MethodType(self._func, (owner if instance is None else instance))

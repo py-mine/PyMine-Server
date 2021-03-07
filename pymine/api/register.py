4f93d822050f852eb3dfd14f746b5d9f464a8c08
Milo Weinberg <iapetus011@gmail.com>
2021-03-07 18:38:10 -0500

    add Register.plugin()

--- pymine/api/register.py
+++ pymine/api/register.py
@@ -3,6 +3,7 @@ from pymine.api.abc import AbstractWorldGenerator
 
 class Register:
     def __init__(self) -> None:
+        self._plugins = {}
         self._generators = {}
 
     def world_generator(self, name: str):
@@ -15,3 +16,9 @@ class Register:
             return cls
 
         return deco
+
+    def plugin(self, plugin: AbstractPlugin) -> None:
+        if not isinstance(plugin, AbstractPlugin):
+            raise ValueError("Plugin must be an instance of AbstractPlugin.")
+
+        self._plugins[plugin.__name__] = plugin

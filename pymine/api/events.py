fd2253a7473302b9db5f724dca8a56f4a2a68949
Milo Weinberg <iapetus011@gmail.com>
2021-03-07 19:07:49 -0500

    update PacketEvent to take func arg instead of cls arg

--- pymine/api/events.py
+++ pymine/api/events.py
@@ -2,7 +2,8 @@ from pymine.api.abc import AbstractEvent, AbstractPlugin
 
 
 class PacketEvent(AbstractEvent):
-    def __init__(self, cls: AbstractPlugin, state_id: int, packet_id: int):
-        self.cls = cls
+    def __init__(self, func, state_id: int, packet_id: int):
+        self.func = func
+        self.cls = func.__self__
         self.state_id = state_id
         self.packet_id = packet_id

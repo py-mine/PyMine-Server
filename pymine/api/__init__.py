dae31edf37bea9b929d8808db63352b059d22940
Milo Weinberg <iapetus011@gmail.com>
2021-03-08 08:54:55 -0500

    fix import

--- pymine/api/__init__.py
+++ pymine/api/__init__.py
@@ -9,8 +9,8 @@ import os
 
 from pymine.util.immutable import make_immutable
 
+from pymine.api.abc import AbstractPlugin, AbstractEvent
 from pymine.api.commands import CommandHandler
-from pymine.api.abc import AbstractPlugin
 from pymine.api.register import Register
 import pymine.api.events as events
 

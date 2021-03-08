a4a8a2197d5de12770c5371676db79a027ed140c
Milo Weinberg <iapetus011@gmail.com>
2021-03-08 08:34:49 -0500

    add docstrings to the different events

--- pymine/api/events.py
+++ pymine/api/events.py
@@ -2,12 +2,16 @@ from pymine.api.abc import AbstractEvent, AbstractPlugin
 
 
 class GenericEvent(AbstractEvent):
+    """Used to create events which take no extra parameters."""
+
     def __init__(self, handler):
         self.handler = handler
         self.cls = handler.__self__
 
 
 class PacketEvent(AbstractEvent):
+    """Triggered when an oncoming packet for the specified state id and packet id is received."""
+
     def __init__(self, handler, state_id: int, packet_id: int):
         self.handler = handler
         self.cls = handler.__self__
@@ -15,5 +19,7 @@ class PacketEvent(AbstractEvent):
         self.packet_id = packet_id
 
 
-class ServerStartEvent(GenericEvent):    
+class ServerStartEvent(GenericEvent):
+    """Triggered when the server starts up."""
+
     pass

93115faa8c56a72df7c63127232157a65fc649d4
Milo Weinberg <iapetus011@gmail.com>
2021-03-07 19:14:35 -0500

    update some arg and attr names in PacketEvent

--- pymine/api/events.py
+++ pymine/api/events.py
@@ -2,8 +2,8 @@ from pymine.api.abc import AbstractEvent, AbstractPlugin
 
 
 class PacketEvent(AbstractEvent):
-    def __init__(self, func, state_id: int, packet_id: int):
-        self.func = func
-        self.cls = func.__self__
+    def __init__(self, handler, state_id: int, packet_id: int):
+        self.handler = handler
+        self.cls = handler.__self__
         self.state_id = state_id
         self.packet_id = packet_id

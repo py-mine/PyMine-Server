a9020c549eee23310f5f6274df99d4c0983e9167
Milo Weinberg <iapetus011@gmail.com>
2021-03-08 08:32:34 -0500

    add events.GenericEvent

--- pymine/api/events.py
+++ pymine/api/events.py
@@ -1,6 +1,12 @@
 from pymine.api.abc import AbstractEvent, AbstractPlugin
 
 
+class GenericEvent(AbstractEvent):
+    def __init__(self, handler):
+        self.handler = handler
+        self.cls = handler.__self__
+
+
 class PacketEvent(AbstractEvent):
     def __init__(self, handler, state_id: int, packet_id: int):
         self.handler = handler
@@ -9,7 +15,5 @@ class PacketEvent(AbstractEvent):
         self.packet_id = packet_id
 
 
-class ServerStartEvent(AbstractEvent):
-    def __init__(self, handler):
-        self.handler = handler
-        self.cls = handler.__self__
+class ServerStartEvent(GenericEvent):    
+    pass

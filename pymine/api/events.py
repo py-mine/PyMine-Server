b4435f0c5c5332516832b3632ffce8c32368cc2b
Milo Weinberg <iapetus011@gmail.com>
2021-03-07 19:02:52 -0500

    update PacketEvent __init__ to take cls arg

--- pymine/api/events.py
+++ pymine/api/events.py
@@ -1,6 +1,7 @@
-from pymine.api.abc import AbstractEvent
+from pymine.api.abc import AbstractEvent, AbstractPlugin
 
 class PacketEvent(AbstractEvent):
-    def __init__(self, state_id: int, packet_id: int):
+    def __init__(self, cls: AbstractPlugin, state_id: int, packet_id: int):
+        self.cls = cls
         self.state_id = state_id
         self.packet_id = packet_id

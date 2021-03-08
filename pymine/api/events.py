2a39883777f89190e8c6665d773f58a7f1e3d42c
Milo Weinberg <iapetus011@gmail.com>
2021-03-08 09:45:00 -0500

    allow PacketEvent to take regular functions

--- pymine/api/events.py
+++ pymine/api/events.py
@@ -14,7 +14,12 @@ class PacketEvent(AbstractEvent):
 
     def __init__(self, handler, state_id: int, packet_id: int):
         self.handler = handler
-        self.cls = handler.__self__
+
+        if hasattr(handler, "__self__"):
+            self.cls = handler.__self___
+        else:
+            self.cls = None
+
         self.state_id = state_id
         self.packet_id = packet_id
 

bd79395085f3d6a2a1586944455651b5659701e9
Milo Weinberg <iapetus011@gmail.com>
2021-03-07 19:08:08 -0500

    update usage of PacketEvent

--- pymine/api/register.py
+++ pymine/api/register.py
@@ -36,6 +36,6 @@ class Register:
             if not asyncio.iscoroutinefunction(func):
                 raise ValueError("Decorated object must be a coroutine function.")
 
-            return PacketEvent(func.__self__, state, packet_id)
+            return PacketEvent(func, state, packet_id)
 
         return deco

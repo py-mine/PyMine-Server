41da91a07b152781d9263394f12f08b03def7d5a
Milo Weinberg <iapetus011@gmail.com>
2021-03-09 19:03:28 -0500

    fix typo

--- pymine/util/chunk.py
+++ pymine/util/chunk.py
@@ -8,7 +8,7 @@ from pymine.types.chunk import Chunk
 def dump_to_obj(file, pymine_chunk: Chunk, palette: AbstractPalette):
     chunk = numpy.ndarray((256, 16, 16))
 
-    for section in pymine_chunk.sections:
+    for section in pymine_chunk.sections.values():
         pymine_chunk[section.y : section.y + 16] = section.block_states
 
     air = palette.encode("minecraft:air")

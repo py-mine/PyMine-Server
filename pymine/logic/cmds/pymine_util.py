c53f7a15f141a1a8495d952acfa101213e366e70
Milo Weinberg <iapetus011@gmail.com>
2021-03-09 21:23:01 -0500

    add testworldgen command

--- pymine/logic/cmds/pymine_util.py
+++ pymine/logic/cmds/pymine_util.py
@@ -1,5 +1,6 @@
 from pymine.logic.parsers.brigadier import *
 
+from pymine.util.chunk import dump_to_obj
 from pymine.util.misc import nice_eval
 
 from pymine.server import server
@@ -18,3 +19,10 @@ async def eval_(uuid, text: String(2)):
 @server.api.commands.on_command(name="test", node="pymine.cmds.test")
 async def test(uuid, b: Bool, f: Float(), d: Double(), i: Integer(), s: str, s2: String(1), s3: String(2)):
     print(uuid, b, f, d, i, s, s2, s3)
+
+@server.api.commands.on_command(name="testworldgen", node="pymine.cmds.testworldgen")
+async def test_world_gen(uuid):
+    chunk = await server.worlds["minecraft:overworld"].fetch_chunk(0, 0)
+
+    with open("chunk_test.obj", "w+") as f:
+        dump_to_obj(f, chunk)
